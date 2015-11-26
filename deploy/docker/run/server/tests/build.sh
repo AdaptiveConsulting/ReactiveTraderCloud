6e31b8f632bf5c50e4b98db28024c75e7cd09202
tdeheurles <tdeheurles@gmail.com>
2015-11-26 10:40:41 +0100

    Add automation for dnx version

--- deploy/docker/run/server/tests/build.sh
+++ deploy/docker/run/server/tests/build.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-version="1.0.0-rc1-final"   #TODO: need to get version from src/global.json
+if [[ ! -f jq ]];then
+  echo "downloading jq to manage json (~2Mb)"
+  curl -L -o jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+  chmod 755 jq
+fi
+
+# Get parameters
+version=`cat ../../../../../src/server/global.json | jq '.sdk.version' | sed 's/"//g'`
 containerPath="./container"
 
-
+# sync server src
 mkdir -p $containerPath
 rsync -aq ../../../../../src/server $containerPath/
 
+# generate Dockerfile from template
 sed "s/__VERSION__/$version/g" template.Dockerfile > $containerPath/Dockerfile
 
+# build
 docker build -t weareadaptive/reactive-trader-server $containerPath/.
