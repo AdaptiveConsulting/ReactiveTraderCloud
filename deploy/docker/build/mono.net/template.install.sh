10ce5e779e88df151fa3466c9bd8db5c6e2b96d6
tdeheurles <tdeheurles@gmail.com>
2015-11-26 12:31:37 +0100

    Correction to mono.net container

--- deploy/docker/build/mono.net/template.install.sh
+++ deploy/docker/build/mono.net/template.install.sh
@@ -1,20 +1,28 @@
 #! /bin/bash
 
 # install mono
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF || exit 1
 echo "deb http://download.mono-project.com/repo/debian wheezy main" \
-| sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-apt-get update
+| sudo tee /etc/apt/sources.list.d/mono-xamarin.list  || exit 1
+apt-get update  || exit 1
 echo "deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main" \
-| sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
-sudo apt-get install -y mono-complete
+| sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list  || exit 1
+sudo apt-get install -y mono-complete  || exit 1
+
+# install dependencies
+sudo apt-get install -y unzip                \
+                        libunwind8           \
+                        libssl-dev           \
+                        curl                 \
+                        libcurl4-openssl-dev \
+                        libcurl3-gnutls      \
+  || exit 1
 
 # install DNVM
 curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh \
-| DNX_BRANCH=dev sh
-mozroots --import --sync
-bash -c "source /root/.dnx/dnvm/dnvm.sh && dnvm upgrade -r mono && dnvm install __VERSION__ -p"
+| DNX_BRANCH=dev sh  || exit 1
+mozroots --import --sync  || exit 1
+bash -c "source /root/.dnx/dnvm/dnvm.sh && dnvm upgrade -r mono && dnvm install __VERSION__ -p"  || exit 1
 
 # Clean
-apt-get remove automake make -y
-apt-get autoremove -y
+apt-get autoremove -y  || exit 1
