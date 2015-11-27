b7b3d8a308859d0df8964dbb76c23fc562f5d50c
tdeheurles <tdeheurles@gmail.com>
2015-11-27 17:48:02 +0100

    Add crossbar container

---
+++ deploy/docker/build/crossbar/install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# crossbar
+apt-key adv --keyserver hkps.pool.sks-keyservers.net --recv D58C6920
+sh -c "echo 'deb http://package.crossbar.io/ubuntu trusty main' \
+    > /etc/apt/sources.list.d/crossbar.list"
+
+apt-get update
+apt-get install crossbar -y
