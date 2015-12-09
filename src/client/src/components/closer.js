c8599bb8e451bd970f648dcfdc569f3de68c5e20
DimitarChristoff <christoff@gmail.com>
2015-12-09 16:32:58 +0000

    removed redundant class

--- src/client/src/components/closer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class Closer extends React.Component {
-  render(){
-    return <i className='fa fa-times pull-right' onClick={this.props.onClick} />;
-  }
-}
