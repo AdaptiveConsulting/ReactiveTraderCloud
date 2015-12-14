6c46d3ca8caa81ec2c550d6877c059bb9ae8394e
thibault deheurles <tdeheurles>
2015-12-14 09:51:30 +0100

    Fix populating cache issue

--- deploy/docker/build/servers/build.sh
+++ deploy/docker/build/servers/build.sh
@@ -35,7 +35,11 @@ sed "s/__VDNX__/$vDnx/g" ./template.Dockerfile > ./build/Dockerfile
 docker build --no-cache -t weareadaptive/serverssrc:latest ./build/.
 
 docker rm dnurestored
-docker run --name dnurestored -v /$(pwd)/dnxcache:/root/.dnx/packages weareadaptive/serverssrc:latest bash -c "dnu restore" 
+buildCommand="mkdir -p /packages"
+buildCommand="$buildCommand && cp -r /packages /root/.dnx/packages"
+buildCommand="$buildCommand && dnu restore"
+buildCommand="$buildCommand && cp -r /root/.dnx/packages /packages"
+docker run --name dnurestored -v /$(pwd)/dnxcache:/packages weareadaptive/serverssrc:latest bash -c "$buildCommand" 
 
 containerTaggedName="$serversContainer:$vMajor.$vMinor.$build"
 docker commit dnurestored $containerTaggedName 
