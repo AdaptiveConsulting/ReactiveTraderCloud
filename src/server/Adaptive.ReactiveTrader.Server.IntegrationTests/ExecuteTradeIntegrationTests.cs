6e03bce5b927130d0c7a018ff7c5619880453e26
James Watson <james@weareadaptive.com>
2015-12-15 17:26:18 +0000

    add timeouts to tests

--- src/server/Adaptive.ReactiveTrader.Server.IntegrationTests/ExecuteTradeIntegrationTests.cs
+++ src/server/Adaptive.ReactiveTrader.Server.IntegrationTests/ExecuteTradeIntegrationTests.cs
@@ -37,6 +37,7 @@ namespace Adaptive.ReactiveTrader.Server.IntegrationTests
                 .Where(hb => hb.Type == ServiceTypes.Execution)
                 .Select(hb => hb.Instance)
                 .Take(1)
+                .Timeout(TimeSpan.FromSeconds(5))
                 .ToTask()
                 .Result;
 
