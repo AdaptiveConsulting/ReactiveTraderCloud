6a6626538dbf8fd048bc160edd7642a1b63fc33e
DimitarChristoff <christoff@gmail.com>
2015-12-21 16:10:24 +0000

    refactors and tweaks, propTypes

--- src/client/src/components/blotter.js
+++ src/client/src/components/blotter.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// data grid
 import FixedDataTable from 'fixed-data-table';
 const {Table, Column, Cell} = FixedDataTable;
 
-
-import Dimensions from 'react-dimensions';
+// utils for date and money
 import numeral from 'numeral';
 import moment from 'moment';
-import Container from './container';
-import _ from 'lodash';
 
+// containers and layout
+import Dimensions from 'react-dimensions';
+import Container from './container';
+
+import { findWhere } from 'lodash';
+
+/**
+ * Formatter for cells of type date/time via moment.js
+ * @class DateCell
+ * @extends {React.Component}
+ */
 class DateCell extends React.Component {
+
+  static propTypes = {
+    format: React.PropTypes.string,
+    prefix: React.PropTypes.string,
+    field: React.PropTypes.string.isRequired,
+    data: React.PropTypes.array.isRequired,
+    rowIndex: React.PropTypes.number.isRequired
+  }
+
   render(){
     const { rowIndex, field, data, format = 'MMM Do, HH:mm:ss', prefix = '', ...props } = this.props;
     const formatted = moment(data[rowIndex][field]).format(format);
@@ -24,10 +42,24 @@ class DateCell extends React.Component {
   }
 }
 
-class MoneyCell extends React.Component {
+/**
+ * Formatter for cells of type notional/currency
+ * @class NotionalCell
+ * @extends {React.Component}
+ */
+class NotionalCell extends React.Component {
+
+  static propTypes = {
+    format: React.PropTypes.string,
+    suffix: React.PropTypes.string,
+    field: React.PropTypes.string.isRequired,
+    data: React.PropTypes.array.isRequired,
+    rowIndex: React.PropTypes.number.isRequired
+  }
+
   render(){
-    const { rowIndex, field, data, ...props } = this.props;
-    const formatted = numeral(data[rowIndex][field]).format('0,000,000[.]00') + ' ' + data[rowIndex].pair.substr(0, 3);
+    const { rowIndex, field, data, format = '0,000,000[.]00', suffix = '', ...props } = this.props;
+    const formatted = numeral(data[rowIndex][field]).format(format) + suffix;
 
     return (
       <Cell {...props}>
@@ -63,7 +95,7 @@ class Blotter extends React.Component {
       // listen to messages about highlighting the relevant trade
       window.fin.desktop.main(() =>{
         window.fin.desktop.InterApplicationBus.subscribe('*', 'acknowledgeTrade', (id) =>{
-          const trade = _.findWhere(this.props.trades, {
+          const trade = findWhere(this.props.trades, {
             id
           });
 
@@ -83,19 +115,23 @@ class Blotter extends React.Component {
     this.state.flagged = false;
   }
 
-  render(){
-    const className = this.props.status ? 'blotter online' : 'blotter offline';
-    const { flagged } = this.state;
+  getRowClass(rowItem){
+    const flagged = this.state.flagged,
+          flash = flagged && flagged === rowItem.id;
 
-    function getClass(item){
-      const flash = flagged && flagged === item.id;
-      return item.status + ' animated ' + (flash ? 'flash' : 'slideInDown');
-    }
+    return rowItem.status + ' animated ' + (flash ? 'flash' : 'slideInDown');
+  }
+
+  render(){
+    const outerClassName = this.props.status ? 'blotter online' : 'blotter offline';
+
+    const { flagged } = this.state,
+          { trades } = this.props;
 
     return (
         <Container
           title='blotter'
-          className={className}
+          className={outerClassName}
           onTearoff={(state) => this.tearOff(state)}
           tearoff={this.state.tearoff}
           width={this.props.containerWidth}
@@ -108,65 +144,65 @@ class Blotter extends React.Component {
           <Table
             rowHeight={24}
             headerHeight={30}
-            rowsCount={this.props.trades.length}
+            rowsCount={trades.length}
             width={this.props.containerWidth}
             height={300}
-            rowClassNameGetter={(index) => getClass(this.props.trades[index])}
+            rowClassNameGetter={(index) => this.getRowClass(trades[index])}
             {...this.props}>
             <Column
               header={<Cell>Id</Cell>}
               cell={props => <Cell {...props}>
-                {this.props.trades[props.rowIndex].id}
+                {trades[props.rowIndex].id}
               </Cell>}
               width={50}
             />
             <Column
               header={<Cell>Date</Cell>}
-              cell={props => <DateCell field='dateTime' data={this.props.trades} {...props} />}
+              cell={props => <DateCell field='dateTime' data={trades} {...props} />}
               width={150}
             />
             <Column
               header={<Cell>Dir</Cell>}
               cell={props => <Cell {...props}>
-                {this.props.trades[props.rowIndex].direction}
+                {trades[props.rowIndex].direction}
               </Cell>}
               width={50}
             />
             <Column
               header={<Cell>CCY</Cell>}
               cell={props => <Cell {...props}>
-                {this.props.trades[props.rowIndex].pair}
+                {trades[props.rowIndex].pair}
               </Cell>}
               width={70}
             />
             <Column
               header={<Cell className='text-right'>Notional</Cell>}
-              cell={props => <MoneyCell className='text-right' data={this.props.trades} field='amount' {...props} />}
+              cell={props => <NotionalCell className='text-right' data={trades} field='amount' suffix={' ' + trades[props.rowIndex].pair.substr(0, 3)} {...props} />}
               width={120}
             />
             <Column
               header={<Cell className='text-right'>Rate</Cell>}
               cell={props => <Cell className='text-right' {...props}>
-                {this.props.trades[props.rowIndex].rate}
+                {trades[props.rowIndex].rate}
               </Cell>}
               width={80}
             />
             <Column
               header={<Cell>Status</Cell>}
               cell={props => <Cell className='trade-status' {...props}>
-                {this.props.trades[props.rowIndex].status}
+                {trades[props.rowIndex].status}
               </Cell>}
               width={80}
             />
             <Column
               header={<Cell>Value date</Cell>}
-              cell={props => <DateCell field='valueDate' prefix='SP. ' format='DD MMM' data={this.props.trades} {...props} />}
+              cell={props => <DateCell field='valueDate' prefix='SP. ' format='DD MMM' data={trades} {...props} />}
               width={100}
             />
             <Column
               header={<Cell>Trader</Cell>}
               cell={props => <Cell {...props}>
-                {this.props.trades[props.rowIndex].trader}
+                {trades[props.rowIndex].trader}
               </Cell>}
               width={80}
             />
