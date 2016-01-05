1362eb3823fc781d14bf9f8609a7bb099cd3cce9
tdeheurles <tdeheurles@gmail.com>
2016-01-05 17:28:24 +0100

    Remove false error from Populate Eventstore container build

--- deploy/docker/build/populatedEventstore/build.sh
+++ deploy/docker/build/populatedEventstore/build.sh
@@ -6,9 +6,10 @@ if [[ $build = "" ]];then
   exit 1
 fi
 
+set -euo pipefail
+
 # get config
 . ../../../config
-
 
 # run eventstore
 docker run -d --net=host $eventstoreContainer:$vEventstore > eventstore_id
@@ -16,27 +17,16 @@ echo "container $(cat eventstore_id) started"
 
 # populate it
 populateCommand=`cat "../../../../src/server/Populate Event Store.bat"`
-docker run -d --net=host                      \
+docker run -t --net=host                      \
      $serversContainer:$vMajor.$vMinor.$build \
      $populateCommand > populate_id
 echo "container $(cat populate_id) started"
 
-echo "============="
-echo "logs from $(cat eventstore_id):"
-echo " "
-docker logs `cat eventstore_id`
-echo " "
-echo "============="
-echo "logs from $(cat populate_id)"
-echo " "
-docker logs `cat populate_id`
-echo " "
-
-sleep 7 && docker kill `cat populate_id` 
-
 # commit container
+echo -e "\ncommit"
 docker commit `cat eventstore_id` $populatedEventstoreContainer:$vMajor.$vMinor.$build
 docker tag -f $populatedEventstoreContainer:$vMajor.$vMinor.$build $populatedEventstoreContainer:latest
 
 # stop eventstore
+echo -e "\nstop"
 docker kill `cat eventstore_id`
