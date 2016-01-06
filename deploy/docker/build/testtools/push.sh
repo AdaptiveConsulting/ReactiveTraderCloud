8d970ab107902d5438da7a28cf4168ca1869d851
tdeheurles <tdeheurles@gmail.com>
2016-01-06 18:50:43 +0100

    Update testtools tag system

--- deploy/docker/build/testtools/push.sh
+++ deploy/docker/build/testtools/push.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
+build=$1
+if [[ $build = "" ]];then
+  echo "servers-build: build number required as first parameter"
+  exit 1
+fi
+
 # fail fast
 set -euo pipefail
 
 . ../../../config
 
-# push
-docker push $testtoolsContainer:$vTesttools
+docker push $testtoolsContainer
+docker push $testtoolsContainer.$build
