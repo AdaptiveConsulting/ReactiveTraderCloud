397d843b142ec809d85eb1a39b39127570baad99
tdeheurles <tdeheurles@gmail.com>
2016-01-06 15:04:19 +0100

    Update crossbar and broker tag system

--- deploy/docker/build/crossbar/push.sh
+++ deploy/docker/build/crossbar/push.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
+build=$1
+if [[ $build = "" ]];then
+  echo "broker-build: build number required as first parameter"
+  exit 1
+fi
+
 # fail fast
 set -euo pipefail
 
-# get and control config
 . ../../../config
 
-# build
-docker push $crossbarContainer:$vCrossbar 
+docker push $crossbarContainer 
+docker push $crossbarContainer.$build 
