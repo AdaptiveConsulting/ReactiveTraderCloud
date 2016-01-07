e4be82b4c1f690ca4f81cf25f767063b7b0450bf
Keith Woods <devshop7@gmail.com>
2016-01-07 17:15:23 +0000

    fonts now get deployed to a fonts folder

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -92,11 +92,11 @@ const webpackConfig = {
         loader: 'json-loader'
       },
       /* eslint-disable */
-      { test: /\.woff(\?.*)?$/,  loader: "url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff" },
-      { test: /\.woff2(\?.*)?$/, loader: "url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2" },
-      { test: /\.ttf(\?.*)?$/,   loader: "url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream" },
-      { test: /\.eot(\?.*)?$/,   loader: "file-loader?prefix=fonts/&name=[path][name].[ext]" },
-      { test: /\.svg(\?.*)?$/,   loader: "url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml" }
+      { test: /\.woff(\?.*)?$/,  loader: "url-loader?prefix=fonts/&name=fonts/[name].[ext]&limit=10000&mimetype=application/font-woff" },
+      { test: /\.woff2(\?.*)?$/, loader: "url-loader?prefix=fonts/&name=fonts/[name].[ext]&limit=10000&mimetype=application/font-woff2" },
+      { test: /\.ttf(\?.*)?$/,   loader: "url-loader?prefix=fonts/&name=fonts/[name].[ext]&limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?.*)?$/,   loader: "file-loader?prefix=fonts/&name=fonts/[name].[ext]" },
+      { test: /\.svg(\?.*)?$/,   loader: "url-loader?prefix=fonts/&name=fonts/[name].[ext]&limit=10000&mimetype=image/svg+xml" }
       /* eslint-enable */
     ]
   },
