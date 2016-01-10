f93340291a8fd9ed3be4c0389bbe0ede8cce27ab
tdeheurles <tdeheurles@gmail.com>
2016-01-10 10:51:34 +0100

    Detail the e2e process

--- deploy/e2e/e2e.sh
+++ deploy/e2e/e2e.sh
@@ -8,6 +8,19 @@ fi
 # fail fast
 set -euo pipefail
 
+startTime=$(date)
+
 ./buildContainers $build_id
 ./runContainers   $build_id
+
+echo "Giving some time for services to start"
+sleep 10
+
 ./testContainers  $build_id
+./stopContainers
+
+echo " "
+echo "============="
+echo "Time details:"
+echo $startTime
+date
