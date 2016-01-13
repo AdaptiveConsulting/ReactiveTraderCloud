a46ca7d323cd0845d622244ec3ef00b4a93223ad
DimitarChristoff <christoff@gmail.com>
2016-01-13 11:18:14 +0000

    fixes #142 - build on mac due to sed gnu vs sed bsd versions, permissions tweak

--- deploy/docker/build/nsgate/build.sh
+++ deploy/docker/build/nsgate/build.sh
@@ -24,8 +24,8 @@ cp template.Dockerfile        ./build/Dockerfile
 cp template.updateServers.sh  ./build/updateServers.sh
 cp template.writeFunctions.sh ./build/writeFunctions.sh
 
-sed -i "s|__NGINX_CONTAINER__|$nginxContainer|g" ./build/Dockerfile
-sed -i "s/__DOMAIN__/$domainName/g" ./build/writeFunctions.sh
+sed -ie "s|__NGINX_CONTAINER__|$nginxContainer|g" ./build/Dockerfile
+sed -ie "s/__DOMAIN__/$domainName/g" ./build/writeFunctions.sh
 
 docker build --no-cache -t $nsGateContainer ./build/.
 docker tag -f $nsGateContainer $nsGateContainer.$build
