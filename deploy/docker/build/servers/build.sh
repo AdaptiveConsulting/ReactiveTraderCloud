b6d7e8e6a9166388f42a03093360df5e4a339df0
tdeheurles <tdeheurles@gmail.com>
2016-01-21 12:49:23 +0100

    change cache method for containers

--- deploy/docker/build/servers/build.sh
+++ deploy/docker/build/servers/build.sh
@@ -13,10 +13,13 @@ set -euo pipefail
 
 mkdir -p ./build
 
-# get source code
+# prepare cache
+dnupackage="dnupackage"
+docker volume create --name=$dnupackage
+
 cp -r ../../../../src/server ./build/
 
-cp  ./template.Dockerfile                      ./build/Dockerfile
+cp  ./template.Dockerfile                       ./build/Dockerfile
 sed -ie "s|__MONO_CONTAINER__|$monoContainer|g" ./build/Dockerfile
 sed -ie "s/__VDNX__/$vDnx/g"                    ./build/Dockerfile
 
@@ -24,13 +27,18 @@ sed -ie "s/__VDNX__/$vDnx/g"                    ./build/Dockerfile
 docker build --no-cache -t weareadaptive/serverssrc:$build ./build/.
 
 # restore package
-docker rm dnurestored || true
-buildCommand="mkdir -p /packages"
-buildCommand="$buildCommand && cp -r /packages /root/.dnx/"
-buildCommand="$buildCommand && dnu restore"
-buildCommand="$buildCommand && cp -r /root/.dnx/packages /"
-docker run -t --name dnurestored -v /$(pwd)/dnxcache:/packages weareadaptive/serverssrc:$build bash -c "$buildCommand"
+dnurestored="dnurestored"
+command="mkdir -p /packages"
+command="$command && cp -r /packages /root/.dnx/"
+command="$command && dnu restore"
+command="$command && cp -r /root/.dnx/packages /"
+docker rm $dnurestored || true
+docker run -t                     \
+  --name $dnurestored             \
+  -v $dnupackage:/packages        \
+  weareadaptive/serverssrc:$build \
+  bash -c "$command"
 
 # commit
-docker commit dnurestored $serversContainer
+docker commit $dnurestored $serversContainer
 docker tag -f $serversContainer $serversContainer.$build
