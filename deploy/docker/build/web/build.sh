0fe23693ee240c9fb051c490ecbdd5c6992b0d08
tdeheurles <tdeheurles@gmail.com>
2016-01-21 12:49:23 +0100

    change cache method for containers

--- deploy/docker/build/web/build.sh
+++ deploy/docker/build/web/build.sh
@@ -14,21 +14,26 @@ set -euo pipefail
 # Build the dist folder
 mkdir -p ./npminstall/build
 rm -rf ./npminstall/build/*
-cp npminstall/npminstall.sh        ./npminstall/build/npminstall.sh
 cp npminstall/template.Dockerfile  ./npminstall/build/Dockerfile
 cp -r ../../../../src/client/      ./npminstall/build/client
 
 sed -ie "s|__NODE_CONTAINER__|$nodeContainer|g" ./npminstall/build/Dockerfile
 
-docker build --no-cache -t weareadaptive/websrc:$build  ./npminstall/build/.
+tempContainer="weareadaptive/websrc:$build"
+docker build --no-cache -t $tempContainer ./npminstall/build/.
 
-# run the build container sharing the cache folder
-# the src are not directly shared as their is an error of synchronisation
-#   when node_modules tryied to be synced between container/VM and Host on windows
-docker run              \
-  -v /$(pwd)/.npm:/.npm \
-  -v /$(pwd)/dist:/dist \
-  weareadaptive/websrc:$build
+nodemodules="nodemodules"
+docker volume create --name=$nodemodules
+
+websrc="websrc"
+docker rm $websrc || true
+docker run                            \
+  --name $websrc                      \
+  -v nodemodules:/client/node_modules \
+  $tempContainer
+
+rm -r ./dist
+docker cp $websrc:/client/dist .
 
 # build nginx container
 mkdir -p ./nginx/build
