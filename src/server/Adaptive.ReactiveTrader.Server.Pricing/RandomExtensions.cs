02cd57e6f5c293499a82ea656eed5da1dcb5a4bc
James Watson <james@weareadaptive.com>
2016-04-22 10:04:15 +0100

    generate random number (0,1) rather than [0,1) to avoid Log blowing up

--- src/server/Adaptive.ReactiveTrader.Server.Pricing/RandomExtensions.cs
+++ src/server/Adaptive.ReactiveTrader.Server.Pricing/RandomExtensions.cs
@@ -6,7 +6,13 @@ namespace Adaptive.ReactiveTrader.Server.Pricing
     {
         public static double NextNormal(this Random random)
         {
-            return Math.Sqrt(-2d*Math.Log(random.NextDouble()))*Math.Sin(2d*Math.PI*random.NextDouble());
+            var r1 = random.NextDouble();
+            while (r1 < double.Epsilon)
+            {
+                r1 = random.NextDouble();
+            }
+
+            return Math.Sqrt(-2d*Math.Log(r1))*Math.Sin(2d*Math.PI*random.NextDouble());
         }
     }
 }
