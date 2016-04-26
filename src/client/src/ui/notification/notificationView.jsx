cf3d1edef8ec8a4e91007d915eca00e153e8f824
Elena Milliner <elena.milliner@ADAPTIVE.local>
2016-04-26 16:15:06 +0100

    #236 open fin notifications  - wip

--- src/client/src/ui/notification/notificationView.jsx
+++ src/client/src/ui/notification/notificationView.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Notification from './notification';
 import { TradeNotification } from '../../services/model';
 import './notification.scss';
+import { REJECTED, DONE} from '../../services/model/tradeNotification';
 
 export default class NotificationView extends React.Component {
 
@@ -12,17 +12,46 @@ export default class NotificationView extends React.Component {
   componentDidMount(){
     window.onNotificationMessage = (message) => this.handleMessage(message);
   }
-  
+
   handleMessage(message){
     this.setState({
       message: message
     });
   }
 
-  render(){
+  dismissNotification(){
+    window.fin.desktop.Notification.getCurrent().close();
+  }
+
+  render() {
+    let trade:TradeNotification = this.state.message;
+    let statusClassName = trade.tradeStatus == DONE ? 'notification__status-done' : 'notification__status-rejected';
+    let tradeDescriptionClassName = trade.tradeStatus === REJECTED ? 'notification__description-rejected' : '';
+    let secondCurrency = trade.dealtCurrency === trade.baseCurrency ? trade.termsCurrency : trade.baseCurrency;
     return (
       <div className='notification__container'>
-        <Notification message={this.state.message}/>
+        <div className='notification__title'>
+          <span>{trade.baseCurrency} / {trade.termsCurrency}</span>
+          <span className={statusClassName}>{trade.tradeStatus}</span>
+        </div>
+        <div className={tradeDescriptionClassName}>
+          <span className='notification__row-secondary'>{trade.direction} </span>
+          <span className='notification__row-primary'>{trade.dealtCurrency} </span>
+          <span className='notification__row-primary'>{trade.notional}</span>
+          <span className='notification__row-secondary'> vs </span>
+          <span className='notification__row-primary'>{secondCurrency}</span>
+          <span className='notification__row-secondary'> at </span>
+          <span className='notification__row-primary'>{trade.spotRate}</span>
+        </div>
+        <div>
+          <span className='notification__row-secondary'>Spot </span>
+          <span className='notification__row-primary'>{trade.valueDate}</span>
+        </div>
+        <div>
+          <span className='notification__row-secondary'>Trade ID </span>
+          <span className='notification__row-primary'>{trade.tradeId}</span>
+          <span className='notification__action-dismiss' onClick={() => this.dismissNotification()}>Done</span>
+        </div>
       </div>
     );
   }
