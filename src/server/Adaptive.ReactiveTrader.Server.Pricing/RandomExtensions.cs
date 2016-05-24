738c1984e6c3a7d59732217038ecc78dd297ee39
James Watson <james@weareadaptive.com>
2016-05-24 16:15:13 +0100

    use single scheduler with timing operators, protect random access

--- src/server/Adaptive.ReactiveTrader.Server.Pricing/RandomExtensions.cs
+++ src/server/Adaptive.ReactiveTrader.Server.Pricing/RandomExtensions.cs
@@ -7,11 +7,8 @@ namespace Adaptive.ReactiveTrader.Server.Pricing
         public static double NextNormal(this Random random)
         {
             var r1 = random.NextDouble();
-            while (r1 < double.Epsilon)
-            {
-                r1 = random.NextDouble();
-            }
-
+            if (r1 < double.Epsilon) r1 = 0.5;
+            
             return Math.Sqrt(-2d*Math.Log(r1))*Math.Sin(2d*Math.PI*random.NextDouble());
         }
     }
