19d7aec34825f594087d853f1b3a89cfce70a90f
Keith Woods <devshop7@gmail.com>
2016-09-02 20:25:37 +0100

    style fixes

--- src/client/src/ui/regions/views/analytics/analyticsRegionView.jsx
+++ src/client/src/ui/regions/views/analytics/analyticsRegionView.jsx
@@ -18,22 +18,23 @@ export default class AnalyticsRegionView extends SingleItemRegionView {
     let model:RegionModel = this.props.model;
 
     if (model.modelRegistrations.length === 1) {
-      innerContent = this._createRootElement(model, 'sidebar__content');
+      let className = classnames('analyticsRegion__content', { 'analyticsRegion__container--no-content' : !model.showContent});
+      innerContent = this._createRootElement(model, className);
     }
 
     let analyticsClassName = classnames (
-      'sidebar__element-button glyphicon glyphicon-stats',
+      'analyticsRegion__element-button glyphicon glyphicon-stats',
       {
-        'sidebar__element--active': innerContent !== null && model.showContent,
-        'sidebar__element--inactive' :  innerContent === null || !model.showContent
+        'analyticsRegion__element--active': innerContent !== null && model.showContent,
+        'analyticsRegion__element--inactive' :  innerContent === null || !model.showContent
       }
     );
     return (
       <div className={this.props.className}>
         {innerContent}
-        <div className='sidebar__container'>
+        <div className='analyticsRegion__container'>
           <i className={analyticsClassName} onClick={() => this.props.router.publishEvent(this.props.model.modelId, 'toggleShowContent', {})} />
-          <div className='sidebar__element'></div>
+          <div className='analyticsRegion__element'></div>
         </div>
       </div>
     );
