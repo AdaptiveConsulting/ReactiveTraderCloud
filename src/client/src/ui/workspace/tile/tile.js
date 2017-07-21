c558feeb9645c9365e5ece70e1c9cc6186f34719
Vasi Puica <vasi.puica@gmail.com>
2017-07-21 11:24:56 +0100

    added storybook

---
+++ src/client/src/ui/workspace/tile/tile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Tile extends Component {
+
+    render() {
+        return (
+            <div>Tile</div>
+        )
+    }
+
+}
+
+export default Tile
---
+++ src/client/stories/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button } from '@storybook/react/demo';
+
+import Tile from '../src/ui/workspace/tile/tile'
+
+// StoryBook example
+// storiesOf('Button', module)
+//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+//   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+
+storiesOf('Tile', module)
+  .add('simple tile', () => <Tile />)
