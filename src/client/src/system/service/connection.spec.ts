90a82ebf317227e8aa81fa9a8e287bc725cc6598
james-gray-adaptive <james.gray@weareadaptive.com>
2017-08-04 14:47:38 +0100

    chore: fix connection ui tests

--- src/client/src/system/service/connection.spec.ts
+++ src/client/src/system/service/connection.spec.ts
@@ -1,98 +1,77 @@
-// import * as Rx from 'rxjs/Rx';
-//
-// import StubAutobahnProxy from './autobahnConnectionProxyStub';
-// import Connection from '../../../src/system/service/connection';
-// import SchedulerService from '../../../src/system/schedulerService';
-// import ConnectionStatus from '../../../src/system/service/connectionStatus';
-//
-// describe('Connection', () => {
-//
-//   beforeEach(() => {
-//     _scheduler = new Rx.HistoricalScheduler(
-//       0, /* initial clock of 0 */
-//       comparer      /* comparer for determining order */
-//     );
-//     _stubAutobahnProxy = new StubAutobahnProxy();
-//     _connection = new Connection('user', _stubAutobahnProxy, new SchedulerService());
-//     _receivedStatusUpdates = [];
-//     _connection.connectionStatusStream.subscribe(isConnected => {
-//       _receivedStatusUpdates.push(isConnected);
-//     });
-//   });
-//
-//   test('subscribes to autobahn open on connect()', () => {
-//     _connection.connect();
-//     expect(_stubAutobahnProxy.onOpenCallbacks.length).toEqual(1);
-//   });
-//
-//   test('subscribes to autobahn close on connect()', () => {
-//     _connection.connect();
-//     expect(_stubAutobahnProxy.onCloseCallbacks.length).toEqual(1);
-//   });
-//
-//   test('only opens underlying once when you call .connect()', () => {
-//     _connection.connect();
-//     _connection.connect();
-//     _connection.connect();
-//     expect(_stubAutobahnProxy.onOpenCallbacks.length).toEqual(1);
-//   });
-//
-//   test('pumps connection status of idle on initial connect before open', () => {
-//     expect(_receivedStatusUpdates.length).toEqual(1);
-//     expect(_receivedStatusUpdates).toEqual([ConnectionStatus.idle]);
-//   });
-//
-//   test('pumps connection status of connected when connection comes up', () => {
-//     _connection.connect();
-//
-//     _stubAutobahnProxy.setIsConnected(true);
-//     expect(_receivedStatusUpdates.length).toEqual(2);
-//     expect(_receivedStatusUpdates).toEqual([
-//       ConnectionStatus.idle,
-//       ConnectionStatus.connected
-//     ]);
-//   });
-//
-//   test('pumps connection status of disconnected when connection goes down', () => {
-//     _connection.connect();
-//     _stubAutobahnProxy.setIsConnected(true);
-//     _stubAutobahnProxy.setIsConnected(false);
-//     expect(_receivedStatusUpdates.length).toEqual(3);
-//     expect(_receivedStatusUpdates).toEqual([
-//       ConnectionStatus.idle,
-//       ConnectionStatus.connected,
-//       ConnectionStatus.disconnected
-//     ]);
-//   });
-//
-//   test('errors if called before session is set', () => {
-//     var streamYieldCount = 0, receivedError;
-//     _connection.subscribeToTopic('status').subscribe(_ => {
-//       streamYieldCount++;
-//     }, ex => {
-//       receivedError = ex;
-//     });
-//     expect(streamYieldCount).toEqual(0);
-//     expect(receivedError).toBeDefined();
-//     expect(receivedError.error).toEqual('Session not connected, can\'t subscribe to topic [status]');
-//   });
-// });
-//
-// function comparer(x, y) {
-//   if (x > y) {
-//     return 1;
-//   }
-//   if (x < y) {
-//     return -1;
-//   }
-//   return 0;
-// }
-//
-// let _stubAutobahnProxy,
-//   _connection,
-//   _receivedStatusUpdates;
-//
-// let _scheduler = new Rx.HistoricalScheduler(
-//   0,
-//   comparer
-// );
+import StubAutobahnProxy from './autobahnConnectionProxyStub';
+import Connection from '../../../src/system/service/connection';
+import SchedulerService from '../../../src/system/schedulerService';
+import ConnectionStatus from '../../../src/system/service/connectionStatus';
+
+describe('Connection', () => {
+  
+  let _stubAutobahnProxy,
+  _connection,
+  _receivedStatusUpdates;
+
+  beforeEach(() => {
+    _stubAutobahnProxy = new StubAutobahnProxy();
+    _connection = new Connection('user', _stubAutobahnProxy, new SchedulerService());
+    _receivedStatusUpdates = [];
+    _connection.connectionStatusStream.subscribe(isConnected => {
+      _receivedStatusUpdates.push(isConnected);
+    });
+  });
+
+  test('subscribes to autobahn open on connect()', () => {
+    _connection.connect();
+    expect(_stubAutobahnProxy.onOpenCallbacks.length).toEqual(1);
+  });
+
+  test('subscribes to autobahn close on connect()', () => {
+    _connection.connect();
+    expect(_stubAutobahnProxy.onCloseCallbacks.length).toEqual(1);
+  });
+
+  test('only opens underlying once when you call .connect()', () => {
+    _connection.connect();
+    _connection.connect();
+    _connection.connect();
+    expect(_stubAutobahnProxy.onOpenCallbacks.length).toEqual(1);
+  });
+
+  test('pumps connection status of idle on initial connect before open', () => {
+    expect(_receivedStatusUpdates.length).toEqual(1);
+    expect(_receivedStatusUpdates).toEqual([ConnectionStatus.idle]);
+  });
+
+  test('pumps connection status of connected when connection comes up', () => {
+    _connection.connect();
+
+    _stubAutobahnProxy.setIsConnected(true);
+    expect(_receivedStatusUpdates.length).toEqual(2);
+    expect(_receivedStatusUpdates).toEqual([
+      ConnectionStatus.idle,
+      ConnectionStatus.connected
+    ]);
+  });
+
+  test('pumps connection status of disconnected when connection goes down', () => {
+    _connection.connect();
+    _stubAutobahnProxy.setIsConnected(true);
+    _stubAutobahnProxy.setIsConnected(false);
+    expect(_receivedStatusUpdates.length).toEqual(3);
+    expect(_receivedStatusUpdates).toEqual([
+      ConnectionStatus.idle,
+      ConnectionStatus.connected,
+      ConnectionStatus.disconnected
+    ]);
+  });
+
+  test('errors if called before session is set', () => {
+    var streamYieldCount = 0, receivedError;
+    _connection.subscribeToTopic('status').subscribe(_ => {
+      streamYieldCount++;
+    }, ex => {
+      receivedError = ex;
+    });
+    expect(streamYieldCount).toEqual(0);
+    expect(receivedError).toBeDefined();
+    expect(receivedError.error).toEqual('Session not connected, can\'t subscribe to topic [status]');
+  });
+});
