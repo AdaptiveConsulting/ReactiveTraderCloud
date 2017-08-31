de9b32278e9b0345f4d469c15fb75724f4622149
Pawel Misiurski <pawel@weareadaptive.com>
2017-08-31 11:52:49 +0100

    rate test fixed

---
+++ src/client/src/redux/spotTile/__tests__/toRate.spec.js
@@ -0,0 +1,44 @@
+import { toRate } from '../spotTileOperations'
+
+describe('Rate Tests', () => {
+
+  describe('rate precision of 4', () => {
+    let rate
+
+    beforeEach(() => {
+      rate = toRate(1.07451, 5, 4) // EURUSD like rate and precision
+    })
+
+    test('extracts the big figure', () => {
+      expect(rate.bigFigure).toEqual(1.07)
+    })
+
+    test('extracts the pips', () => {
+      expect(rate.pips).toEqual(45)
+    })
+
+    test('extracts the pip fraction', () => {
+      expect(rate.pipFraction).toEqual(1)
+    })
+  })
+
+  describe('rate precision of 3', () => {
+    let rate
+
+    beforeEach(() => {
+      rate = toRate(183.794, 3, 2)  // GBPJPY like rate and precision
+    })
+
+    test('extracts the big figure', () => {
+      expect(rate.bigFigure).toEqual(183)
+    })
+
+    test('extracts the pips', () => {
+      expect(rate.pips).toEqual(79)
+    })
+
+    test('extracts the pip fraction', () => {
+      expect(rate.pipFraction).toEqual(4)
+    })
+  })
+})
--- src/client/src/redux/spotTile/spotTileOperations.ts
+++ src/client/src/redux/spotTile/spotTileOperations.ts
@@ -189,7 +189,7 @@ function getSpread(bid: number, ask: number, pipsPosition: number, ratePrecision
   }
 }
 
-function toRate(rawRate: number, ratePrecision: number, pipPrecision: number): Rate {
+export function toRate(rawRate: number, ratePrecision: number, pipPrecision: number): Rate {
   const rateString = rawRate.toFixed(ratePrecision)
   const priceParts = rateString.split('.')
   const wholeNumber = priceParts[0]
@@ -223,51 +223,3 @@ function buildNotification(trade, error) {
     tradeId: trade.tradeId,
   }
 }
-
-// Rate spec for former object, now must be converted for toRate function
-// import Rate from '../../../src/services/model/rate'
-
-// describe('Rate Tests', () => {
-
-//   describe('rate precision of 4', () => {
-//     let rate
-
-//     beforeEach(() => {
-//       rate = new Rate(1.07451, 5, 4) // EURUSD like rate and precision
-//     })
-
-//     test('extracts the big figure', () => {
-//       expect(rate.bigFigure).toEqual(1.07)
-//     })
-
-//     test('extracts the pips', () => {
-//       expect(rate.pips).toEqual(45)
-//     })
-
-//     test('extracts the pip fraction', () => {
-//       expect(rate.pipFraction).toEqual(1)
-//     })
-//   })
-
-//   describe('rate precision of 3', () => {
-//     let rate
-
-//     beforeEach(() => {
-//       rate = new Rate(183.794, 3, 2)  // GBPJPY like rate and precision
-//     })
-
-//     test('extracts the big figure', () => {
-//       expect(rate.bigFigure).toEqual(183)
-//     })
-
-//     test('extracts the pips', () => {
-//       expect(rate.pips).toEqual(79)
-//     })
-
-//     test('extracts the pip fraction', () => {
-//       expect(rate.pipFraction).toEqual(4)
-//     })
-//   })
-// })
-
-
