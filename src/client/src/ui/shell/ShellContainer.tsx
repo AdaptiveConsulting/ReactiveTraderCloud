522122750a57932dbed14ffc4236e2f60089f0e1
james-gray-adaptive <james.gray@weareadaptive.com>
2017-09-07 15:33:39 +0100

    chore: fix session expired modal

--- src/client/src/ui/shell/ShellContainer.tsx
+++ src/client/src/ui/shell/ShellContainer.tsx
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux'
 import { reconnect } from './actions'
 import Shell from './Shell'
+import { ConnectionStatus } from '../../types/connectionStatus';
 
 function mapStateToProps({ connectionStatus }) {
-  const sessionExpired = connectionStatus.connection !== 'connected' && connectionStatus.connection !== 'idle'
+  const sessionExpired = connectionStatus.connection === ConnectionStatus.sessionExpired
   return { sessionExpired }
 }
 
