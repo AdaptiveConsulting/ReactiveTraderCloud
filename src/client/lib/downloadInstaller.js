34115a325b70d2471b6e1b0d7e73294aed0e1a2c
Pawel Misiurski <pawel@weareadaptive.com>
2017-09-08 15:28:31 +0100

    use demo.app.json when building installer

--- src/client/lib/downloadInstaller.js
+++ src/client/lib/downloadInstaller.js
@@ -2,7 +2,7 @@ const https = require('https')
 const fs = require('fs')
 
 const fileName = 'ReactiveTraderInstaller'
-const appJSONUrl = 'https://raw.githubusercontent.com/AdaptiveConsulting/ReactiveTraderCloud/feature/refactor/src/client/config/openfin/2084.app.json'
+const appJSONUrl = 'https://raw.githubusercontent.com/AdaptiveConsulting/ReactiveTraderCloud/feature/refactor/src/client/config/openfin/demo.app.json'
 
 const file = fs.createWriteStream('install/ReactiveTraderCloud-demo.zip')
 const request = https.get(`https://dl.openfin.co/services/download?fileName=${fileName}&config=${appJSONUrl}`, function(response) {
