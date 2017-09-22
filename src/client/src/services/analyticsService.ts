110633243c0c9708ee15749b5f6f0c14bcace370
Mustafa Celik <celikmus@gmail.com>
2017-09-22 12:31:09 +0100

    appBootstrapper enhancements: * Clean-up of 'new's * Moved connection concerns to connection.ts * Removed Connection's parent: DisposableBase * Moved serviceType and connect() into respective services.

--- src/client/src/services/analyticsService.ts
+++ src/client/src/services/analyticsService.ts
@@ -3,29 +3,33 @@ import { ServiceBase } from '../system/service'
 import { PositionsMapper } from './mappers'
 import { Guard, logger, RetryPolicy } from '../system'
 import '../system/observableExtensions/retryPolicyExt'
+import { ServiceConst } from '../types'
 
 const log = logger.create('AnalyticsService')
 
 export default class AnalyticsService extends ServiceBase {
   positionsMapper: any
 
-  constructor(serviceType, connection, referenceDataService) {
-    super(serviceType, connection)
+  constructor(connection, referenceDataService) {
+    super(ServiceConst.AnalyticsServiceKey, connection)
     this.positionsMapper = new PositionsMapper(referenceDataService)
+    this.connect()
   }
 
   getAnalyticsStream(analyticsRequest) {
     Guard.isDefined(analyticsRequest, 'analyticsRequest required')
-    return Observable.create(
-      (o) => {
-        log.debug('Subscribing to analytics stream')
+    return Observable.create(o => {
+      log.debug('Subscribing to analytics stream')
 
-        return this.serviceClient
-          .createStreamOperation('getAnalytics', analyticsRequest)
-          .retryWithPolicy(RetryPolicy.backoffTo10SecondsMax, 'getAnalytics', Scheduler.async)
-          .map(dto => this.positionsMapper.mapFromDto(dto))
-          .subscribe(o)
-      },
-    )
+      return this.serviceClient
+        .createStreamOperation('getAnalytics', analyticsRequest)
+        .retryWithPolicy(
+          RetryPolicy.backoffTo10SecondsMax,
+          'getAnalytics',
+          Scheduler.async
+        )
+        .map(dto => this.positionsMapper.mapFromDto(dto))
+        .subscribe(o)
+    })
   }
 }
--- src/client/src/services/referenceDataService.ts
+++ src/client/src/services/referenceDataService.ts
@@ -1,7 +1,7 @@
 import * as _ from 'lodash'
 import { Observable, Scheduler } from 'rxjs/Rx'
 import { ReferenceDataMapper } from './mappers'
-import { ConnectionStatus, UpdateType } from '../types'
+import { ConnectionStatus, UpdateType, ServiceConst } from '../types'
 import { logger, RetryPolicy } from '../system'
 import { ServiceBase } from '../system/service'
 import '../system/observableExtensions/retryPolicyExt'
@@ -12,30 +12,35 @@ export default class ReferenceDataService extends ServiceBase {
   referenceDataStreamConnectable
   currencyPairCache
 
-  constructor(serviceType, connection) {
-    super(serviceType, connection)
+  constructor(connection) {
+    super(ServiceConst.ReferenceServiceKey, connection)
     this.referenceDataMapper = new ReferenceDataMapper()
     this.referenceDataStreamConnectable = this.referenceDataStream().publish()
     this.currencyPairCache = {
-      hasLoaded:false,
+      hasLoaded: false
     }
 
     // on connection/reconnection get reference data stream
-    connection.connectionStatusStream.filter(el => el === ConnectionStatus.connected).subscribe(() => {
-      // TEMP force refdata reconnecting
-      this.serviceClient.isConnectCalled = false
-      this.referenceDataStreamConnectable.connection = null
+    connection.connectionStatusStream
+      .filter(el => el === ConnectionStatus.connected)
+      .subscribe(() => {
+        // TEMP force refdata reconnecting
+        this.serviceClient.isConnectCalled = false
+        this.referenceDataStreamConnectable.connection = null
 
-      this.addDisposable(this.referenceDataStreamConnectable.connect())
-    })
+        this.addDisposable(this.referenceDataStreamConnectable.connect())
+      })
+    this.connect()
   }
 
   getCurrencyPair(symbol) {
     if (!this.currencyPairCache.hasLoaded) {
-      throw new Error('Reference data cache hasn\'t finished loading')
+      throw new Error("Reference data cache hasn't finished loading")
     }
     if (!this.currencyPairCache.hasOwnProperty(symbol)) {
-      throw new Error(`CurrencyPair with symbol [${symbol}] is not in the cache.`)
+      throw new Error(
+        `CurrencyPair with symbol [${symbol}] is not in the cache.`
+      )
     }
     return this.currencyPairCache[symbol]
   }
@@ -45,43 +50,51 @@ export default class ReferenceDataService extends ServiceBase {
   }
 
   referenceDataStream() {
-    return Observable.create(
-      (o) => {
-        log.debug('Subscribing reference data stream')
-        return this.serviceClient
-          .createStreamOperation('getCurrencyPairUpdatesStream', { /* noop request */ })
-          .retryWithPolicy(RetryPolicy.backoffTo10SecondsMax, 'getCurrencyPairUpdatesStream', Scheduler.async)
-          .map(data => this.referenceDataMapper.mapCurrencyPairsFromDto(data))
-          .subscribe(
-            (updates) => {
-              // note : we have a side effect here.
-              // In this instance it's ok as this stream is published and ref counted, i.e. there is only ever 1
-              // and this services is designed to be run at startup and other calls should block until it's loaded.
-              // The intent here is all reference data should be exposed via both a synchronous and push API.
-              // Push only (i.e. Observable only) APIs within applications for data that is effectively already known are a pain to work with.
-              this.updateCache(updates)
-              o.next(updates)
-            },
-            (err) => {
-              o.error(err)
-            },
-            () => {
-              o.complete()
-            },
-          )
-      },
-    )
+    return Observable.create(o => {
+      log.debug('Subscribing reference data stream')
+      return this.serviceClient
+        .createStreamOperation('getCurrencyPairUpdatesStream', {
+          /* noop request */
+        })
+        .retryWithPolicy(
+          RetryPolicy.backoffTo10SecondsMax,
+          'getCurrencyPairUpdatesStream',
+          Scheduler.async
+        )
+        .map(data => this.referenceDataMapper.mapCurrencyPairsFromDto(data))
+        .subscribe(
+          updates => {
+            // note : we have a side effect here.
+            // In this instance it's ok as this stream is published and ref counted, i.e. there is only ever 1
+            // and this services is designed to be run at startup and other calls should block until it's loaded.
+            // The intent here is all reference data should be exposed via both a synchronous and push API.
+            // Push only (i.e. Observable only) APIs within applications for data that is effectively already known are a pain to work with.
+            this.updateCache(updates)
+            o.next(updates)
+          },
+          err => {
+            o.error(err)
+          },
+          () => {
+            o.complete()
+          }
+        )
+    })
   }
 
   updateCache(update) {
-    _.forEach(update.currencyPairUpdates, (currencyPairUpdate) => {
+    _.forEach(update.currencyPairUpdates, currencyPairUpdate => {
       if (currencyPairUpdate.updateType === UpdateType.Added) {
-        this.currencyPairCache[currencyPairUpdate.currencyPair.symbol] = currencyPairUpdate.currencyPair
+        this.currencyPairCache[currencyPairUpdate.currencyPair.symbol] =
+          currencyPairUpdate.currencyPair
       } else if (currencyPairUpdate.updateType === UpdateType.Removed) {
         delete this.currencyPairCache[currencyPairUpdate.currencyPair.symbol]
       }
     })
-    if (!this.currencyPairCache.hasLoaded && update.currencyPairUpdates.length > 0) {
+    if (
+      !this.currencyPairCache.hasLoaded &&
+      update.currencyPairUpdates.length > 0
+    ) {
       this.currencyPairCache.hasLoaded = true
     }
   }
