1182744e60750d80f5525e8e04306980a6b4e15a
Mustafa Celik <celikmus@gmail.com>
2017-09-26 10:19:55 +0100

    Added connection property to the compositeStatusService, ie. fixes the issue with reconnect.

--- src/client/src/services/compositeStatusService.ts
+++ src/client/src/services/compositeStatusService.ts
@@ -79,7 +79,8 @@ export default function compositeStatusService(
     get serviceStatus() {
       return currentServiceStatusLookup
     },
-    serviceStatusStream
+    serviceStatusStream,
+    connection
   }
 }
 
