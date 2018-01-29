4f17c7325adaef0d405e676346f4a3238c3fd99e
Silviu <silviu.huzum@weareadaptive.com>
2018-01-29 11:50:05 +0000

    Reduced indentation.

--- src/client/src/services/pricingService.ts
+++ src/client/src/services/pricingService.ts
@@ -4,42 +4,46 @@ import { logger, RetryPolicy } from '../system'
 import '../system/observableExtensions/retryPolicyExt'
 
 const log = logger.create('PricingService')
+const getPriceUpdatesOperationName = 'getPriceUpdates'
 
 export default class PricingService extends ServiceBase {
   cachedObservablesBySymbol = {}
 
+  private createSpotPriceStream(request) {
+    return Observable.create(
+      (o) => {
+        log.debug(`Subscribing to spot price stream for [${request.symbol}]`)
+        let lastPrice = null
+        return this.serviceClient
+          .createStreamOperation(getPriceUpdatesOperationName, request)
+          // we retry the price stream forever, if it errors (likely connection down) we pump a non tradable price
+          .retryWithPolicy(
+            RetryPolicy.indefiniteEvery2Seconds,
+            getPriceUpdatesOperationName,
+            Scheduler.async,
+            (err, willRetry) => {
+              if (willRetry && lastPrice !== null) {
+                // TODO: remove is stale price fully supported
+              }
+            },
+          )
+          .map(item => keysToLower(item))
+          .subscribe(
+            (price) => {
+              lastPrice = price
+              o.next(price)
+            },
+            (err) => { o.error(err) },
+            () => { o.complete() },
+          )
+      },
+    ).share()
+  }
+
   getSpotPriceStream(request) {
     const { symbol } = request
     if (!this.cachedObservablesBySymbol[symbol]) {
-      const getPriceUpdatesOperationName = 'getPriceUpdates'
-      this.cachedObservablesBySymbol[symbol] = Observable.create(
-        (o) => {
-          log.debug(`Subscribing to spot price stream for [${request.symbol}]`)
-          let lastPrice = null
-          return this.serviceClient
-            .createStreamOperation(getPriceUpdatesOperationName, request)
-            // we retry the price stream forever, if it errors (likely connection down) we pump a non tradable price
-            .retryWithPolicy(
-              RetryPolicy.indefiniteEvery2Seconds,
-              getPriceUpdatesOperationName,
-              Scheduler.async,
-              (err, willRetry) => {
-                if (willRetry && lastPrice !== null) {
-                  // TODO: remove is stale price fully supported
-                }
-              },
-            )
-            .map(item => keysToLower(item))
-            .subscribe(
-              (price) => {
-                lastPrice = price
-                o.next(price)
-              },
-              (err) => { o.error(err) },
-              () => { o.complete() },
-            )
-        },
-      ).share()
+      this.cachedObservablesBySymbol[symbol] = this.createSpotPriceStream(request)
     }
     return this.cachedObservablesBySymbol[symbol]
   }
