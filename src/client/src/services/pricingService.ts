c54f28907c6b505a4abd5fa95cc980bdd3f49523
Silviu <silviu.huzum@weareadaptive.com>
2018-01-29 17:23:05 +0000

    Made the pricingService easier to read.

--- src/client/src/services/pricingService.ts
+++ src/client/src/services/pricingService.ts
@@ -5,77 +5,75 @@ import { logger, RetryPolicy } from '../system'
 import '../system/observableExtensions/retryPolicyExt'
 
 const log = logger.create('PricingService')
+const getPriceUpdatesOperationName = 'getPriceUpdates'
 
 interface Request {
   symbol: string,
 }
 
+const createSpotPriceStream = (serviceClient, request: Request) => {
+  const observable = Observable.create(clientObserver => {
+    log.debug(`Subscribing to spot price stream for [${request.symbol}]`)
+    let lastPrice = null
+    const innerObserver = {
+      next: price => {
+        lastPrice = price
+        clientObserver.next(price)
+      },
+      error: err => {
+        clientObserver.error(err)
+      },
+      complete: () => {
+        clientObserver.complete()
+      }
+    }
+    const retryWithPolicyErrorCb = (err, willRetry) => {
+      if (willRetry && lastPrice !== null) {
+        // TODO: remove is stale price fully supported
+      }
+    }
+    const retryWithPolicyArgs = [
+      RetryPolicy.indefiniteEvery2Seconds,
+      getPriceUpdatesOperationName,
+      Scheduler.async,
+      retryWithPolicyErrorCb,
+    ]
+    const subscription = serviceClient
+      .createStreamOperation(getPriceUpdatesOperationName, request)
+      // we retry the price stream forever, if it errors (likely connection down) we pump a non tradable price
+      .retryWithPolicy(...retryWithPolicyArgs)
+      .map(adaptDTO)
+      .subscribe(innerObserver)
+    return subscription
+  })
+  return observable.share()
+}
+
 export default function createPricingService(connection) {
-  const getPriceUpdatesOperationName = 'getPriceUpdates'
-  const cachedObservablesBySymbol = {}
-  const serviceClient = new ServiceClient(
-    ServiceConst.PricingServiceKey,
-    connection
-  )
-  const createSpotPriceStream = (request: Request) => {
-    const observable = Observable.create(clientObserver => {
-      log.debug(`Subscribing to spot price stream for [${request.symbol}]`)
-      let lastPrice = null
-      const innerObserver = {
-        next: price => {
-          lastPrice = price
-          clientObserver.next(price)
-        },
-        error: err => {
-          clientObserver.error(err)
-        },
-        complete: () => {
-          clientObserver.complete()
-        }
-      }
-      const retryWithPolicyErrorCb = (err, willRetry) => {
-        if (willRetry && lastPrice !== null) {
-          // TODO: remove is stale price fully supported
-        }
-      }
-      const retryWithPolicyArgs = [
-        RetryPolicy.indefiniteEvery2Seconds,
-        getPriceUpdatesOperationName,
-        Scheduler.async,
-        retryWithPolicyErrorCb,
-      ]
-      const subscription = serviceClient
-        .createStreamOperation(getPriceUpdatesOperationName, request)
-        // we retry the price stream forever, if it errors (likely connection down) we pump a non tradable price
-        .retryWithPolicy(...retryWithPolicyArgs)
-        .map(adaptDTO)
-        .subscribe(innerObserver)
-      return subscription
-    })
-    return observable.share()
-  }
+  const cachedSpotStreamBySymbol = {}
+  const serviceClient = new ServiceClient(ServiceConst.PricingServiceKey, connection)
   serviceClient.connect()
   return {
     get serviceStatusStream() {
       return serviceClient.serviceStatusStream
     },
-    getSpotPriceStream(request:Request) {
+    getSpotPriceStream(request: Request) {
       const { symbol } = request
-      if (!cachedObservablesBySymbol[symbol]) {
-        cachedObservablesBySymbol[symbol] = createSpotPriceStream(request)
+      if (!cachedSpotStreamBySymbol[symbol]) {
+        cachedSpotStreamBySymbol[symbol] = createSpotPriceStream(serviceClient, request)
       }
-      return cachedObservablesBySymbol[symbol]
+      return cachedSpotStreamBySymbol[symbol]
     }
   }
 }
 
-function adaptDTO(object) {
+function adaptDTO(dto) {
   return {
-    ask: object.Ask,
-    bid: object.Bid,
-    mid: object.Mid,
-    creationTimestamp: object.CreationTimestamp,
-    symbol: object.Symbol,
-    valueDate: object.ValueDate
+    ask: dto.Ask,
+    bid: dto.Bid,
+    mid: dto.Mid,
+    creationTimestamp: dto.CreationTimestamp,
+    symbol: dto.Symbol,
+    valueDate: dto.ValueDate
   }
 }
