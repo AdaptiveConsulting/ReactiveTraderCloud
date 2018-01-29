154da80dc1f69dd562702d30b09efaed6f9281a0
Silviu <silviu.huzum@weareadaptive.com>
2018-01-29 16:47:34 +0000

    Fixes "Client subscribes twice to every currency pair #610" because of the previous conflicts.

--- src/client/src/services/pricingService.ts
+++ src/client/src/services/pricingService.ts
@@ -7,49 +7,60 @@ import '../system/observableExtensions/retryPolicyExt'
 const log = logger.create('PricingService')
 
 export default function createPricingService(connection) {
+  const getPriceUpdatesOperationName = 'getPriceUpdates'
+  const cachedObservablesBySymbol = {}
   const serviceClient = new ServiceClient(
     ServiceConst.PricingServiceKey,
     connection
   )
+  const createSpotPriceStream = request => {
+    return Observable.create(o => {
+      log.debug(`Subscribing to spot price stream for [${request.symbol}]`)
+      let lastPrice = null
+      return (
+        serviceClient
+          .createStreamOperation(getPriceUpdatesOperationName, request)
+          // we retry the price stream forever, if it errors (likely connection down) we pump a non tradable price
+          .retryWithPolicy(
+            RetryPolicy.indefiniteEvery2Seconds,
+            getPriceUpdatesOperationName,
+            Scheduler.async,
+            (err, willRetry) => {
+              if (willRetry && lastPrice !== null) {
+                // TODO: remove is stale price fully supported
+              }
+            }
+          )
+          .map(keysToLower)
+          .subscribe(
+            price => {
+              lastPrice = price
+              o.next(price)
+            },
+            err => {
+              o.error(err)
+            },
+            () => {
+              o.complete()
+            }
+          )
+      )
+    }).share();
+  }
   serviceClient.connect()
   return {
     get serviceStatusStream() {
       return serviceClient.serviceStatusStream
     },
     getSpotPriceStream(request) {
-      const getPriceUpdatesOperationName = 'getPriceUpdates'
-      return Observable.create(o => {
-        log.debug(`Subscribing to spot price stream for [${request.symbol}]`)
-        let lastPrice = null
-        return (
-          serviceClient
-            .createStreamOperation(getPriceUpdatesOperationName, request)
-            // we retry the price stream forever, if it errors (likely connection down) we pump a non tradable price
-            .retryWithPolicy(
-              RetryPolicy.indefiniteEvery2Seconds,
-              getPriceUpdatesOperationName,
-              Scheduler.async,
-              (err, willRetry) => {
-                if (willRetry && lastPrice !== null) {
-                  // TODO: remove is stale price fully supported
-                }
-              }
-            )
-            .map(item => keysToLower(item))
-            .subscribe(
-              price => {
-                lastPrice = price
-                o.next(price)
-              },
-              err => {
-                o.error(err)
-              },
-              () => {
-                o.complete()
-              }
-            )
-        )
-      })
+      const { symbol } = request
+      if(symbol === `EURUSD`){
+        debugger
+      }
+      if (!cachedObservablesBySymbol[symbol]) {
+        cachedObservablesBySymbol[symbol] = createSpotPriceStream(request)
+      }
+      return cachedObservablesBySymbol[symbol]
     }
   }
 }
