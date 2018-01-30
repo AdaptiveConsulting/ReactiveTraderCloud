e748d858f974b7779512d5e1bea04842944c549e
Silviu <silviu.huzum@weareadaptive.com>
2018-01-30 10:22:38 +0000

    Adds types and minor docs to the referenceOperations epic/reducer.

--- src/client/src/referenceOperations.ts
+++ src/client/src/referenceOperations.ts
@@ -2,17 +2,37 @@ import { createAction } from 'redux-actions'
 import * as keyBy from 'lodash.keyby'
 
 export enum ACTION_TYPES {
+  /**
+   * Looks like is like an init action that is only dispatched on init.
+   */
   REFERENCE_SERVICE = '@ReactiveTraderCloud/REFERENCE_SERVICE',
 }
 
-export const fetchReference = createAction(ACTION_TYPES.REFERENCE_SERVICE)
+export const createReferenceServiceAction = createAction(ACTION_TYPES.REFERENCE_SERVICE)
 
 export const referenceServiceEpic = refService$ => action$ => {
   return refService$.getCurrencyPairUpdatesStream()
-    .map(fetchReference)
+    .map(createReferenceServiceAction)
 }
 
-export const referenceServiceReducer = (state = {}, action) => {
+interface CurrencyPair {
+  symbol: string,
+  ratePrecision: number,
+  pipsPosition: number,
+  base: string,
+  terms: string,
+}
+
+interface CurrencyPairWrapper {
+  updateType: string,
+  currencyPair: CurrencyPair
+}
+
+interface State {
+  [id: string]: CurrencyPairWrapper
+}
+
+export const referenceServiceReducer = (state: State = {}, action): State => {
   switch (action.type) {
     case ACTION_TYPES.REFERENCE_SERVICE:
       return keyBy(action.payload.currencyPairUpdates, 'currencyPair.symbol')
