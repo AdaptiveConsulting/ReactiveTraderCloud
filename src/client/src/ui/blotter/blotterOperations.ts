e9109206479eca227b4fdf888a63b093734ec409
silviuadaptive <35812387+silviuadaptive@users.noreply.github.com>
2018-02-01 11:33:14 +0000

    Rxjs refactor: reference data service (#634)
    * Cleaned blotterOperations.
    
    * Removed @todo.

--- src/client/src/ui/blotter/blotterOperations.ts
+++ src/client/src/ui/blotter/blotterOperations.ts
@@ -1,14 +1,15 @@
-import * as _ from 'lodash'
 import { createAction } from 'redux-actions'
 
 import { regionsSettings } from '../../regions/regionsOperations'
 import { ACTION_TYPES as REF_ACTION_TYPES } from '../../referenceOperations'
+import { Trade } from '../../types'
+import * as keyBy from 'lodash.keyby'
 
 export const ACTION_TYPES = {
-  BLOTTER_SERVICE: '@ReactiveTraderCloud/BLOTTER_SERVICE',
+  BLOTTER_SERVICE_NEW_TRADES: '@ReactiveTraderCloud/BLOTTER_SERVICE_NEW_TRADES',
 }
 
-export const fetchBlotter = createAction(ACTION_TYPES.BLOTTER_SERVICE)
+export const createNewTradesAction = createAction(ACTION_TYPES.BLOTTER_SERVICE_NEW_TRADES)
 
 const subscribeOpenFinToBlotterData = (openFin, store) => () => {
   const cb = (msg, uuid) => openFin.sendAllBlotterData(uuid, store.getState().blotterService.trades)
@@ -19,27 +20,33 @@ export const blotterServiceEpic = (blotterService$, openFin) => (action$, store)
   return action$.ofType(REF_ACTION_TYPES.REFERENCE_SERVICE)
     .do(subscribeOpenFinToBlotterData(openFin, store))
     .flatMapTo(blotterService$.getTradesStream())
-    .map(fetchBlotter)
+    .map(createNewTradesAction)
 }
 
 export const blotterRegionsSettings = regionsSettings('Blotter', 850, 250, false)
 
-export const blotterServiceReducer = (state: any = {}, action) => {
-  switch (action.type) {
-    case ACTION_TYPES.BLOTTER_SERVICE:
-      const newState = state
-      const payloadTrades = action.payload.trades
+interface Trades {
+  [tradeId: number]: Trade
+}
 
-      // removing the existing key for a single trade
-      if (payloadTrades.length === 1) {
-        const tradeKey = payloadTrades[0]['tradeId']
-        if (state.trades[tradeKey]) {
-          newState.trades = _.omit(state.trades, tradeKey)
-        }
-      }
-      const trades = _.mapKeys(_.values(payloadTrades), 'tradeId')
+interface State {
+  trades: Trades
+}
+
+const initialState = {
+  trades: {},
+}
+
+export const blotterServiceReducer = (state: State = initialState, action): State => {
+  switch (action.type) {
+    case ACTION_TYPES.BLOTTER_SERVICE_NEW_TRADES:
+      const newTradesById = keyBy(action.payload.trades, `tradeId`)
       return {
-        trades: {...trades, ...newState.trades},
+        ...state,
+        trades: {
+          ...state.trades,
+          ...newTradesById,
+        }
       }
     default:
       return state
