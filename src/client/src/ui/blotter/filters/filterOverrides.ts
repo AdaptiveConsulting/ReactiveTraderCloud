17f1673d12f76bb0a0912e1f127dd1573f741a40
Elena Milliner <elena@weareadaptive.com>
2018-02-26 15:27:42 +0000

    code cleanup

--- src/client/src/ui/blotter/filters/filterOverrides.ts
+++ src/client/src/ui/blotter/filters/filterOverrides.ts
@@ -1,6 +1,5 @@
 import * as AgGrid from 'ag-grid'
 import './filters.scss'
-// import { Column, FilterManager, FilterWrapper, Promise, Utils as _ } from "ag-grid";
 
 const getFilterSelect = (filter: any) => {
   const defaultFilterTypes = filter.getApplicableFilterTypes()
