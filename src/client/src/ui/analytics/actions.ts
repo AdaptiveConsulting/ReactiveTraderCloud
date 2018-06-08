b2b5b4ecdc89568f23acea07a4ef403086a17166
Keshal Desai <keshaldesai@gmail.com>
2018-06-08 12:27:39 -0400

    actions and reducers  typed

--- src/client/src/ui/analytics/actions.ts
+++ src/client/src/ui/analytics/actions.ts
@@ -1,7 +1,12 @@
-import { createAction } from 'redux-actions'
+import { action, ActionUnion } from '../../ActionHelper'
+import { PositionUpdates } from '../../types'
 
 export enum ACTION_TYPES {
   ANALYTICS_SERVICE = '@ReactiveTraderCloud/ANALYTICS_SERVICE'
 }
 
-export const fetchAnalytics = createAction(ACTION_TYPES.ANALYTICS_SERVICE)
+export const AnalyticsActions = {
+  fetchAnalytics: action<typeof ACTION_TYPES.ANALYTICS_SERVICE, PositionUpdates>(ACTION_TYPES.ANALYTICS_SERVICE)
+}
+
+export type AnalyticsActions = ActionUnion<typeof AnalyticsActions>
--- src/client/src/ui/analytics/epics.ts
+++ src/client/src/ui/analytics/epics.ts
@@ -4,7 +4,7 @@ import { ApplicationEpic } from '../../ApplicationEpic'
 import { ACTION_TYPES as REF_ACTION_TYPES } from '../../referenceDataOperations'
 import { PositionUpdates } from '../../types'
 import { DISCONNECT_SERVICES } from './../../connectionActions'
-import { fetchAnalytics } from './actions'
+import { AnalyticsActions } from './actions'
 
 const CURRENCY: string = 'USD'
 
@@ -12,13 +12,11 @@ export const analyticsServiceEpic: ApplicationEpic = (action$, store, { analytic
   return action$.pipe(
     ofType(REF_ACTION_TYPES.REFERENCE_SERVICE),
     mergeMapTo(
-      analyticsService
-        .getAnalyticsStream(CURRENCY)
-        .pipe(
-          tap((action: PositionUpdates) => openFin.publishCurrentPositions(action.currentPositions)),
-          map(fetchAnalytics),
-          takeUntil(action$.pipe(ofType(DISCONNECT_SERVICES)))
-        )
+      analyticsService.getAnalyticsStream(CURRENCY).pipe(
+        tap((action: PositionUpdates) => openFin.publishCurrentPositions(action.currentPositions)),
+        map(AnalyticsActions.fetchAnalytics),
+        takeUntil(action$.pipe(ofType(DISCONNECT_SERVICES)))
+      )
     )
   )
 }
