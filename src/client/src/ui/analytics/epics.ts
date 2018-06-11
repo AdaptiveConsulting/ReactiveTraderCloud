3740e619070c9acb1ebc244b385990805316ee6b
Keshal Desai <keshaldesai@gmail.com>
2018-06-11 09:54:40 -0400

    Operators typed

--- src/client/src/ui/analytics/epics.ts
+++ src/client/src/ui/analytics/epics.ts
@@ -1,24 +1,28 @@
+import { Action } from 'redux'
 import { ofType } from 'redux-observable'
 import { map, mergeMapTo, takeUntil, tap } from 'rxjs/operators'
 import { ApplicationEpic } from '../../ApplicationEpic'
-import { ACTION_TYPES as REF_ACTION_TYPES } from '../../referenceDataOperations'
+import { ACTION_TYPES as REF_ACTION_TYPES, createReferenceServiceAction } from '../../referenceDataOperations'
 import { PositionUpdates } from '../../types'
-import { DISCONNECT_SERVICES } from './../../connectionActions'
+import { disconnect, DISCONNECT_SERVICES } from './../../connectionActions'
 import { AnalyticsActions } from './actions'
 
 const CURRENCY: string = 'USD'
 
-export const analyticsServiceEpic: ApplicationEpic = (action$, state$, { analyticsService, openFin }) => {
-  return action$.pipe(
-    ofType(REF_ACTION_TYPES.REFERENCE_SERVICE),
-    mergeMapTo(
+type ReferenceServiceAction = ReturnType<typeof createReferenceServiceAction>
+type DisconnectAction = ReturnType<typeof disconnect>
+type FetchAnalyticsAction = ReturnType<typeof AnalyticsActions.fetchAnalytics>
+
+export const analyticsServiceEpic: ApplicationEpic = (action$, state$, { analyticsService, openFin }) =>
+  action$.pipe(
+    ofType<Action, ReferenceServiceAction>(REF_ACTION_TYPES.REFERENCE_SERVICE),
+    mergeMapTo<FetchAnalyticsAction>(
       analyticsService.getAnalyticsStream(CURRENCY).pipe(
-        tap((action: PositionUpdates) => openFin.publishCurrentPositions(action.currentPositions)),
-        map(AnalyticsActions.fetchAnalytics),
-        takeUntil(action$.pipe(ofType(DISCONNECT_SERVICES)))
+        tap((positionUpdates: PositionUpdates) => openFin.publishCurrentPositions(positionUpdates.currentPositions)),
+        map<PositionUpdates, FetchAnalyticsAction>(AnalyticsActions.fetchAnalytics),
+        takeUntil<FetchAnalyticsAction>(action$.pipe(ofType<Action, DisconnectAction>(DISCONNECT_SERVICES)))
       )
     )
   )
-}
 
 export default analyticsServiceEpic
