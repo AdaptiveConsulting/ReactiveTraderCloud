70553fc654ed609ce4d9785b77ec4756894c72a4
Keshal Desai <keshaldesai@gmail.com>
2018-06-13 11:26:02 -0400

    Sidebar view refactored

--- src/client/src/ui/sidebar/SidebarRegionView.tsx
+++ src/client/src/ui/sidebar/SidebarRegionView.tsx
@@ -1,25 +1,19 @@
 import * as classnames from 'classnames'
 import * as React from 'react'
+import { SidebarRegionActions } from './actions'
 
 import { AnalyticsContainer } from '../analytics'
 
-interface SidebarRegionViewProps {
+export interface SidebarRegionViewProps {
   displayAnalytics: boolean
-  toggleAnalytics: () => void
+  toggleAnalytics: typeof SidebarRegionActions.toggleAnalytics
 }
 
-export const SidebarRegionView: React.SFC<SidebarRegionViewProps> = (
-  props: SidebarRegionViewProps
-) => {
-  const buttonStyles = classnames(
-    'sidebar-region__element-button glyphicon glyphicon-stats',
-    (props.displayAnalytics && 'sidebar-region__element--active') ||
-      'sidebar-region__element--inactive'
-  )
-  const analyticsStyles = classnames(
-    'sidebar-region__content',
-    !props.displayAnalytics && 'sidebar-region__container--no-content'
-  )
+export const SidebarRegionView: React.SFC<SidebarRegionViewProps> = ({
+  displayAnalytics,
+  toggleAnalytics
+}: SidebarRegionViewProps) => {
+  const { analyticsStyles, buttonStyles } = getSidebarStyles(displayAnalytics)
 
   return (
     <div className="shell__sidebar">
@@ -27,9 +21,20 @@ export const SidebarRegionView: React.SFC<SidebarRegionViewProps> = (
         <AnalyticsContainer />
       </div>
       <div className="sidebar-region__container">
-        <i className={buttonStyles} onClick={() => props.toggleAnalytics()} />
+        <i className={buttonStyles} onClick={toggleAnalytics} />
         <div className="sidebar-region__element" />
       </div>
     </div>
   )
+}
+function getSidebarStyles(displayAnalytics: boolean) {
+  const buttonStyles = classnames(
+    'sidebar-region__element-button glyphicon glyphicon-stats',
+    (displayAnalytics && 'sidebar-region__element--active') || 'sidebar-region__element--inactive'
+  )
+  const analyticsStyles = classnames(
+    'sidebar-region__content',
+    !displayAnalytics && 'sidebar-region__container--no-content'
+  )
+  return { analyticsStyles, buttonStyles }
 }
