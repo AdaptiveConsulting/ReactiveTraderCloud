31b51ba235938ea02ba043a0c49e801bb40df37c
Keshal Desai <keshaldesai@gmail.com>
2018-06-14 11:22:12 -0400

    Typed spotTile actions and reducers

--- src/client/src/ui/spotTile/reducer.ts
+++ src/client/src/ui/spotTile/reducer.ts
@@ -1,19 +1,32 @@
 import { ACTION_TYPES as CONNECTION_ACTION_TYPES, DisconnectAction } from '../../connectionActions'
+import { Notification, Trade } from '../../types'
 import { buildNotification } from '../notification/notificationUtils'
 import { tradeError, tradeSuccesful } from './../../types/executeTradeRequest'
 import { ACTION_TYPES, SpotTileActions } from './actions'
 
-const updateSpotTile = (state, symbol, value) => {
-  return {
-    ...state,
-    [symbol]: {
-      ...state[symbol],
-      ...value
-    }
-  }
+interface SpotTileState {
+  [tradeId: number]: Trade
 }
 
-export const spotTileDataReducer = (state: any = {}, action: SpotTileActions | DisconnectAction) => {
+interface SpotTileValue {
+  currencyChartIsOpening?: boolean
+  hasError?: boolean
+  isTradeExecutionInFlight?: boolean
+  notification?: Notification
+}
+
+const updateSpotTile = (state: SpotTileState, symbol: string, value: SpotTileValue): SpotTileState => ({
+  ...state,
+  [symbol]: {
+    ...state[symbol],
+    ...value
+  }
+})
+
+export const spotTileDataReducer = (
+  state: SpotTileState = {},
+  action: SpotTileActions | DisconnectAction
+): SpotTileState => {
   switch (action.type) {
     case ACTION_TYPES.DISPLAY_CURRENCY_CHART:
       return updateSpotTile(state, action.payload, {
