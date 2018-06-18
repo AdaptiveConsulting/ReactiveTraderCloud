9861c77478924a35cc7d5842af0adfbf551471ed
Keshal Desai <keshaldesai@gmail.com>
2018-06-18 15:25:06 -0400

    Typed close position epic

--- src/client/src/services/openFin/epics/closePosition.ts
+++ src/client/src/services/openFin/epics/closePosition.ts
@@ -1,10 +1,11 @@
+import { Action } from 'redux'
 import { ofType } from 'redux-observable'
 import { bindCallback } from 'rxjs'
-import { map, mergeMap, withLatestFrom } from 'rxjs/operators'
+import { map, mergeMapTo, withLatestFrom } from 'rxjs/operators'
 import { ApplicationEpic } from '../../../ApplicationEpic'
-import { ACTION_TYPES as CONNECTION_ACTION_TYPES } from '../../../operations/connectionStatus'
+import { ACTION_TYPES as CONNECTION_ACTION_TYPES, ConnectAction } from '../../../operations/connectionStatus'
 import { Direction } from '../../../types'
-import { SpotTileActions } from '../../../ui/spotTile/actions'
+import { SpotTileActions } from '../../../ui/spotTile'
 
 function createTrade(msg, price) {
   const direction = msg.amount > 0 ? Direction.Sell : Direction.Buy
@@ -23,10 +24,8 @@ function createTrade(msg, price) {
 
 export const closePositionEpic: ApplicationEpic = (action$, state$, { openFin }) =>
   action$.pipe(
-    ofType(CONNECTION_ACTION_TYPES.CONNECT_SERVICES),
-    mergeMap(() => {
-      return bindCallback(openFin.addSubscription).bind(openFin)('close-position')
-    }),
+    ofType<Action, ConnectAction>(CONNECTION_ACTION_TYPES.CONNECT_SERVICES),
+    mergeMapTo(bindCallback(openFin.addSubscription).bind(openFin)('close-position')),
     withLatestFrom(state$),
     map<any, any>(([[msg, uuid], state]) => {
       const trade = createTrade(msg, state.pricingService[msg.symbol])
