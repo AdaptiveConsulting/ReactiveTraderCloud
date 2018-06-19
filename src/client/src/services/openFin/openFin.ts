efb8787fdd7984a7d0e811776ff204840ea6d933
Keshal Desai <keshaldesai@gmail.com>
2018-06-19 16:44:47 -0400

    Publish instead of send for a temporary fix

--- src/client/src/services/openFin/openFin.ts
+++ src/client/src/services/openFin/openFin.ts
@@ -233,7 +233,7 @@ export default class OpenFin {
         // this._router.publishEvent(WellKnownModelIds.blotterModelId, 'highlightTradeRow', { trade })
       }
     })
-    fin.desktop.InterApplicationBus.publish('blotter-new-item', tradeNotification)
+    ///fin.desktop.InterApplicationBus.publish('blotter-new-item', tradeNotification)
   }
 
   publishCurrentPositions(ccyPairPositions) {
@@ -251,12 +251,15 @@ export default class OpenFin {
     fin.desktop.InterApplicationBus.publish('price-update', price)
   }
 
-  sendAllBlotterData(uuid, blotterData: Trades, currencyPairs: CurrencyPairState) {
+  sendAllBlotterData(blotterData: Trades, currencyPairs: CurrencyPairState) {
+    if (Object.keys(currencyPairs).length === 0 || Object.keys(blotterData).length === 0) {
+      return
+    }
     const parsed = Object.keys(blotterData).map(x =>
       formatTradeNotification(blotterData[x], currencyPairs[blotterData[x].symbol])
     )
 
-    fin.desktop.InterApplicationBus.send(uuid, 'blotter-data', parsed)
+    fin.desktop.InterApplicationBus.publish('blotter-data', parsed)
   }
 
   sendPositionClosedNotification(uuid: string, correlationId: string) {
