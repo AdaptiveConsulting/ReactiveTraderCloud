9c4cd2b4a1c42e7f313d1353f420ba81a39cabb5
Keshal Desai <keshaldesai@gmail.com>
2018-06-19 16:44:47 -0400

    Publish instead of send for a temporary fix

--- src/client/src/services/openFin/epics/blotterServiceEpic.ts
+++ src/client/src/services/openFin/epics/blotterServiceEpic.ts
@@ -1,25 +1,23 @@
 import { Action } from 'redux'
-import { ofType, StateObservable } from 'redux-observable'
-import { ignoreElements, tap } from 'rxjs/operators'
+import { ofType } from 'redux-observable'
+import { debounceTime, ignoreElements, tap } from 'rxjs/operators'
 import { ApplicationEpic } from '../../../ApplicationEpic'
-import { GlobalState } from '../../../combineReducers'
-import { CurrencyPairState } from '../../../operations/currencyPairs'
-import { ACTION_TYPES as BLOTTER_ACTION_TYPES, BlotterActions, Trades } from '../../../ui/blotter'
-import OpenFin from '../openFin'
+import { ACTION_TYPES as BLOTTER_ACTION_TYPES, BlotterActions } from '../../../ui/blotter'
 
 const { createNewTradesAction } = BlotterActions
 type NewTradesAction = ReturnType<typeof createNewTradesAction>
 
-const subscribeOpenFinToBlotterData = (openFin: OpenFin, state$: StateObservable<GlobalState>) => () => {
+/* export const subscribeOpenFinToBlotterData = (openFin: OpenFin, state$: StateObservable<GlobalState>) => () => {
   const trades: Trades = state$.value.blotterService.trades
   const currencyPairs: CurrencyPairState = state$.value.currencyPairs
-  const cb = (msg: any, uuid: string) => openFin.sendAllBlotterData(uuid, trades, currencyPairs)
+  const cb = (msg: any, uuid: string) => openFin.sendAllBlotterData(trades, currencyPairs)
   openFin.addSubscription('fetch-blotter', cb)
-}
+} */
 
 export const connectBlotterServiceToOpenFinEpic: ApplicationEpic = (action$, state$, { openFin }) =>
   action$.pipe(
     ofType<Action, NewTradesAction>(BLOTTER_ACTION_TYPES.BLOTTER_SERVICE_NEW_TRADES),
-    tap(subscribeOpenFinToBlotterData(openFin, state$)),
+    debounceTime(2000),
+    tap(() => openFin.sendAllBlotterData(state$.value.blotterService.trades, state$.value.currencyPairs)),
     ignoreElements()
   )
