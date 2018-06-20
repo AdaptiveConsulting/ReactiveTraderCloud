2aabc08de3a90b4d0863c9ef7d0e594d2a29a522
Keshal Desai <keshaldesai@gmail.com>
2018-06-20 11:29:12 -0400

    Interval based publish

--- src/client/src/services/openFin/epics/blotterServiceEpic.ts
+++ src/client/src/services/openFin/epics/blotterServiceEpic.ts
@@ -1,11 +1,13 @@
 import { Action } from 'redux'
 import { ofType } from 'redux-observable'
-import { debounceTime, ignoreElements, tap } from 'rxjs/operators'
+import { interval } from 'rxjs'
+import { ignoreElements, switchMapTo, takeUntil, tap } from 'rxjs/operators'
 import { ApplicationEpic } from '../../../ApplicationEpic'
-import { ACTION_TYPES as BLOTTER_ACTION_TYPES, BlotterActions } from '../../../ui/blotter'
-
-const { createNewTradesAction } = BlotterActions
-type NewTradesAction = ReturnType<typeof createNewTradesAction>
+import {
+  ACTION_TYPES as CONNECTION_ACTION_TYPES,
+  ConnectAction,
+  DisconnectAction
+} from '../../../operations/connectionStatus'
 
 /* export const subscribeOpenFinToBlotterData = (openFin: OpenFin, state$: StateObservable<GlobalState>) => () => {
   const trades: Trades = state$.value.blotterService.trades
@@ -16,8 +18,12 @@ type NewTradesAction = ReturnType<typeof createNewTradesAction>
 
 export const connectBlotterServiceToOpenFinEpic: ApplicationEpic = (action$, state$, { openFin }) =>
   action$.pipe(
-    ofType<Action, NewTradesAction>(BLOTTER_ACTION_TYPES.BLOTTER_SERVICE_NEW_TRADES),
-    debounceTime(2000),
-    tap(() => openFin.sendAllBlotterData(state$.value.blotterService.trades, state$.value.currencyPairs)),
-    ignoreElements()
+    ofType<Action, ConnectAction>(CONNECTION_ACTION_TYPES.CONNECT_SERVICES),
+    switchMapTo(
+      interval(7500).pipe(
+        takeUntil(action$.pipe(ofType<Action, DisconnectAction>(CONNECTION_ACTION_TYPES.DISCONNECT_SERVICES))),
+        tap(() => openFin.sendAllBlotterData(state$.value.blotterService.trades, state$.value.currencyPairs)),
+        ignoreElements()
+      )
+    )
   )
