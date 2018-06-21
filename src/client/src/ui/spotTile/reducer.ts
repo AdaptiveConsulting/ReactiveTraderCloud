348909266943dc9ae9966bdd544c1ff1ef1f5b53
Keshal Desai <keshaldesai@gmail.com>
2018-06-21 16:07:18 -0400

    Redux spotTile reducer composed in a split pattern

--- src/client/src/ui/spotTile/reducer.ts
+++ src/client/src/ui/spotTile/reducer.ts
@@ -1,30 +1,41 @@
 import { ACTION_TYPES as CONNECTION_ACTION_TYPES, DisconnectAction } from '../../operations/connectionStatus'
-import { Notification } from '../../types'
+import { TradeErrorResponse, TradeSuccessResponse } from '../../types'
 import { SpotTileData } from '../../types/spotTileData'
 import { buildNotification } from '../notification/notificationUtils'
-import { tradeError, tradeSuccesful } from './../../types/executeTradeRequest'
 import { ACTION_TYPES, SpotTileActions } from './actions'
-
-const updateSpotTile = (state: SpotTileState, symbol: string, value: SpotTileValue): SpotTileState => ({
-  ...state,
-  [symbol]: {
-    ...state[symbol],
-    ...value
-  }
-})
 
 interface SpotTileState {
   [currencyPair: string]: SpotTileData
 }
 
-interface SpotTileValue {
-  currencyChartIsOpening?: boolean
-  hasError?: boolean
-  isTradeExecutionInFlight?: boolean
-  notification?: Notification
-}
-
 const INITIAL_STATE: SpotTileState = {}
+
+const spotTileReducer = (state: SpotTileData, action: SpotTileActions): SpotTileData => {
+  switch (action.type) {
+    case ACTION_TYPES.DISPLAY_CURRENCY_CHART:
+      return { ...state, currencyChartIsOpening: true }
+    case ACTION_TYPES.CURRENCY_CHART_OPENED:
+      return { ...state, currencyChartIsOpening: false }
+    case ACTION_TYPES.EXECUTE_TRADE:
+      return { ...state, isTradeExecutionInFlight: true }
+    case ACTION_TYPES.TRADE_EXECUTED: {
+      const { hasError } = action.payload
+      const notification = hasError
+        ? buildNotification(null, (action.payload as TradeErrorResponse).error)
+        : buildNotification((action.payload as TradeSuccessResponse).trade)
+      return {
+        ...state,
+        hasError,
+        notification,
+        isTradeExecutionInFlight: false
+      }
+    }
+    case ACTION_TYPES.DISMISS_NOTIFICATION:
+      return { ...state, notification: null }
+    default:
+      return state
+  }
+}
 
 export const spotTileDataReducer = (
   state: SpotTileState = INITIAL_STATE,
@@ -32,40 +43,16 @@ export const spotTileDataReducer = (
 ): SpotTileState => {
   switch (action.type) {
     case ACTION_TYPES.DISPLAY_CURRENCY_CHART:
-      return updateSpotTile(state, action.payload, {
-        currencyChartIsOpening: true
-      })
-
     case ACTION_TYPES.CURRENCY_CHART_OPENED:
-      return updateSpotTile(state, action.payload, { currencyChartIsOpening: false })
-
-    case ACTION_TYPES.EXECUTE_TRADE:
-      return updateSpotTile(state, action.payload.CurrencyPair, {
-        isTradeExecutionInFlight: true
-      })
-
-    case ACTION_TYPES.TRADE_EXECUTED: {
-      const { payload } = action
-      const symbol = action.payload.request.CurrencyPair
-
-      if (tradeError(payload)) {
-        return updateSpotTile(state, symbol, {
-          hasError: true,
-          isTradeExecutionInFlight: false,
-          notification: buildNotification(null, payload.error)
-        })
-      }
-      if (tradeSuccesful(payload)) {
-        return updateSpotTile(state, symbol, {
-          hasError: false,
-          isTradeExecutionInFlight: false,
-          notification: buildNotification(payload.trade)
-        })
-      }
-      return state
-    }
     case ACTION_TYPES.DISMISS_NOTIFICATION:
-      return updateSpotTile(state, action.payload, { notification: null })
+      return { ...state, [action.payload]: spotTileReducer(state[action.payload], action) }
+    case ACTION_TYPES.EXECUTE_TRADE:
+      return { ...state, [action.payload.CurrencyPair]: spotTileReducer(state[action.payload.CurrencyPair], action) }
+    case ACTION_TYPES.TRADE_EXECUTED:
+      return {
+        ...state,
+        [action.payload.request.CurrencyPair]: spotTileReducer(state[action.payload.request.CurrencyPair], action)
+      }
     case CONNECTION_ACTION_TYPES.DISCONNECT_SERVICES:
       return INITIAL_STATE
     default:
