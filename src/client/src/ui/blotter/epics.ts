f3f37a4f145f85f8c3694e179005192d2afdddf8
Bhavesh Desai <bhavesh@weareadaptive.com>
2018-06-21 11:11:50 +0100

    example

--- src/client/src/ui/blotter/epics.ts
+++ src/client/src/ui/blotter/epics.ts
@@ -1,26 +1,17 @@
-import { Action } from 'redux'
-import { ofType } from 'redux-observable'
 import { map, switchMapTo, takeUntil } from 'rxjs/operators'
 import { ApplicationEpic } from '../../ApplicationEpic'
-import {
-  ACTION_TYPES as CONNECTION_ACTION_TYPES,
-  ConnectAction,
-  DisconnectAction
-} from '../../operations/connectionStatus'
+import { applicationConnected, applicationDisconnected } from '../../operations/connectionStatus'
 import { BlotterActions } from './actions'
 
 const { createNewTradesAction } = BlotterActions
-type NewTradesAction = ReturnType<typeof createNewTradesAction>
 
 export const blotterServiceEpic: ApplicationEpic = (action$, state$, { blotterService }) =>
   action$.pipe(
-    ofType<Action, ConnectAction>(CONNECTION_ACTION_TYPES.CONNECT_SERVICES),
+    applicationConnected,
     switchMapTo(
       blotterService.getTradesStream().pipe(
         map(createNewTradesAction),
-        takeUntil<NewTradesAction>(
-          action$.pipe(ofType<Action, DisconnectAction>(CONNECTION_ACTION_TYPES.DISCONNECT_SERVICES))
-        )
+        takeUntil(action$.pipe(applicationDisconnected))
       )
     )
   )
