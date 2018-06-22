836d211e687bf1698ce9d927a20612537932da91
Keshal Desai <keshaldesai@gmail.com>
2018-06-22 09:08:01 -0400

    Redux regions reducer composed in a split pattern

--- src/client/src/ui/common/regions/reducer.ts
+++ src/client/src/ui/common/regions/reducer.ts
@@ -1,33 +1,49 @@
 import { ACTION_TYPES as CONNECTION_ACTION_TYPES, DisconnectAction } from '../../../operations/connectionStatus'
 import { ACTION_TYPES, Region, RegionActions } from './actions'
 
-const changeRegionTearOffStatus = (state: RegionState, payload: Region, status: boolean) => ({
-  ...state,
-  [payload.id]: {
-    ...state[payload.id],
-    isTearedOff: status
-  }
-})
-
-export interface RegionState {
+export interface RegionsState {
   [regionId: string]: Region
 }
 
-const INITIAL_STATE: RegionState = {}
+const INITIAL_STATE: RegionsState = {}
 
-export const regionsReducer = (state: RegionState = {}, action: RegionActions | DisconnectAction): RegionState => {
+export const regionsReducer = (
+  state: RegionsState = INITIAL_STATE,
+  action: RegionActions | DisconnectAction
+): RegionsState => {
   switch (action.type) {
     case ACTION_TYPES.REGION_ADD:
-      return {
-        [action.payload.id]: action.payload,
-        ...state
-      }
     case ACTION_TYPES.REGION_ATTACH_WINDOW:
-      return changeRegionTearOffStatus(state, action.payload, false)
     case ACTION_TYPES.REGION_TEAROFF_WINDOW:
-      return changeRegionTearOffStatus(state, action.payload, true)
+      return { ...state, [action.payload.id]: regionReducer(state[action.payload.id], action) }
     case CONNECTION_ACTION_TYPES.DISCONNECT_SERVICES:
       return INITIAL_STATE
+    default:
+      return state
+  }
+}
+
+const REGION_INITIAL_STATE = {
+  id: '',
+  isTearedOff: false,
+  container: null,
+  settings: null
+}
+
+const regionReducer = (state: Region = REGION_INITIAL_STATE, action: RegionActions): Region => {
+  switch (action.type) {
+    case ACTION_TYPES.REGION_ADD:
+      return { ...action.payload, ...state }
+    case ACTION_TYPES.REGION_ATTACH_WINDOW:
+      return {
+        ...state,
+        isTearedOff: false
+      }
+    case ACTION_TYPES.REGION_TEAROFF_WINDOW:
+      return {
+        ...state,
+        isTearedOff: true
+      }
     default:
       return state
   }
