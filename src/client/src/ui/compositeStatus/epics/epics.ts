3b55e2a8cbea6e6449a17dedbf3b58050e24131c
Keshal Desai <keshaldesai@gmail.com>
2018-07-20 14:43:15 -0400

    Created an epics folder for composite status

--- src/client/src/ui/compositeStatus/epics/epics.ts
+++ src/client/src/ui/compositeStatus/epics/epics.ts
@@ -1,7 +1,7 @@
 import { map, switchMapTo, takeUntil } from 'rxjs/operators'
-import { ApplicationEpic } from '../../ApplicationEpic'
-import { applicationConnected, applicationDisconnected } from '../../operations/connectionStatus'
-import { CompositeStatusServiceActions } from './actions'
+import { ApplicationEpic } from '../../../ApplicationEpic'
+import { applicationConnected, applicationDisconnected } from '../../../operations/connectionStatus'
+import { CompositeStatusServiceActions } from '../actions'
 
 const { createCompositeStatusServiceAction } = CompositeStatusServiceActions
 type CreateStatusServiceAction = ReturnType<typeof createCompositeStatusServiceAction>
