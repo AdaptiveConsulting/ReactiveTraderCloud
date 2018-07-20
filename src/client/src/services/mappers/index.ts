70d63a856e564b54857076e031c19dc0bc17c4e8
Bhavesh Desai <bhavesh@weareadaptive.com>
2018-07-20 16:33:57 +0100

    remove unused type

--- src/client/src/services/mappers/index.ts
+++ src/client/src/services/mappers/index.ts
@@ -1,3 +1,2 @@
 export { mapFromTradeDto } from './tradeMapper'
 export { default as referenceDataMapper } from './referenceDataMapper'
-export { connectionTypeMapper } from './connectionTypeMapper'
