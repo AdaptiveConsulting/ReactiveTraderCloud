45eba06e21cf15454081e4fa484607ef4729ac2e
Keshal Desai <keshaldesai@gmail.com>
2018-07-20 15:15:38 -0400

    Removed unused import

--- src/client/src/ui/compositeStatus/index.ts
+++ src/client/src/ui/compositeStatus/index.ts
@@ -1,3 +1,2 @@
 export { compositeStatusServiceReducer } from './reducer'
 export { compositeStatusServiceEpic } from './epics'
-export { default as CompositeStatusService } from './compositeStatusService'
