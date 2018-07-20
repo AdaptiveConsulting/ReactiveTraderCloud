83bdb56ec62bcf426e0de66d37608c0fbd081c4a
Keshal Desai <keshaldesai@gmail.com>
2018-07-20 13:29:35 -0400

    Fixed wrong dispatch import

--- src/client/src/ui/analytics/AnalyticsContainer.tsx
+++ src/client/src/ui/analytics/AnalyticsContainer.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import { connect, Dispatch } from 'react-redux'
+import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 import { GlobalState } from '../../combineReducers'
 import { Environment } from '../../system'
 import { AnalyticsActions } from './actions'
