6573639634440df71f8c6f2f429918576fe2666c
Keshal Desai <keshaldesai@gmail.com>
2018-07-25 10:07:11 -0400

    Renamed ACTION_TYPES to FOOTER_ACTION_TYPES

--- src/client/src/shell/openFin/epics/openLinkInBrowser.ts
+++ src/client/src/shell/openFin/epics/openLinkInBrowser.ts
@@ -2,14 +2,14 @@ import { Action } from 'redux'
 import { ofType } from 'redux-observable'
 import { ignoreElements, tap } from 'rxjs/operators'
 import { ApplicationEpic } from '../../../ApplicationEpic'
-import { ACTION_TYPES, FooterActions } from '../../../ui/footer'
+import { FOOTER_ACTION_TYPES, FooterActions } from '../../../ui/footer'
 
 const { openLink } = FooterActions
 type OpenLinkAction = ReturnType<typeof openLink>
 
 export const openLinkWithOpenFinEpic: ApplicationEpic = (action$, state$, { openFin }) =>
   action$.pipe(
-    ofType<Action, OpenLinkAction>(ACTION_TYPES.OPEN_LINK),
+    ofType<Action, OpenLinkAction>(FOOTER_ACTION_TYPES.OPEN_LINK),
     tap(link => openFin.openLink(link.payload)),
     ignoreElements()
   )
--- src/client/src/ui/footer/actions.ts
+++ src/client/src/ui/footer/actions.ts
@@ -1,13 +1,13 @@
 import { action, ActionUnion } from 'rt-util'
 
-export enum ACTION_TYPES {
+export enum FOOTER_ACTION_TYPES {
   TOGGLE_STATUS_SERVICES = '@ReactiveTraderCloud/TOGGLE_STATUS_SERVICES',
   OPEN_LINK = '@ReactiveTraderCloud/OPEN_LINK'
 }
 
 export const FooterActions = {
-  toggleStatusServices: action<ACTION_TYPES.TOGGLE_STATUS_SERVICES>(ACTION_TYPES.TOGGLE_STATUS_SERVICES),
-  openLink: action<ACTION_TYPES.OPEN_LINK, string>(ACTION_TYPES.OPEN_LINK)
+  toggleStatusServices: action<FOOTER_ACTION_TYPES.TOGGLE_STATUS_SERVICES>(FOOTER_ACTION_TYPES.TOGGLE_STATUS_SERVICES),
+  openLink: action<FOOTER_ACTION_TYPES.OPEN_LINK, string>(FOOTER_ACTION_TYPES.OPEN_LINK)
 }
 
 export type FooterActions = ActionUnion<typeof FooterActions>
