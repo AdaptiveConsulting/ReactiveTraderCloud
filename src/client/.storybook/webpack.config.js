eda0025d211704bbc3aa7cbac2aad92aedca3fc9
Keshal Desai <keshaldesai@gmail.com>
2018-07-25 12:20:52 -0400

    Working with typescript

--- src/client/.storybook/webpack.config.js
+++ src/client/.storybook/webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path')
 const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin')
-module.exports = (baseConfig, env, config) => {
-  config.module.rules.push({
+module.exports = (baseConfig, env, defaultConfig) => {
+  defaultConfig.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('awesome-typescript-loader')
   })
-  config.plugins.push(new TSDocgenPlugin()) // optional
-  config.resolve.extensions.push('.ts', '.tsx')
-  return config
+  defaultConfig.plugins.push(new TSDocgenPlugin())
+  defaultConfig.resolve.extensions.push('.ts', '.tsx')
+  defaultConfig.resolve.alias = {
+    'rt-components': path.resolve(__dirname, '../src', 'rt-components'),
+    'rt-util': path.resolve(__dirname, '../src', 'rt-util'),
+    'rt-types': path.resolve(__dirname, '../src', 'rt-types'),
+    'rt-actions': path.resolve(__dirname, '../src', 'rt-actions')
+  }
+  return defaultConfig
 }
--- src/client/stories/index.stories.js
+++ src/client/stories/index.stories.js
@@ -1,19 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { action } from '@storybook/addon-actions'
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Footer } from '../src/ui/footer/Footer'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+const stories = storiesOf('Components', module)
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+stories.add('Footer', withInfo({ inline: true })(() => <Footer />))
