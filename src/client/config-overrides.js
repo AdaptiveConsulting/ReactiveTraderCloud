5a6e3c36c159f02de1095917cc3ec84bd7fe099a
Evan Schneider <me@evanrs.com>
2018-08-16 12:39:43 -0400

    Inject babel into ts loader

--- src/client/config-overrides.js
+++ src/client/config-overrides.js
@@ -1,29 +1,33 @@
-const { injectBabelPlugin } = require('react-app-rewired')
+const { injectBabelPlugin, getLoader, getBabelLoader, loaderNameMatches } = require('react-app-rewired')
 
 module.exports = function override(config, env) {
-  if (env === 'production') {
-    config = injectBabelPlugin(
-      [
-        'emotion',
-        {
-          hoist: true,
-          autoLabel: true
-        }
-      ],
-      config
-    )
-  } else {
-    config = injectBabelPlugin(
-      [
-        'emotion',
-        {
-          sourceMap: true,
-          autoLabel: true,
-          labelFormat: '[filename]--[local]'
-        }
-      ],
-      config
-    )
+  // Inject all babel plugins before rewiring typescript
+  // Inject babel-plugin-emotion
+  config = injectBabelPlugin(
+    [
+      'emotion',
+      {
+        hoist: env === 'production',
+        autoLabel: true,
+        labelFormat: '[filename]--[local]'
+      }
+    ],
+    config
+  )
+  // Rewire typescript with our rewired babel config
+  let babelLoader = getBabelLoader(config.module.rules)
+  let tsLoader = getLoader(config.module.rules, rule => /ts|tsx/.test(rule.test))
+  console.log(babelLoader)
+  console.log(tsLoader)
+  // process.exit(0)
+  try {
+    tsLoader.use.unshift({
+      loader: babelLoader.loader,
+      options: babelLoader.options
+    })
+  } catch (e) {
+    console.error('Failed to add babel to typescript loader')
+    console.error(e)
   }
 
   return config
