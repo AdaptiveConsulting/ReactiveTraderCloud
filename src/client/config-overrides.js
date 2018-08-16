4f45ceb5e5ee7bd485b9e77e26cf28b8afe6a580
Evan Schneider <me@evanrs.com>
2018-08-16 12:44:07 -0400

    Remove log

--- src/client/config-overrides.js
+++ src/client/config-overrides.js
@@ -2,6 +2,7 @@ const { injectBabelPlugin, getLoader, getBabelLoader, loaderNameMatches } = requ
 
 module.exports = function override(config, env) {
   // Inject all babel plugins before rewiring typescript
+
   // Inject babel-plugin-emotion
   config = injectBabelPlugin(
     [
@@ -14,21 +15,14 @@ module.exports = function override(config, env) {
     ],
     config
   )
+
   // Rewire typescript with our rewired babel config
-  let babelLoader = getBabelLoader(config.module.rules)
-  let tsLoader = getLoader(config.module.rules, rule => /ts|tsx/.test(rule.test))
-  console.log(babelLoader)
-  console.log(tsLoader)
-  // process.exit(0)
-  try {
-    tsLoader.use.unshift({
-      loader: babelLoader.loader,
-      options: babelLoader.options
-    })
-  } catch (e) {
-    console.error('Failed to add babel to typescript loader')
-    console.error(e)
-  }
+  const babelLoader = getBabelLoader(config.module.rules)
+  const tsLoader = getLoader(config.module.rules, rule => /ts|tsx/.test(rule.test))
+  tsLoader.use.unshift({
+    loader: babelLoader.loader,
+    options: babelLoader.options
+  })
 
   return config
 }
