b3bf3a45ddfbf3308b32abf1d98dc89d83617f6c
Evan Schneider <me@evanrs.com>
2018-08-17 20:20:04 -0400

    Put <Analytics/> in a cute little box …

--- src/client/src/shell/sidebar/styled.tsx
+++ src/client/src/shell/sidebar/styled.tsx
@@ -1,16 +1,19 @@
 import { styled } from 'rt-theme'
 
 export const Root = styled.div`
+  margin: 0.5rem;
+  border-radius: 0.25rem;
   flex: 0.1 1 16rem;
   overflow-y: auto;
-  background-color: ${props => props.theme.shell.backgroundColor};
-  color: ${props => props.theme.shell.textColor};
+  background-color: ${props => props.theme.component.backgroundColor};
+  color: ${props => props.theme.component.textColor};
 
   display: flex;
   flex-direction: row;
 `
 
 export const RegionContent = styled.div`
+  margin: 1rem 0.25rem;
   width: 100%;
   min-width: 16rem;
   max-width: 24rem;
