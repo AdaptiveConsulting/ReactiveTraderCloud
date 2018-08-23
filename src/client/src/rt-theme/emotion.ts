7cefd11be5733d2c4fcbf0d239cddc2b5a4858d1
Keshal Desai <keshaldesai@gmail.com>
2018-08-23 15:14:56 -0400

    Working stylis plugin

--- src/client/src/rt-theme/emotion.ts
+++ src/client/src/rt-theme/emotion.ts
@@ -1,5 +1,12 @@
 import createEmotion from 'create-emotion'
 
+const plugin = (context, content, selectors, parent, line, column, length) => {
+  // Context is -2 for post processed context, before the compiled css output is returned
+  if (context === -2) {
+    console.log('%c POST-PROCESS ', 'background: #222; color: #bada55')
+  }
+}
+
 export const {
   flush,
   hydrate,
@@ -11,4 +18,12 @@ export const {
   css,
   sheet,
   caches
-} = createEmotion({})
+} = createEmotion(
+  {},
+  {
+    stylisPlugins: [plugin]
+  }
+)
+
+//Force emotion to run in "production" mode
+sheet.speedy(true)
