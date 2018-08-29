25e9d44ffe84a0df6ab58c744a526884c547d47d
Bhavesh Desai <bhavesh@weareadaptive.com>
2018-08-29 16:50:36 +0100

    make layout simpler

--- src/client/src/shell/layouts/AppLayout.tsx
+++ src/client/src/shell/layouts/AppLayout.tsx
@@ -18,11 +18,12 @@ class AppLayout extends React.Component<Props> {
     return (
       <AppLayoutRoot>
         {before}
+
         <Header>{header}</Header>
 
         <Body>{body}</Body>
 
-        <Footer>{footer}</Footer>
+        {footer}
         {after}
       </AppLayoutRoot>
     )
@@ -31,31 +32,23 @@ class AppLayout extends React.Component<Props> {
 
 const AppLayoutRoot = styled.div`
   width: 100%;
-  max-width: 100%;
   height: 100%;
   min-height: 100vh;
   max-height: 100vh;
   overflow: hidden;
 
   display: grid;
-  grid-template-rows: minmax(4rem, min-content) calc(100vh - 6rem) minmax(2rem, min-content);
-
+  grid-template-rows: auto 1fr auto;
   background-color: ${({ theme }) => theme.shell.backgroundColor};
   color: ${({ theme }) => theme.shell.textColor};
-
-  justify-content: stretch;
-  align-items: stretch;
 `
 
 const Body = styled.div`
   display: flex;
+  overflow: hidden;
 
   background-color: ${({ theme }) => theme.shell.backgroundColor};
   color: ${({ theme }) => theme.shell.textColor};
-`
-
-const Footer = styled.div`
-  position: relative;
 `
 
 export default AppLayout
