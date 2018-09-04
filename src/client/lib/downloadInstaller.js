ea01115c19e55f6aa3b2189a10c01720e06cb602
Keshal Desai <keshaldesai@gmail.com>
2018-09-04 15:28:44 -0400

    Cleanup

--- src/client/lib/downloadInstaller.js
+++ src/client/lib/downloadInstaller.js
@@ -1,31 +1,27 @@
+const unzip = require('unzip')
+const https = require('https')
+const fs = require('fs')
+const fstream = require('fstream')
 
- const unzip = require('unzip');
+const createinstaller = enviroment => {
+  const fileName = `ReactiveTraderCloud-${enviroment}`
+  const appJSONUrl = `https://raw.githubusercontent.com/AdaptiveConsulting/ReactiveTraderCloud/master/src/client/public/config/openfin/${enviroment}.app.json`
 
- const https = require('https')
- const fs = require('fs')
- const fstream = require('fstream')
- 
- const createinstaller = enviroment => {
-   const fileName = `ReactiveTraderCloud-${enviroment}`
-   const appJSONUrl = `https://raw.githubusercontent.com/AdaptiveConsulting/ReactiveTraderCloud/master/src/client/public/config/openfin/${enviroment}.app.json`
- 
-   const file = fs.createWriteStream(`install/ReactiveTraderCloud-${enviroment}.zip`)
-   const request = https.get(
-     `https://install.openfin.co/download/?config=${appJSONUrl}&fileName=${fileName}`,
-     response =>
-       response.pipe(file).on('finish', () => {
-         var readStream = fs.createReadStream(`install/ReactiveTraderCloud-${enviroment}.zip`),
-         writeStream = fstream.Writer('install');
-         readStream.pipe(unzip.Parse())
-         .pipe(writeStream)
-       
-         writeStream.on('close', () => {
-           fs.unlink(`install/ReactiveTraderCloud-${enviroment}.zip`)
-         });
-       })
-   )
- }
- 
- createinstaller('demo')
- createinstaller('dev')
- 
+  const file = fs.createWriteStream(`install/ReactiveTraderCloud-${enviroment}.zip`)
+  const request = https.get(
+    `https://install.openfin.co/download/?config=${appJSONUrl}&fileName=${fileName}`,
+    response =>
+      response.pipe(file).on('finish', () => {
+        var readStream = fs.createReadStream(`install/ReactiveTraderCloud-${enviroment}.zip`)
+        var writeStream = fstream.Writer('install')
+        readStream.pipe(unzip.Parse()).pipe(writeStream)
+
+        writeStream.on('close', () => {
+          fs.unlink(`install/ReactiveTraderCloud-${enviroment}.zip`)
+        })
+      })
+  )
+}
+
+createinstaller('demo')
+createinstaller('dev')
