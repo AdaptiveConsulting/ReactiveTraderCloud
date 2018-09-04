3478ac9e337bd8b4712b588ed404dc4725898c55
Bhavesh Desai <bhavesh@weareadaptive.com>
2018-09-04 18:10:20 +0100

    auto update openfin installer

--- src/client/lib/downloadInstaller.js
+++ src/client/lib/downloadInstaller.js
@@ -1,10 +1,30 @@
+
+ const unzip = require('unzip');
+
 const https = require('https')
 const fs = require('fs')
+const fstream = require('fstream')
 
-const fileName = 'ReactiveTraderInstaller'
-const appJSONUrl = 'https://raw.githubusercontent.com/AdaptiveConsulting/ReactiveTraderCloud/feature/refactor/src/client/config/openfin/demo.app.json'
+const createinstaller = enviroment => {
+  const fileName = `ReactiveTraderCloud-${enviroment}`
+  const appJSONUrl = `https://raw.githubusercontent.com/AdaptiveConsulting/ReactiveTraderCloud/master/src/client/public/config/openfin/${enviroment}.app.json`
 
-const file = fs.createWriteStream('install/ReactiveTraderCloud-demo.zip')
-const request = https.get(`https://dl.openfin.co/services/download?fileName=${fileName}&config=${appJSONUrl}`, function(response) {
-  response.pipe(file)
-})
+  const file = fs.createWriteStream(`openfin-installers/ReactiveTraderCloud-${enviroment}.zip`)
+  const request = https.get(
+    `https://install.openfin.co/download/?config=${appJSONUrl}&fileName=${fileName}`,
+    response =>
+      response.pipe(file).on('finish', () => {
+        var readStream = fs.createReadStream(`openfin-installers/ReactiveTraderCloud-${enviroment}.zip`),
+        writeStream = fstream.Writer('openfin-installers');
+        readStream.pipe(unzip.Parse())
+        .pipe(writeStream)
+      
+        writeStream.on('close', () => {
+          fs.unlink(`openfin-installers/ReactiveTraderCloud-${enviroment}.zip`)
+        });
+      })
+  )
+}
+
+createinstaller('demo')
+createinstaller('dev')
