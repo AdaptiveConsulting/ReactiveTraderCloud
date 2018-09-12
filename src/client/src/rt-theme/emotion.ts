3d0adacda980958dea37a243890a34618c1e064e
Keshal Desai <keshaldesai@gmail.com>
2018-09-12 16:36:40 -0400

    Enable speedy emotion

--- src/client/src/rt-theme/emotion.ts
+++ src/client/src/rt-theme/emotion.ts
@@ -13,10 +13,6 @@ export const {
   caches
 } = createEmotion({})
 
-/**
- * Until Emotion 10, we need to block Emotion from using speedy mode.
- * This allows us to use a MutationObserver in our portals to watch
- * for changes in style tags.
- */
+//Enable speedy mode for improved dev performance
 
-sheet.speedy(false)
+sheet.speedy(true)
