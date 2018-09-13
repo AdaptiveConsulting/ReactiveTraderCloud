bd0d207c80be87bf3db59df51cfe268e6d4fe2be
Keshal Desai <keshaldesai@gmail.com>
2018-09-13 15:04:22 -0400

    Default props

--- src/client/src/ui/spotTile/components/TileSwitch.tsx
+++ src/client/src/ui/spotTile/components/TileSwitch.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Action } from 'redux'
 import { CurrencyPair } from 'rt-types'
 import { ExecuteTradeRequest, SpotTileData } from '../model'
+import { PriceMovementTypes } from '../model/priceMovementTypes'
 import { TileBooking } from './notifications'
 import NotificationContainer from './notifications'
 import SpotTile from './SpotTile'
@@ -18,6 +19,29 @@ interface Props {
 }
 
 export default class TileSwitch extends Component<Props> {
+  static defaultProps: Partial<Props> = {
+    spotTileData: {
+      isTradeExecutionInFlight: false,
+      price: {
+        ask: 0,
+        bid: 0,
+        mid: 0,
+        creationTimestamp: 0,
+        symbol: '',
+        valueDate: '',
+        priceMovementType: PriceMovementTypes.None,
+        priceStale: false
+      }
+    },
+    currencyPair: {
+      symbol: '',
+      ratePrecision: 0,
+      pipsPosition: 0,
+      base: '',
+      terms: ''
+    }
+  }
+
   render() {
     const {
       currencyPair,
@@ -28,11 +52,10 @@ export default class TileSwitch extends Component<Props> {
       onNotificationDismissed,
       displayCurrencyChart
     } = this.props
-    if (!spotTileData || !spotTileData.price || !currencyPair) {
-      return null
-    }
+
     const { lastTradeExecutionStatus, isTradeExecutionInFlight } = spotTileData
     const isPriceStale = !lastTradeExecutionStatus && spotTileData.price && spotTileData.price.priceStale
+
     return (
       <SpotTile currencyPair={currencyPair} spotTileData={spotTileData} executeTrade={executeTrade}>
         {<TileControls canPopout={tornOff} onPopoutClick={onPopoutClick} displayCurrencyChart={displayCurrencyChart} />}
