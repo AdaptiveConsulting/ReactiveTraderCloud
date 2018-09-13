2ec102029b7084804bbe97ebcc021184f86a74e2
Keshal Desai <keshaldesai@gmail.com>
2018-09-13 16:19:51 -0400

    Removed console.log

--- src/client/src/ui/spotTile/epics/pricingEpics.ts
+++ src/client/src/ui/spotTile/epics/pricingEpics.ts
@@ -1,7 +1,7 @@
 import { Action } from 'redux'
 import { ofType } from 'redux-observable'
 import { applicationDisconnected } from 'rt-actions'
-import { map, mergeMap, takeUntil, tap } from 'rxjs/operators'
+import { map, mergeMap, takeUntil } from 'rxjs/operators'
 import { ApplicationEpic } from 'StoreTypes'
 import { SpotTileActions, TILE_ACTION_TYPES } from '../actions'
 import PricingService from './pricingService'
@@ -14,7 +14,6 @@ export const pricingServiceEpic: ApplicationEpic = (action$, state$, { loadBalan
 
   return action$.pipe(
     ofType<Action, SubscribeToSpotTileAction>(TILE_ACTION_TYPES.SPOT_TILE_SUBSCRIBE),
-    tap(console.log),
     mergeMap((action: SubscribeToSpotTileAction) =>
       pricingService.getSpotPriceStream({
         symbol: action.payload
