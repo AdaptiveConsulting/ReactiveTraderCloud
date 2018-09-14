81d24a179b7ca416064c03ebe60018e3ad3585dc
Keshal Desai <keshaldesai@gmail.com>
2018-09-14 13:49:30 -0400

    Brought back openfin publish

--- src/client/src/ui/spotTile/epics/index.ts
+++ src/client/src/ui/spotTile/epics/index.ts
@@ -2,6 +2,7 @@ import { combineEpics } from 'redux-observable'
 import { closePositionEpic } from './closePosition'
 import { connectCurrencyChartToOpenFinEpic } from './currencyChartEpic'
 import { pricingServiceEpic } from './pricingEpics'
+import { publishPriceToOpenFinEpic } from './publishPrice'
 import { spotTileEpic } from './spotTileEpics'
 import { connectTradeExecutedToOpenFinEpic } from './tradeExecutedEpic'
 
@@ -9,7 +10,7 @@ const epics = [spotTileEpic, pricingServiceEpic]
 
 if (typeof fin !== 'undefined') {
   epics.push(
-    // publishPriceToOpenFinEpic(pricesForCurrenciesInRefData),
+    publishPriceToOpenFinEpic,
     connectTradeExecutedToOpenFinEpic,
     closePositionEpic,
     connectCurrencyChartToOpenFinEpic
