d6f5e464cb8db724e23609a272b6ac4271928910
Evan Schneider <me@evanrs.com>
2018-09-17 14:14:26 -0400

    Remove prefixes from rules

--- src/client/src/routes/StyleguideRoute/styled/rules.ts
+++ src/client/src/routes/StyleguideRoute/styled/rules.ts
@@ -3,18 +3,15 @@ import { css } from 'rt-theme'
 export { mapMarginPaddingProps, MarginPaddingProps } from './mapMarginPaddingProps'
 
 export const preventStutter = css`
-  -webkit-backface-visibility: hidden;
-  -webkit-transform-style: preserve-3d;
+  backface-visibility: hidden;
+  transform-style: preserve-3d;
 `
 
 export const userSelectNone = css`
   -webkit-touch-callout: none;
-  touch-callout: none;
 
   -webkit-user-drag: none;
-  user-drag: none;
 
-  -webkit-user-select: none;
   user-select: none;
 
   cursor: default;
@@ -22,12 +19,9 @@ export const userSelectNone = css`
 
 export const userSelectButton = ({ disabled }: { disabled?: boolean }) => css`
   -webkit-touch-callout: rgba(0, 0, 0, 0.05);
-  touch-callout: none;
 
   -webkit-user-drag: none;
-  user-drag: none;
 
-  -webkit-user-select: none;
   user-select: none;
 
   cursor: pointer;
