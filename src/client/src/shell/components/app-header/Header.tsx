0c2cc4808d4c3ee539ba5012ca0656c9e3b98a26
Evan Schneider <me@evanrs.com>
2018-09-20 12:27:04 -0400

    Set role on non-button button

--- src/client/src/shell/components/app-header/Header.tsx
+++ src/client/src/shell/components/app-header/Header.tsx
@@ -72,7 +72,7 @@ const Fill = styled.div`
   cursor: -webkit-grab;
 `
 
-const IconButton = styled.button<{ type: string }>`
+const IconButton = styled.div<{ type: string }>`
   width: 2rem;
   height: 2rem;
   font-size: 1rem;
@@ -93,6 +93,9 @@ const IconButton = styled.button<{ type: string }>`
     color: ${({ theme }) => theme.button.secondary.textColor};
   }
 `
+IconButton.defaultProps = {
+  role: 'button',
+}
 
 const Division = styled.div`
   height: 100%;
