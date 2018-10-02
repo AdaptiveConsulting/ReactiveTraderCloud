7284b31dd8e527b78e95b6010610f2dd365ca2ee
Evan Schneider <me@evanrs.com>
2018-10-02 11:33:42 -0400

    Add types to Launcher

--- src/client/src/routes/SimpleLauncher/Link.tsx
+++ src/client/src/routes/SimpleLauncher/Link.tsx
@@ -1,36 +1,34 @@
 import React from 'react'
-import { styled, Styled } from 'rt-theme'
+import { styled } from 'rt-theme'
+import { ConfigType } from './config'
 
-declare const global: any
+import { open } from './tools'
 
 export interface LinkProps {
   is?: React.ComponentType<any>
-  to: any
-  [key: string]: any
+  to: ConfigType
 }
 
-const LinkRoot: Styled<{ onClick: any }> = styled.button`
-  width: 100%;
-  height: 100%;
-  flex: 1 1 auto;
-  font-size: 1.5rem;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-decoration: none;
-  color: currentColor;
-  position: relative;
-  z-index: 100;
-
-  &:hover {
-    color: ${({ theme }) => theme.button.accent.backgroundColor};
-  }
-`
-
-export class Link extends React.Component<any> {
+export class Link extends React.Component<LinkProps> {
   static defaultProps = {
-    is: LinkRoot,
+    is: styled.button`
+      width: 100%;
+      height: 100%;
+      flex: 1 1 auto;
+      font-size: 1.5rem;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-decoration: none;
+      color: currentColor;
+      position: relative;
+      z-index: 100;
+
+      &:hover {
+        color: ${({ theme }) => theme.button.accent.backgroundColor};
+      }
+    `,
   }
 
   onClick = (event: MouseEvent) => {
@@ -44,82 +42,4 @@ export class Link extends React.Component<any> {
 
     return <LinkElement onClick={this.onClick}>{children}</LinkElement>
   }
-}
-
-export async function open({ name, url, provider, provider: { options } }: any) {
-  options = { name, url, ...options }
-
-  // under openfin
-  if (typeof fin !== 'undefined') {
-    // open as url through openfin
-    if (provider.platform === 'browser') {
-      const res = fin.desktop.System.openUrlWithBrowser(url)
-      console.log({ res })
-
-      return res
-    }
-    // open new openfin application
-    else if (provider.platform === 'openfin') {
-      let app: any
-      try {
-        console.log('creating app')
-        app = await createApp({
-          ...options,
-          defaultCentered: true,
-          autoShow: true,
-          shadow: true,
-        })
-      } catch (e) {
-        console.log(e)
-      }
-
-      console.log({ app })
-
-      try {
-        console.log(
-          /// run
-          await new Promise((resolve, reject) => app.run(resolve, reject)),
-        )
-      } catch (e) {
-        console.log(e)
-      }
-
-      return app
-    }
-  }
-  // open as url
-  else {
-    global.open(url, name)
-  }
-}
-
-export function createApp(options: any) {
-  return new Promise((resolve, reject) => {
-    const window: any = new fin.desktop.Application(
-      {
-        name: options.name,
-        uuid: options.name,
-        nonPersistent: false,
-        mainWindowOptions: options,
-      } as any,
-      () => resolve(window),
-      reject,
-    )
-  })
-}
-
-export function createWindow(options: any) {
-  return new Promise((resolve, reject) => {
-    const window: any = new fin.desktop.Window(
-      {
-        ...options,
-        uuid: options.name,
-        defaultCentered: true,
-        autoShow: true,
-        shadow: true,
-      },
-      () => resolve(window),
-      reject,
-    )
-  })
 }
--- src/client/src/routes/SimpleLauncher/config.tsx
+++ src/client/src/routes/SimpleLauncher/config.tsx
@@ -1,6 +1,8 @@
-import { faChartArea, faExchangeAlt } from '@fortawesome/free-solid-svg-icons'
+import { faChartArea, faExchangeAlt, faWindowRestore } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+
+export type ConfigType = Partial<typeof config[0]>
 
 export const config = [
   {
@@ -9,6 +11,7 @@ export const config = [
     icon: <FontAwesomeIcon icon={faExchangeAlt} />,
     provider: {
       platform: 'openfin',
+      as: 'application',
       options: {
         autoShow: true,
         defaultWidth: 1280,
@@ -28,6 +31,7 @@ export const config = [
       },
     },
   },
+
   {
     name: 'Reactive Analytics',
     url: 'https://demo-reactive-analytics.adaptivecluster.com/',
@@ -36,29 +40,37 @@ export const config = [
       platform: 'browser',
     },
   },
-  // {
-  //   name: 'Reactive Analytics 2',
-  //   url: 'https://demo-reactive-analytics.adaptivecluster.com/',
-  //   icon: <FontAwesomeIcon icon={faChartPie} />,
-  //   provider: {
-  //     platform: 'openfin',
-  //     options: {
-  //       autoShow: true,
-  //       defaultWidth: 1280,
-  //       defaultHeight: 900,
-  //       minWidth: 800,
-  //       minHeight: 600,
-  //       resizable: true,
-  //       maximizable: true,
-  //       frame: false,
-  //       nonPersistent: true,
-  //       accelerator: {
-  //         devtools: true,
-  //         reload: true,
-  //         reloadIgnoringCache: true,
-  //         zoom: true,
-  //       },
-  //     },
-  //   },
-  // },
 ]
+
+// Demo of opening as window instead of Application
+//
+// However, this does not work as the app can quit
+// on behalf of the launcher
+if (process.env.NODE_ENV === 'development') {
+  config.push({
+    name: 'Reactive Trader as Window',
+    url: 'http://localhost:3000/',
+    icon: <FontAwesomeIcon icon={faWindowRestore} />,
+    provider: {
+      platform: 'openfin',
+      as: 'window',
+      options: {
+        autoShow: true,
+        defaultWidth: 1280,
+        defaultHeight: 900,
+        minWidth: 800,
+        minHeight: 600,
+        resizable: true,
+        maximizable: true,
+        frame: false,
+        nonPersistent: true,
+        accelerator: {
+          devtools: true,
+          reload: true,
+          reloadIgnoringCache: true,
+          zoom: true,
+        },
+      },
+    },
+  })
+}
