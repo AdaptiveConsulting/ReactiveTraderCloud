af36ff4f7b30326cb7181cca0b3bcfaa4e2fdfc5
Evan Schneider <me@evanrs.com>
2018-10-31 16:42:43 -0400

    Add immediate option to <Timer/>

--- src/client/src/routes/OrderTicketRoute/Timer.tsx
+++ src/client/src/routes/OrderTicketRoute/Timer.tsx
@@ -3,17 +3,30 @@ import { PureComponent } from 'react'
 export interface TimerProps {
   name?: string
   duration: number
+  immediate?: boolean
   timeout?: (name?: string) => any
   interval?: (name?: string) => any
   children?: (name?: string) => any
 }
 
-export class Timer extends PureComponent<TimerProps, { id: NodeJS.Timer }> {
-  id = (this.props.interval ? setInterval : setTimeout)(() => {
+export class Timer extends PureComponent<TimerProps> {
+  id: NodeJS.Timer
+
+  callback = () => {
     const { name, children, interval, timeout } = this.props
 
     return (children || interval || timeout)(name)
-  }, this.props.duration)
+  }
+
+  componentDidMount() {
+    const { interval, duration, immediate } = this.props
+
+    this.id = (interval ? setInterval : setTimeout)(this.callback, duration)
+
+    if (immediate) {
+      this.callback()
+    }
+  }
 
   componentWillUnmount() {
     const { interval } = this.props
