dc25f821917c8a14bb3f7b006874666364a441e1
Keshal Desai <keshaldesai@gmail.com>
2018-11-08 16:55:27 -0500

    Use platform for OpenFinRoute

--- src/client/src/shell/Router.tsx
+++ src/client/src/shell/Router.tsx
@@ -1,22 +1,21 @@
 import React, { SFC } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import { Environment } from 'rt-components'
+import { PlatformAdapter, withPlatform } from 'rt-components'
 import { AnalyticsRoute, BlotterRoute, OpenFinRoute, ShellRoute, SpotRoute } from './routes'
+
+const ShellSwitchRoute = ({ header, platform }: { header: React.ReactChild; platform: PlatformAdapter }) =>
+  platform.type === 'openfin' ? <OpenFinRoute /> : <ShellRoute header={header} />
+
+const ShellSwitchRouteWithPlatform = withPlatform(ShellSwitchRoute)
 
 export const Router: SFC = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={ShellSwitchRoute} />
+      <Route exact path="/" component={ShellSwitchRouteWithPlatform} />
       <Route path="/analytics" component={AnalyticsRoute} />
       <Route path="/blotter" component={BlotterRoute} />
       <Route path="/spot/:symbol" component={SpotRoute} />
     </Switch>
   </BrowserRouter>
-)
-
-const ShellSwitchRoute = ({ header }: { header: React.ReactChild }) => (
-  <Environment.Consumer>
-    {({ openfin }) => (openfin ? <OpenFinRoute /> : <ShellRoute header={header} />)}
-  </Environment.Consumer>
 )
