6be199a471fc29ba167d1f6860cad73295856d3c
Keshal Desai <keshaldesai@gmail.com>
2018-11-08 09:23:57 -0500

    Change which platform is chosen

--- src/client/src/rt-components/platform/index.ts
+++ src/client/src/rt-components/platform/index.ts
@@ -1,4 +1,4 @@
-export { Platform } from './platform'
+export { default as Platform } from './platform'
 export { PlatformAdapter } from './platformAdapter'
 export { PlatformProvider } from './context'
 export { withPlatform } from './withPlatform'
--- src/client/src/rt-components/platform/platform.ts
+++ src/client/src/rt-components/platform/platform.ts
@@ -3,11 +3,13 @@ import OpenFin from './openfin/openFin'
 
 const isOpenFin = typeof fin !== 'undefined'
 
-export const Platform = (() => {
+const getPlatform = () => {
   if (isOpenFin) {
     console.log('Using OpenFin API')
     return OpenFin
   }
   console.log('Using Browser API')
   return Browser
-})()
+}
+
+export default getPlatform()
