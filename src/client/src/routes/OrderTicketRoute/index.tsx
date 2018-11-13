8ffac32ef92485f189d33fa4b987024a78abad6a
Keshal Desai <keshaldesai@gmail.com>
2018-11-13 11:04:11 -0500

    Add platform to OrderTicketRoute

--- src/client/src/routes/OrderTicketRoute/index.tsx
+++ src/client/src/routes/OrderTicketRoute/index.tsx
@@ -1,14 +1,19 @@
 import React, { PureComponent } from 'react'
 
+import { Platform, PlatformProvider } from 'rt-components'
 import { ThemeName, ThemeStorage } from 'rt-theme'
 
 import PlainOrderTicketRoute from './OrderTicketRoute'
+
+const platform = new Platform()
 
 export class OrderTicketRoute extends PureComponent {
   render() {
     return (
       <ThemeStorage.Provider storage={sessionStorage} default={ThemeName.Dark}>
-        <PlainOrderTicketRoute />
+        <PlatformProvider value={platform}>
+          <PlainOrderTicketRoute />
+        </PlatformProvider>
       </ThemeStorage.Provider>
     )
   }
