aaa14561821da04ff85cbe195be363eb7261b368
Pierre de Leastar <pdeleastar@gmail.com>
2018-11-27 11:10:37 +0100

    refactor: excel interop made more modular

--- src/client/src/rt-components/platform/adapters/openfin/excel/index.ts
+++ src/client/src/rt-components/platform/adapters/openfin/excel/index.ts
@@ -5,23 +5,49 @@ interface ExcelInterface {
   initExcel: () => void
 }
 
-const Excel: ExcelInterface = {
+const ExcelModule: ExcelInterface = {
   workbook: null,
   worksheet: null,
 
   initExcel: () => {
     // @ts-ignore
-    fin.desktop.ExcelService.init().then(() => {
+    const { ExcelService } = fin.desktop
+
+    // @ts-ignore
+    if (!fin.desktop.Excel) {
+      ExcelService.init().catch((err: string) => console.log(err))
+    }
+
+    ExcelService.addEventListener('excelConnected', () => {
+      console.log('excel init')
       // @ts-ignore
-      const ExcelInstance = fin.desktop.Excel
-      ExcelInstance.addWorkbook().then((res: any) => (this.workbook = res))
+      const { Excel } = fin.desktop
+      Excel.getWorkbooks((workbooks: any) => {
+        if (!workbooks.length) {
+          Excel.addWorkbook().then((res: any) => {
+            this.workbook = res
+          })
+        } else {
+          this.workbook = workbooks[0]
+        }
+      })
+    })
+
+    ExcelService.addEventListener('excelDisconnected', (data: any) => {
+      console.log('Excel Disconnected: ' + data.connectionUuid)
     })
   },
 
   publishExcel: (message: any) => {
-    if (this.workbook) {
-      this.workbook.getWorksheets((res: any) => (this.worksheet = res[0]))
+    // Forming worksheet if a workbook exist
+    if (!this.worksheet && this.workbook) {
+      this.workbook.getWorksheets((res: any) => {
+        this.worksheet = res[0]
+        styleWorksheet(this.worksheet)
+      })
     }
+
+    // If a worksheet exists and a message is received
     if (this.worksheet && message) {
       const keys = Object.keys(message[0])
       const values = message.map((item: any) => Object.values(item))
@@ -30,4 +56,12 @@ const Excel: ExcelInterface = {
   },
 }
 
-export default Excel
+async function styleWorksheet(worksheet: any) {
+  await worksheet.formatRange('A1:K1', {
+    border: { color: '0,0,0,1', style: 'continuous' },
+    font: { color: '100,100,100,1', size: 12, bold: true, name: 'Verdana' },
+    fill: { color: '#4472C4' },
+  })
+}
+
+export default ExcelModule
