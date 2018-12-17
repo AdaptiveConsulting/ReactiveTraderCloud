d509ceb18acbf837940261771a876b1153e5aeb7
Keshal Desai <keshaldesai@gmail.com>
2018-12-17 15:52:26 -0500

    Workspace using sc

--- src/client/src/ui/workspace/Workspace.tsx
+++ src/client/src/ui/workspace/Workspace.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { TearOff } from 'rt-components'
-import { styled } from 'rt-theme'
+import { styled } from 'test-theme'
 import SpotTileContainer from '../spotTile/SpotTileContainer'
 import { PortalProps } from './selectors'
 
