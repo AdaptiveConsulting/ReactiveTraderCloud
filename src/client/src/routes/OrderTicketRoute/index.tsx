6e43f6d8098ecbf2508d70510f24f79f0cc4bbbf
Keshal Desai <keshaldesai@gmail.com>
2018-12-19 12:29:42 -0500

    Switch to sc for order ticket

--- src/client/src/routes/OrderTicketRoute/index.tsx
+++ src/client/src/routes/OrderTicketRoute/index.tsx
@@ -1,16 +1,35 @@
 import { mix } from 'polished'
-import React, { PureComponent } from 'react'
+import React, { PureComponent, FunctionComponent } from 'react'
 
 import { Platform, PlatformProvider } from 'rt-components'
 import { TestThemeProvider, Theme } from 'test-theme'
 
 import { OrderTicket } from './OrderTicket'
 import { ThemeProvider } from 'styled-components'
-import { withTheme } from 'emotion-theming'
+import { withTheme } from 'styled-components'
 
 const platform = new Platform()
 
-class OrderTicketRoute extends PureComponent<{ theme: Theme }, { instance: number }> {
+interface Props {
+  theme: Theme
+  reset: () => void
+  instance: number
+}
+
+const BaseOrderTicket: FunctionComponent<Props> = ({ theme, reset, instance }) => (
+  <ThemeProvider
+    theme={{
+      muteColor: mix(0.5, theme.primary.base, theme.secondary[2]),
+      ruleColor: theme.primary.base,
+    }}
+  >
+    <OrderTicket key={'OrderTicket' + instance} reset={reset} />
+  </ThemeProvider>
+)
+
+const OrderTicketWithTheme = withTheme(BaseOrderTicket)
+
+export default class OrderTicketRoute extends PureComponent<{}, { instance: number }> {
   state = {
     instance: 0,
   }
@@ -18,24 +37,12 @@ class OrderTicketRoute extends PureComponent<{ theme: Theme }, { instance: numbe
   reset = () => this.setState(({ instance }) => ({ instance: instance + 1 }))
 
   render() {
-    const { theme } = this.props
     return (
       <TestThemeProvider storage={sessionStorage}>
         <PlatformProvider value={platform}>
-          <ThemeProvider
-            theme={{
-              muteColor: mix(0.5, theme.primary.base, theme.secondary[2]),
-              ruleColor: theme.primary.base,
-            }}
-          >
-            <OrderTicket key={'OrderTicket' + this.state.instance} reset={this.reset} />
-          </ThemeProvider>
+          <OrderTicketWithTheme instance={this.state.instance} reset={this.reset} />
         </PlatformProvider>
       </TestThemeProvider>
     )
   }
 }
-
-export default withTheme(OrderTicketRoute)
-
-//withtheme
