54ddedb4015922b58385c43df7900892c1272bdd
Dave Sexton <dave.sexton@weareadaptive.com>
2019-01-08 15:49:56 -0500

    ARTP-397 Fixed blotter popup background color.

--- src/client/src/shell/routes/BlotterRoute.tsx
+++ src/client/src/shell/routes/BlotterRoute.tsx
@@ -7,7 +7,7 @@ const BlotterContainerStyle = styled('div')`
   height: 100%;
   padding-left: 10px;
   padding-right: 10px;
-  background-color: ${({ theme }) => theme.blotter.backgroundColor};
+  background-color: ${({ theme }) => theme.core.darkBackground};
 `
 
 const BlotterRoute = () => (
