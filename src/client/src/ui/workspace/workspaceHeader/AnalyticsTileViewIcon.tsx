aae95d635e9d3c37d73fce1d1da086ba70e36366
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2019-01-10 15:00:50 +0000

    improved the style of analyticsViewIcon

--- src/client/src/ui/workspace/workspaceHeader/AnalyticsTileViewIcon.tsx
+++ src/client/src/ui/workspace/workspaceHeader/AnalyticsTileViewIcon.tsx
@@ -1,30 +1,53 @@
 import React from 'react'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquare } from '@fortawesome/free-solid-svg-icons'
+import { faChartLine } from '@fortawesome/free-solid-svg-icons'
 import { styled } from 'rt-theme'
 
-library.add(faSquare)
+library.add(faChartLine)
 
 const Foo = styled.div`
   display: flex;
   justify-content: space-between;
 `
-const HR = styled.hr`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const Rect = styled.div`
+  background-color: transparent;
+  border: 1px solid white;
+  width: 10px;
+  height: 10px;
+  margin-bottom: 2px;
+`
+//TODO I must remove this one here
+export const HR = styled.hr`
   width: 80%;
   margin: 0 auto;
   height: 2px;
   background-color: black;
 `
 
+const ChartLineIconWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
 const AnalyticsViewIcon: React.SFC = () => (
-  <div>
-    <Foo>
-      <FontAwesomeIcon icon="square" />
-      <FontAwesomeIcon icon="square" />
-    </Foo>
-    <HR />
-  </div>
+  <Foo>
+    <ChartLineIconWrapper>
+      <FontAwesomeIcon icon="chart-line" />
+    </ChartLineIconWrapper>
+    <Wrapper>
+      <Rect />
+      <Rect />
+    </Wrapper>
+  </Foo>
 )
 
 export default AnalyticsViewIcon
