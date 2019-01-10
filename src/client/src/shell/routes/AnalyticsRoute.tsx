19ad4fb912d377c942875b55da8e8bb6ede6670d
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2019-01-10 12:44:34 +0000

    added padding to AnalyticsComponent when in pop up

--- src/client/src/shell/routes/AnalyticsRoute.tsx
+++ src/client/src/shell/routes/AnalyticsRoute.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { RouteWrapper } from 'rt-components'
 import { AnalyticsContainer } from '../../ui/analytics'
+import { styled } from 'rt-theme'
 
+const AnalyticsStyle = styled.div`
+  height: 100%;
+  padding: 10px;
+  background-color: ${({ theme }) => theme.core.darkBackground};
+  overflow: auto;
+`
 const AnalyticsRoute = () => (
   <RouteWrapper>
-    <AnalyticsContainer />
+    <AnalyticsStyle>
+      <AnalyticsContainer />
+    </AnalyticsStyle>
   </RouteWrapper>
 )
 
