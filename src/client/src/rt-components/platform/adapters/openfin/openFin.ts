a6a7d42a8511d176d5ae6e061d23a78e9f7c8b39
Keshal Desai <keshaldesai@gmail.com>
2019-01-16 15:46:49 -0500

    lower notification time

--- src/client/src/rt-components/platform/adapters/openfin/openFin.ts
+++ src/client/src/rt-components/platform/adapters/openfin/openFin.ts
@@ -73,7 +73,7 @@ export default class OpenFin implements PlatformAdapter {
       new fin.desktop.Notification({
         url: '/notification',
         message,
-        timeout: 20000,
+        timeout: 8000,
       } as any),
   }
 }
