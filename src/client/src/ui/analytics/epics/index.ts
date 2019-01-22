7683f0366c6a51beb8e9c519bccf23e3fa6d3adb
Pierre de Leastar <pdeleastar@gmail.com>
2019-01-22 12:02:33 +0100

    added types

--- src/client/src/ui/analytics/epics/index.ts
+++ src/client/src/ui/analytics/epics/index.ts
@@ -1,9 +1,10 @@
 import { combineEpics } from 'redux-observable'
 import { publishPositionUpdateEpic } from './analyticsServiceEpic'
 import { analyticsServiceEpic } from './epics'
+import { InteropServices, PlatformAdapter } from 'rt-components'
 
-export default ({ platform }: any) => {
-  const { interopServices } = platform
+export default ({ platform }: { platform: PlatformAdapter }) => {
+  const { interopServices }: { interopServices: InteropServices } = platform
   const epics = [analyticsServiceEpic]
 
   if (interopServices.excel) {
