1a33316c9d1e60e63b08c90357992e23c32676b8
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2019-01-29 09:43:14 +0000

    removed redeclaration of config in config-overrides.js

--- src/client/config-overrides.js
+++ src/client/config-overrides.js
@@ -1,6 +1,5 @@
-const rewireStyledComponents = require('react-app-rewire-styled-components');
+const rewireStyledComponents = require('react-app-rewire-styled-components')
 
 module.exports = function override(config, env) {
-    config = rewireStyledComponents(config, env);
-    return config;
+    return rewireStyledComponents(config, env);
 }
