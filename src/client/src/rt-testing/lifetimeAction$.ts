944b11287642ed11392e9d11e28c6dfbd847c594
Dave Sexton <dave.sexton@weareadaptive.com>
2019-01-31 13:03:23 -0500

    Small improvements to doc comments

--- src/client/src/rt-testing/lifetimeAction$.ts
+++ src/client/src/rt-testing/lifetimeAction$.ts
@@ -18,14 +18,13 @@ import { Action } from 'rt-util'
  *   Valid: '--C------D----'     -- Generates the app connected action, followed eventually by the disconnected action.
  *   Valid: '-C----1-D|'         -- App connected, then custom action #1 is eventually generated, and finally the disconnected action.
  *   Valid: '-C----1---'         -- App connected, then custom action #1 is eventually generated, and no further actions appear.
- *   Valid: '--C--1--2--3--D--'  -- App connected, then custom actions ##1, 2, 3, and finally the disconnected action.
+ *   Valid: '--C--1--2--3--D--'  -- App connected, then custom actions ##1, 2, 3, and then the disconnected action.
  *   Valid: '---1--C---'         -- Custom action #1, then the app connected action.
- *   Valid: '--1--C--2--'        -- Custom action #1, then the app connected action, and finally custom action #2.
+ *   Valid: '--1--C--2--'        -- Custom action #1, then the app connected action, and then custom action #2.
  *
  * Invalid: '--C--0--D--'        -- Custom actions' marbles must start at 1
  * Invalid: '--C--1--4--'        -- Custom actions' marbles must increase monotonically; e.g., 1, 2, 3, 4, 5...
  * Invalid: '--C--a--b--e--D--'  -- Custom actions' marbles cannot be letters.
- * Invalid: '--C--C--1--2--D--'  -- Only one 'C' marble is permitted. Likewise, only one 'D' marble is permitted as well.
  *
  */
 export enum AppLifetimeEvent {
