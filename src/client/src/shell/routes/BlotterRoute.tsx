b97fe21d6afefa6ef4bb919f1438de724be67c79
Pierre de Leastar <pdeleastar@gmail.com>
2019-02-06 17:45:40 +0100

    remove min height + width

--- src/client/src/shell/routes/BlotterRoute.tsx
+++ src/client/src/shell/routes/BlotterRoute.tsx
@@ -4,9 +4,7 @@ import { BlotterContainer } from '../../ui/blotter'
 import { styled } from 'rt-theme'
 
 const BlotterContainerStyle = styled('div')`
-  min-height: 28.125rem;
   height: 100%;
-  min-width: 53.125rem;
   width: 100%;
   padding: 0.625rem;
   margin: auto;
