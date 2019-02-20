cd5f34623795c1a32af01856335e9157acba4004
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2019-02-20 17:23:44 +0000

    added tests for analyticsService

--- src/client/src/ui/analytics/analyticsService.test.ts
+++ src/client/src/ui/analytics/analyticsService.test.ts
@@ -4,11 +4,6 @@ import { MockServiceClient } from 'rt-system/__mocks__'
 import { PositionsRaw } from './model/index'
 import { map } from 'rxjs/operators'
 
-const position = {
-  CurrentPositions: [],
-  History: [],
-}
-
 const positionRaw = {
   CurrentPositions: [],
   History: [],
@@ -23,7 +18,7 @@ describe('AnalyticsService getAnalyticsStream', () => {
     const actionReference = {
       p: positionRaw,
     }
-    new MockScheduler().run(({ cold, expectObservable, flush }) => {
+    new MockScheduler().run(({ cold, expectObservable }) => {
       const actionLifetime = '--p--'
       const createStreamOperation$ = jest.fn((s: string, o: string, r: any) =>
         cold<PositionsRaw>(actionLifetime, actionReference),
@@ -33,11 +28,33 @@ describe('AnalyticsService getAnalyticsStream', () => {
       const epics$ = analyticsService
         .getAnalyticsStream('USD')
         .pipe(map(({ currentPositions, history }) => currentPositions.length == 0 && history.length == 0))
+
       expectObservable(epics$).toBe('--p--', { p: true })
       expect(createStreamOperation$).toHaveBeenCalled()
-      expect(1).toBe(1)
+      expect(createStreamOperation$).toHaveBeenCalledWith('analytics', 'getAnalytics', 'USD')
     })
   })
 
-  it('Should call createStreamOperation and correctly map positionRaw to positionUpdates', () => {})
+  it('Should correctly map positionRaw to positionUpdates', () => {
+    const actionReference = {
+      p: {
+        CurrentPositions: [{ Symbol: 'AAPL', BasePnl: 5, BaseTradedAmount: 5 }],
+        History: [{ Timestamp: '2019-02-20', UsdPnl: 5 }],
+      },
+    }
+    new MockScheduler().run(({ cold, expectObservable }) => {
+      const actionLifetime = '--p--'
+      const createStreamOperation$ = jest.fn((s: string, o: string, r: any) =>
+        cold<PositionsRaw>(actionLifetime, actionReference),
+      )
+
+      const serviceClient = new MockServiceClient(createStreamOperation$)
+      const analyticsService = new AnalyticsService(serviceClient)
+      const epics$ = analyticsService
+        .getAnalyticsStream('USD')
+        .pipe(map(({ currentPositions }) => currentPositions[0].basePnlName === 'basePnl'))
+
+      expectObservable(epics$).toBe('--p--', { p: true })
+    })
+  })
 })
