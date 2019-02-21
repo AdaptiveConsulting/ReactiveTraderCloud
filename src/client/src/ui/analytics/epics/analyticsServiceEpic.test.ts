c61228f4d41fb22cdea5c0e07c959c7715e20455
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2019-02-21 10:54:10 +0000

    removed a test from analyticsServiceEpic.test

--- src/client/src/ui/analytics/epics/analyticsServiceEpic.test.ts
+++ src/client/src/ui/analytics/epics/analyticsServiceEpic.test.ts
@@ -10,7 +10,10 @@ const MockPlatformAdapter = jest.fn<PlatformAdapter>(() => ({
   interop: {
     subscribe: (sender: string, topic: string, listener: () => void) => jest.fn(() => 'values'),
     unsubscribe: (sender: string, topic: string, listener: () => void) => {},
-    publish: jest.fn((topic: string, message: any) => {}),
+    publish: (topic: string, message: any) => {},
+    excel: {
+      publish: jest.fn((topic: string, message: any) => topic),
+    },
   },
 }))
 
@@ -39,30 +42,25 @@ describe('publishPositionUpdateEpic', () => {
       expectObservable(epics$).toBe(expectedAction)
       flush()
 
-      expect(platform.interop!.publish).toHaveBeenCalledTimes(0)
+      expect(platform.interop!.excel!.publish).toHaveBeenCalledTimes(0)
     })
   })
 
-  it.only('should correctly map currencyPairPosition if action is of type FetchAnalytics ', () => {
+  it('should call platform publish on FetchAnalyticsAction with arguments publishUpdate and currentPositions', () => {
     const testScheduler = new MockScheduler()
     const platform = new MockPlatformAdapter()
     const currencyPairPos = {
       symbol: 'AAPL',
       basePnl: 10,
       baseTradedAmount: 120,
-      basePnlName: 'basePnl' as 'basePnl',
-      baseTradedAmountName: 'baseTradedAmount' as 'baseTradedAmount',
+      basePnlName: 'basePnl',
+      baseTradedAmountName: 'baseTradedAmount',
     }
 
     const payload: PositionUpdates = {
       currentPositions: [currencyPairPos],
       history: [],
     }
-    const expected = {
-      symbol: currencyPairPos.symbol,
-      basePnl: currencyPairPos.basePnl,
-      baseTradedAmount: currencyPairPos.baseTradedAmount,
-    }
 
     const actionReference = {
       a: AnalyticsActions.fetchAnalytics(payload),
@@ -80,36 +78,8 @@ describe('publishPositionUpdateEpic', () => {
       expectObservable(epics$).toBe(expectedAction)
       flush()
 
-      expect(platform.interop!.publish).toHaveBeenCalledTimes(1)
-      expect(platform.interop!.publish).toHaveBeenCalledWith(Update, [expected])
-    })
-  })
-
-  it('should call platform publish on FetchAnalyticsAction with arguments publishUpdate and currentPositions', () => {
-    const testScheduler = new MockScheduler()
-    const payload: PositionUpdates = {
-      currentPositions: [],
-      history: [],
-    }
-    const platform = new MockPlatformAdapter()
-    const actionReference = {
-      a: AnalyticsActions.fetchAnalytics(payload),
-    }
-
-    testScheduler.run(({ cold, expectObservable, flush }) => {
-      const actionLifetime = '--a--|'
-      const expectedAction = '-----|'
-
-      const coldAction = cold<Action<any>>(actionLifetime, actionReference)
-      const action$ = ActionsObservable.from(coldAction, testScheduler)
-
-      const epics$ = publishPositionUpdateEpic(action$, undefined, { platform })
-
-      expectObservable(epics$).toBe(expectedAction)
-      flush()
-
-      expect(platform.interop!.publish).toHaveBeenCalledTimes(1)
-      expect(platform.interop!.publish).toHaveBeenCalledWith(Update, [])
+      expect(platform.interop!.excel!.publish).toHaveBeenCalledTimes(1)
+      expect(platform.interop!.excel!.publish).toHaveBeenCalledWith(Update, [currencyPairPos])
     })
   })
 })
