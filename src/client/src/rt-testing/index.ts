5d7b4570efe791103a45ee7d6a3662bf44945b67
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2019-02-21 17:50:17 +0000

    removed lifetime events; removed serviceStubsMock; redid the tests

--- src/client/src/rt-testing/index.ts
+++ src/client/src/rt-testing/index.ts
@@ -1,3 +1,1 @@
 export { MockScheduler } from './MockScheduler'
-export { createTestScheduler, fromMarbles, fromActionsToMarbles } from './marbles'
-export { mockLifetimeAction$, mockAction$, AppLifetimeEvent } from './lifetimeAction$'
