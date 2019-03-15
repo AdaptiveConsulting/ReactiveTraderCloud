1a0d960e953e80f8fc605139e95287ab92e029dd
spcfran <f.x.rodriguez@hotmail.com>
2019-03-15 18:24:27 +0000

    Minor amendments to openfin excel typings

--- src/client/src/type-declarations/openfin-excel/ExcelWorkbook.d.ts
+++ src/client/src/type-declarations/openfin-excel/ExcelWorkbook.d.ts
@@ -1,9 +1,16 @@
+/*
+!!!
+These declaration files were obtained from https://github.com/openfin/excel-api-example and amended for better typing
+!!!
+*/
+
 import { RpcDispatcher } from './RpcDispatcher';
 import { ExcelApplication } from './ExcelApplication';
 import { ExcelWorksheet } from './ExcelWorksheet';
 export declare class ExcelWorkbook extends RpcDispatcher {
     application: ExcelApplication;
     workbookName: string;
+    name?: string; // the `toObject` version uses this instead of workbookName!
     worksheets: {
         [worksheetName: string]: ExcelWorksheet;
     };
--- src/client/src/type-declarations/openfin-excel/index.d.ts
+++ src/client/src/type-declarations/openfin-excel/index.d.ts
@@ -1,4 +1,9 @@
-// These declaration files were obtained from https://github.com/openfin/excel-api-example and amended for better typing
+/*
+!!!
+These declaration files were obtained from https://github.com/openfin/excel-api-example and amended for better typing
+!!!
+*/
+
 
 export { ExcelService } from './ExcelService'
 export { ExcelApplication } from './ExcelApplication'
