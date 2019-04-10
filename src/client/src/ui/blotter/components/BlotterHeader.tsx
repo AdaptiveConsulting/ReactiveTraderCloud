64c3fd4505012683241981503886fc3081e74deb
Matthew <matthew.donovan@mac.com>
2019-04-10 15:47:02 +0100

    style: destructured props

--- src/client/src/ui/blotter/components/BlotterHeader.tsx
+++ src/client/src/ui/blotter/components/BlotterHeader.tsx
@@ -1,5 +1,5 @@
 import { GridApi } from 'ag-grid'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, FC } from 'react'
 import { flexStyle } from 'rt-components'
 import { PopoutIcon } from 'rt-components'
 import { styled } from 'rt-theme'
@@ -44,55 +44,50 @@ const Fill = styled.div`
   height: 1rem;
 `
 
-const BlotterHeader = (props: Props) => {
+const BlotterHeader: FC<Props> = ({ gridApi, canPopout, onExportToExcelClick, onPopoutClick }) => {
   const [quickFilterText, setQuickFilterText] = useState('')
 
   const quickFilterChangeHandler = useCallback(
     event => {
       setQuickFilterText(event.currentTarget.value)
-      return props.gridApi && props.gridApi.setQuickFilter(event.currentTarget.value)
+      return gridApi && gridApi.setQuickFilter(event.currentTarget.value)
     },
-    [setQuickFilterText, props.gridApi],
+    [setQuickFilterText, gridApi],
   )
 
   const removeQuickFilter = useCallback(() => {
-    if (!props.gridApi) {
+    if (!gridApi) {
       return
     }
-    props.gridApi.setQuickFilter(null)
-    props.gridApi.onFilterChanged()
+    gridApi.setQuickFilter(null)
+    gridApi.onFilterChanged()
     setQuickFilterText('')
-  }, [props.gridApi, setQuickFilterText])
+  }, [gridApi, setQuickFilterText])
 
-  const removeAllFilters = useCallback(() => {
-    return props.gridApi && props.gridApi.setFilterModel(null)
-  }, [props.gridApi])
+  const removeAllFilters = useCallback(() => gridApi && gridApi.setFilterModel(null), [gridApi])
 
-  const removeFilter = useCallback(
-    (key: string) => {
-      return props.gridApi && props.gridApi.destroyFilter(key)
-    },
-    [props.gridApi],
-  )
+  const removeFilter = useCallback((key: string) => gridApi && gridApi.destroyFilter(key), [
+    gridApi,
+  ])
 
   return (
     <BlotterHeaderStyle>
       <BlotterLeft>Executed Trades</BlotterLeft>
       <BlotterRight>
-        <ExcelButton onClick={props.onExportToExcelClick} />
+        <ExcelButton onClick={onExportToExcelClick} />
         <BlotterToolbar
           isQuickFilterApplied={quickFilterText.length !== 0}
           quickFilterChangeHandler={quickFilterChangeHandler}
           removeQuickFilter={removeQuickFilter}
           removeAllFilters={removeAllFilters}
           removeFilter={removeFilter}
-          filterModel={props.gridApi ? props.gridApi.getFilterModel() : null}
+          filterModel={gridApi ? gridApi.getFilterModel() : null}
           columnDefinitions={columnDefinitions}
         />
-        {props.canPopout && (
+        {canPopout && (
           <React.Fragment>
             <Fill />
-            <BlotterControls onClick={props.onPopoutClick}>
+            <BlotterControls onClick={onPopoutClick}>
               <PopoutIcon width={0.8125} height={0.75} />
             </BlotterControls>
           </React.Fragment>
