f98e0b08dc52a6fad5414e0b948c6a5b6f7488e3
Taylor Mitchell-St Joseph <taylor@weareadaptive.com>
2019-05-17 10:29:51 +0100

    Updated Workspace header rates selector responsive styles

--- src/client/src/ui/workspace/workspaceHeader/styled.tsx
+++ src/client/src/ui/workspace/workspaceHeader/styled.tsx
@@ -26,6 +26,9 @@ export const LeftNavItemFirst = styled(LiStyle)`
   list-style-type: none;
   margin-right: 30px;
   font-size: 16px;
+  @media (max-width: 768px) {
+    margin-right: 5px;
+  }
 `
 
 export const NavItem = styled(LiStyle)<{ active: boolean }>`
