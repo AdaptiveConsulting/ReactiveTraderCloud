d7b1aa94b750a3e98b89feb4c422fcf01794d554
James Livolsi <jameslivolsi@Jamess-MacBook-Pro.local>
2019-08-08 10:22:08 -0400

    Tweak create react app webpack config

---
+++ src/client/allowEslintrc.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const reactScriptsWebpackConfig = path.join(__dirname, 'node_modules', 'react-scripts', 'config', 'webpack.config.js');
+const webpack = fs.readFileSync(reactScriptsWebpackConfig, 'utf8');
+const customConfig = webpack.replace(/(?<=useEslintrc:\s)false/gm, 'true');
+
+fs.writeFile(reactScriptsWebpackConfig, customConfig, 'utf8', function(err) {
+    if (err) {
+        process.exit(1);
+    }
+    process.exit(0);
+});
