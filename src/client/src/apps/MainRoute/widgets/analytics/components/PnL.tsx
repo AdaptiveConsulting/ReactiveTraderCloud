9c1714c31f91d02c43e805059beeb3f10b99d5b5
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-09-06 15:38:43 -0400

    remove unused styled components

--- src/client/src/apps/MainRoute/widgets/analytics/components/PnL.tsx
+++ src/client/src/apps/MainRoute/widgets/analytics/components/PnL.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { styled } from 'rt-theme'
 import { AnalyticsBarChart } from './analyticsBarChart'
 import { CurrencyPairPosition } from '../model'
 import { Title } from './styled'
@@ -8,13 +7,11 @@ interface PnLProps {
   chartData: CurrencyPairPosition[]
 }
 
-export const PnLStyle = styled.div``
-
 export const PnL: React.FC<PnLProps> = ({ chartData }) => {
   return (
-    <PnLStyle>
+    <div>
       <Title>PnL</Title>
       <AnalyticsBarChart chartData={chartData} />
-    </PnLStyle>
+    </div>
   )
 }
--- src/client/src/apps/MainRoute/widgets/analytics/components/Positions.tsx
+++ src/client/src/apps/MainRoute/widgets/analytics/components/Positions.tsx
@@ -3,20 +3,18 @@ import { CurrencyPairs } from './Analytics'
 import { CurrencyPairPosition } from '../model'
 import { BubbleChart, Title } from './styled'
 import PositionsBubbleChart from './positions-chart/PositionsBubbleChart'
-import { styled } from 'rt-theme'
 
 interface PositionProps {
   data: CurrencyPairPosition[]
   currencyPairs: CurrencyPairs
 }
-export const PositionStyle = styled.div``
 export const Positions: React.FC<PositionProps> = ({ data, currencyPairs }) => {
   return (
-    <PositionStyle>
+    <div>
       <Title>Positions</Title>
       <BubbleChart>
         <PositionsBubbleChart data={data} currencyPairs={currencyPairs} />
       </BubbleChart>
-    </PositionStyle>
+    </div>
   )
 }
