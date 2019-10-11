2cd111920687ad2d25cbabbb2e3e424a27bd629e
Ben Gladman <ben.gladman@weareadaptive.com>
2019-10-11 15:27:49 +0100

    ARTP-599 Typecheck with strict null checks on pre-commit

--- src/client/src/rt-util/asyncUtils.ts
+++ src/client/src/rt-util/asyncUtils.ts
@@ -11,8 +11,8 @@ export function wait<T>(time: number, value: T) {
  * Returns a tuple with a new promise and its resolve/reject callbacks to invoke directly
  */
 export function getDeferredPromise<T>(): [Promise<T>, (value?: T) => void, (reason?: any) => void] {
-  let res: (value?: T) => void
-  let rej: (reason?: any) => void
+  let res!: (value?: T) => void
+  let rej!: (reason?: any) => void
   const p = new Promise<T>((r, rj) => {
     res = r
     rej = rj
