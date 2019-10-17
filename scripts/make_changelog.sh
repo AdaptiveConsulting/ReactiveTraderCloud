e67afcec11458f69564f7aacc396d5f9b0f0eb88
peter.margaritoff <peter.margaritoff@weareadaptive.com>
2019-10-17 11:29:34 -0400

    Updates changelog script to allow for tag ranges

--- scripts/make_changelog.sh
+++ scripts/make_changelog.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-COMMITS=$(git log --merges --pretty=format:"%H" origin/master)
+echo -n "Enter a tag range using using the following format: \$TAG1..\$TAG2 or leave blank to generate a changelog of all commits to master:"
+read range
+
+if [ -z "$range" ]
+then
+  COMMITS=$(git log --merges --pretty=format:"%H" origin/master)
+else
+  COMMITS=$(git log ${range} --merges --pretty=format:"%H" origin/master)
+fi
 
 GIT_USER_NAME=$(git config user.name)
 GIT_USER_EMAIL=$(git config user.email)
 DATE=$(date '+%Y-%m-%d')
 
 MARKDOWN="## Changelog \n"
+
+if [ -z "$range" ]
+then
+  MARKDOWN+='\n'
+  MARKDOWN+="### ${range}"
+fi
+
 MARKDOWN+='\n'
 
 for COMMIT in $COMMITS; do
