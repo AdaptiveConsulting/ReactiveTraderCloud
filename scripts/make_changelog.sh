aa166a87616f4649ddb96afa7e3679b35fcbe93a
Peter Margaritoff <pmargaritoff@gmail.com>
2019-10-18 09:36:49 -0400

    better structure/layout of generated changelog

--- scripts/make_changelog.sh
+++ scripts/make_changelog.sh
@@ -1,38 +1,45 @@
 #!/bin/bash
 
-echo -n "Enter a tag range using using the following format: \$TAG1..\$TAG2 or leave blank to generate a changelog of all commits to master:"
-read range
-
-if [ -z "$range" ]
-then
-  COMMITS=$(git log --merges --pretty=format:"%H" origin/master)
-else
-  COMMITS=$(git log ${range} --merges --pretty=format:"%H" origin/master)
-fi
-
-GIT_USER_NAME=$(git config user.name)
-GIT_USER_EMAIL=$(git config user.email)
-DATE=$(date '+%Y-%m-%d')
+TAGS=$(git tag)
+TAGS_ARRAY=($TAGS)
 
 MARKDOWN="## Changelog \n"
 
-if [ -z "$range" ]
-then
-  MARKDOWN+='\n'
-  MARKDOWN+="### ${range}"
-fi
+PREV_TAG="";
 
-MARKDOWN+='\n'
+for i in "${!TAGS_ARRAY[@]}"; do 
+  if [ $i != 0 ]; then
+    PREV_TAG=${TAGS_ARRAY[$i-1]}
+    CURR_TAG=${TAGS_ARRAY[$i]}
+    
+    RANGE="${PREV_TAG}...${CURR_TAG}"
+    
+    TAG_DATE=$(git log -1 --format=%ai ${PREV_TAG})
+    MARKDOWN+="\n"
+    MARKDOWN+="### ${PREV_TAG} ($TAG_DATE)"
+    MARKDOWN+="\n"
 
-for COMMIT in $COMMITS; do
-	SUBJECT=$(git log -1 ${COMMIT} --pretty=format:"%s")
-	PULL_REQUEST=$( grep -Eo "Merge pull request #[[:digit:]]+" <<< "$SUBJECT" )
-	if [[ $PULL_REQUEST ]]; then
-		PULL_NUM=${PULL_REQUEST#"Merge pull request #"}
-		BODY=$(git log -1 ${COMMIT} --pretty=format:"%b")
-		MARKDOWN+='\n'
-		MARKDOWN+=" - [#$PULL_NUM](https://github.com/AdaptiveConsulting/ReactiveTraderCloud/pull/$PULL_NUM): $BODY"
-	fi
+    COMMITS=$(git log ${RANGE} origin/master --merges --pretty=format:"%H")
+    COMMITS_ARRAY=($COMMITS)
+    for x in "${!COMMITS_ARRAY[@]}"; do 
+      echo "Processing Tag ${i} of ${#TAGS_ARRAY[@]}"
+      echo "--> Processing commit ${x} of ${#COMMITS_ARRAY[@]}"
+      
+      COMMIT=${COMMITS_ARRAY[$x]}
+
+      SUBJECT=$(git log -1 ${COMMIT} --pretty=format:"%s")
+      PULL_REQUEST=$( grep -Eo "Merge pull request #[[:digit:]]+" <<< "$SUBJECT" )
+      
+      if [[ $PULL_REQUEST ]]; then
+        PULL_NUM=${PULL_REQUEST#"Merge pull request #"}
+        BODY=$(git log -1 ${COMMIT} --pretty=format:"%b")
+        MARKDOWN+=" - [#$PULL_NUM](https://github.com/AdaptiveConsulting/ReactiveTraderCloud/pull/$PULL_NUM): $BODY"
+        MARKDOWN+="\n"
+      fi
+    done
+    MARKDOWN+="\n"
+    MARKDOWN+="\n"
+  fi
 done
 
 echo -e $MARKDOWN > ../CHANGELOG.md
