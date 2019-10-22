861a1e08ddfb0a36a51122ccfe75cd7e79de4375
Fabrice <fabrice.dore@gmail.com>
2019-10-22 10:37:18 +0100

    refactor price history service to use shared project

---
+++ src/node-server/price-history/src/domain/index.ts
@@ -0,0 +1,36 @@
+
+export interface Price {
+  ask: number
+  bid: number
+  mid: number
+  creationTimestamp: number
+  symbol: string
+  valueDate: string
+}
+
+export function convertToPrice(dto: RawPrice): Price {
+  return {
+    ask: dto.Ask,
+    bid: dto.Bid,
+    mid: dto.Mid,
+    creationTimestamp: dto.CreationTimestamp,
+    symbol: dto.Symbol,
+    valueDate: dto.ValueDate,
+  }
+}
+
+export interface RawPrice {
+  Ask: number
+  Bid: number
+  Mid: number
+  CreationTimestamp: number
+  Symbol: string
+  ValueDate: string
+}
+
+export interface RawServiceStatus {
+  Type: string
+  Instance: string
+  TimeStamp: number
+  Load: number
+}
