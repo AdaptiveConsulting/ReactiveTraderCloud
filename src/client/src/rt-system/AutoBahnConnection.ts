4285f37865d9b072ace1eea7b666af8895e70fa0
Ivars Arins <ivars@weareadaptive.com>
2019-12-17 09:39:45 +0100

    client side PoC

--- src/client/src/rt-system/AutoBahnConnection.ts
+++ src/client/src/rt-system/AutoBahnConnection.ts
@@ -1,8 +1,12 @@
 import { Connection } from 'autobahn'
 import AutobahnSessionProxy from './AutobahnSessionProxy'
 import { DisconnectionReason } from './DisconnectionReason'
+import {RxStomp, RxStompRPC} from '@stomp/rx-stomp'
 
 export interface AutobahnConnection {
+  rpcEndpoint: RxStompRPC
+  streamEndpoint: RxStomp
+  
   session?: AutobahnSessionProxy
 
   open(): boolean
