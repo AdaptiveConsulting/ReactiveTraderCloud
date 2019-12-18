a121d84284d071a9283dd1531cbb0fb5c9dbce32
Ivars Arins <ivars@weareadaptive.com>
2019-12-18 11:36:14 +0100

    removed commented out code

--- src/client/src/rt-system/ServiceStub.ts
+++ src/client/src/rt-system/ServiceStub.ts
@@ -11,7 +11,6 @@ interface SubscriptionDTO<TPayload> {
   Username: string
 }
 
-
 /**
  * A stub Used to call services. Hides the complexity of server interactions
  */
@@ -35,122 +34,38 @@ export class ServiceStub {
     return this.connection.streamEndpoint.watch(`/exchange/${topic}`).pipe(
       tap(x => this.logResponse(topic, {headers: x.headers, body: x.body})),
       map(x => <T>JSON.parse(x.body)))
-      /*
-      return this.connection$.pipe(
-        filter((connection): connection is ConnectionOpenEvent => connection.type === ConnectionEventType.CONNECTED),
-        switchMap(
-          ({ session }) =>
-          new Observable<T>(obs => {
-            console.info(LOG_NAME, `Subscribing to topic [${topic}].`)
+  }
             
-            let subscription: ISubscription
-            
-            session
-            .subscribe<T>(topic, response => {
-              this.logResponse(topic, response)
-              obs.next(response[0])
-            })
-            .then(
-              sub => {
-                // subscription succeeded, subscription is an instance of autobahn.Subscription
-                if (acknowledgementObs) {
-                  acknowledgementObs.next(topic)
-                }
-                subscription = sub
-              },
-              (error: Error) => {
-                // subscription failed, error is an instance of autobahn.Error
-                console.error(LOG_NAME, `Error on topic ${topic}`, error)
-                obs.error(error)
-              },
-              )
-              
-              return () => {
-                console.info(LOG_NAME, `Tearing down topic ${topic}`)
-                
-                if (!subscription) {
-                  return
-                }
-                try {
-                  if (session && session.isOpen()) {
-                    // It appears that AutoBahn's unsubscribe function is not implemented properly. In the case of an internal websocket exception,
-                    // due to disconnection for example, the promise that is returned never completes; i.e., neither callback passed to `then` is
-                    // ever invoked. I would at least have expected the rejected callback to have been invoked with the error. -D.S.
-                    session
-                    .unsubscribe(subscription)
-                    .then(
-                      () => console.info(LOG_NAME, `Successfully unsubscribing from topic ${topic}`),
-                      err => console.error(LOG_NAME, `Error unsubscribing from topic ${topic}`, err),
-                      )
-                    }
-                  } catch (err) {
-                    console.error(LOG_NAME, `Error thrown unsubscribing from topic ${topic}`, err)
-                  }
-                }
-              }),
-              ),
-              )*/
-            }
-            
-            requestStream<TResult, TPayload>(remoteProcedure: string, payload: TPayload) {
-              const dto: SubscriptionDTO<TPayload> = 
-              {
-                payload,
-                Username: this.userName,
-              }
+  requestStream<TResult, TPayload>(remoteProcedure: string, payload: TPayload) {
+    const dto: SubscriptionDTO<TPayload> = 
+    {
+      payload,
+      Username: this.userName,
+    }
 
-              return this.connection.rpcEndpoint.stream({
-                destination: `/amq/queue/${remoteProcedure}`,
-                body: JSON.stringify(dto)
-              }).pipe(
-                tap(x => this.logResponse(remoteProcedure, {headers: x.headers, body: x.body})),
-                map(x => <TResult>JSON.parse(x.body)))
-            }
-            /**
-             * wraps a RPC up as an observable stream
-             */
-            
-            requestResponse<TResult, TPayload>(remoteProcedure: string, payload: TPayload, responseTopic: string = '') {
-              const dto: SubscriptionDTO<TPayload> = 
-              {
-                payload,
-                Username: this.userName,
-              }
-              
-              return this.connection.rpcEndpoint.rpc({
-                destination: `/amq/queue/${remoteProcedure}`,
-                body: JSON.stringify(dto)
-              }).pipe(
-                tap(x => this.logResponse(remoteProcedure, {headers: x.headers, body: x.body})),
-                map(x => <TResult>JSON.parse(x.body)))
-                /*
-                return this.connection$.pipe(
-                  filter((connection): connection is ConnectionOpenEvent => connection.type === ConnectionEventType.CONNECTED),
-                  switchMap(
-                    ({ session }) =>
-                    new Observable<TResult>(obs => {
-                      console.debug(LOG_NAME, `Doing a RPC to [${remoteProcedure}]]`)
-
-            const dto: SubscriptionRequest<TPayload> = [
-              {
-                payload,
-                replyTo: responseTopic,
-                Username: this.userName,
-              },
-            ]
-
-            session.call<TResult>(remoteProcedure, dto).then(
-              result => {
-                obs.next(result)
-                obs.complete()
-              },
-              error => {
-                obs.error(error)
-              },
-            )
-          }),
-      ),
-      take(1),
-    )*/
+    return this.connection.rpcEndpoint.stream({
+      destination: `/amq/queue/${remoteProcedure}`,
+      body: JSON.stringify(dto)
+    }).pipe(
+      tap(x => this.logResponse(remoteProcedure, {headers: x.headers, body: x.body})),
+      map(x => <TResult>JSON.parse(x.body)))
+  }
+  /**
+   * wraps a RPC up as an observable stream
+   */
+  
+  requestResponse<TResult, TPayload>(remoteProcedure: string, payload: TPayload, responseTopic: string = '') {
+    const dto: SubscriptionDTO<TPayload> = 
+    {
+      payload,
+      Username: this.userName,
+    }
+    
+    return this.connection.rpcEndpoint.rpc({
+      destination: `/amq/queue/${remoteProcedure}`,
+      body: JSON.stringify(dto)
+    }).pipe(
+      tap(x => this.logResponse(remoteProcedure, {headers: x.headers, body: x.body})),
+      map(x => <TResult>JSON.parse(x.body)))
   }
 }
