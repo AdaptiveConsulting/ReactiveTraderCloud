99366e5a951a2b9a7698e615a4cf672d94960739
Moz Morris <moz@earthview.co.uk>
2020-01-15 12:19:57 +0000

    feat(server): add non-demo env name to openfin configs [ARTP-877]

--- src/node-server/openfin-config/src/config/launcher.ts
+++ src/node-server/openfin-config/src/config/launcher.ts
@@ -2,89 +2,93 @@
 import { OpenFinApplicationConfiguration } from '../types'
 
 /* eslint-disable @typescript-eslint/camelcase */
-export default (env: string): OpenFinApplicationConfiguration => ({
-  startup_app: {
-    name: 'Reactive Ecosystem Launcher',
-    url: `https://web-${env}.adaptivecluster.com/launcher`,
-    uuid: `reactive-launcher-${env}`,
-    defaultWidth: 650,
-    defaultHeight: 52,
-    defaultTop: 160,
-    defaultLeft: 30,
-    autoShow: true,
-    permissions: {
-      System: {
-        launchExternalProcess: true,
-      },
-    },
-    saveWindowState: false,
-    resizable: false,
-    shadow: true,
-    frame: false,
-    backgroundColor: '#444C5F',
-    alwaysOnTop: true,
-    icon: `https://web-${env}.adaptivecluster.com/static/media/icon.ico`,
-    applicationIcon: `https://web-${env}.adaptivecluster.com/static/media/adaptive-mark-large.png`,
-    contextMenu: true,
-    // @ts-ignore
-    _comment:
-      'Openfin Excel API preloaded below + added in appAssets (not included in standard OpenFin package)',
-    preload: [
-      {
-        url: `https://web-${env}.adaptivecluster.com/plugin/service-loader.js`,
-      },
-      {
-        url: `https://web-${env}.adaptivecluster.com/plugin/fin.desktop.Excel.js`,
-      },
-    ],
-    accelerator: {
-      devtools: true,
-      reload: true,
-      reloadIgnoreCache: true,
-      zoom: true,
-    },
-  },
-  runtime: {
-    arguments: '--remote-debugging-port=9222',
-    version: '13.76.44.21',
-  },
-  shortcut: {
-    company: 'Adaptive Consulting',
-    icon: `https://web-${env}.adaptivecluster.com/static/media/icon.ico`,
-    name: 'Reactive Ecosystem Launcher',
-  },
-  appAssets: [
-    {
-      src: `https://web-${env}.adaptivecluster.com/plugin/add-in.zip`,
-      alias: 'excel-api-addin',
-      version: '2.0.0',
-    },
-    {
-      src: `https://web-${env}.adaptivecluster.com/plugin/LimitChecker.zip`,
-      alias: 'LimitChecker',
-      version: '1.6.0',
-      target: 'LimitChecker.application',
-    },
-  ],
-  services: [
-    {
-      name: 'layouts',
-      config: {
-        features: {
-          snap: true,
-          dock: true,
-          tab: false,
+export default (env: string): OpenFinApplicationConfiguration => {
+  const name = `Reactive Ecosystem Launcher${env !== 'demo' ? ` (${env.toUpperCase()})` : ''}`
+
+  return {
+    startup_app: {
+      name,
+      url: `https://web-${env}.adaptivecluster.com/launcher`,
+      uuid: `reactive-launcher-${env}`,
+      defaultWidth: 650,
+      defaultHeight: 52,
+      defaultTop: 160,
+      defaultLeft: 30,
+      autoShow: true,
+      permissions: {
+        System: {
+          launchExternalProcess: true,
         },
       },
-      manifestUrl: 'https://cdn.openfin.co/services/openfin/layouts/1.1.0/app.json',
+      saveWindowState: false,
+      resizable: false,
+      shadow: true,
+      frame: false,
+      backgroundColor: '#444C5F',
+      alwaysOnTop: true,
+      icon: `https://web-${env}.adaptivecluster.com/static/media/icon.ico`,
+      applicationIcon: `https://web-${env}.adaptivecluster.com/static/media/adaptive-mark-large.png`,
+      contextMenu: true,
+      // @ts-ignore
+      _comment:
+        'Openfin Excel API preloaded below + added in appAssets (not included in standard OpenFin package)',
+      preload: [
+        {
+          url: `https://web-${env}.adaptivecluster.com/plugin/service-loader.js`,
+        },
+        {
+          url: `https://web-${env}.adaptivecluster.com/plugin/fin.desktop.Excel.js`,
+        },
+      ],
+      accelerator: {
+        devtools: true,
+        reload: true,
+        reloadIgnoreCache: true,
+        zoom: true,
+      },
     },
-    {
-      name: 'fdc3',
-      manifestUrl: 'https://cdn.openfin.co/services/openfin/fdc3/0.2.0/app.json',
+    runtime: {
+      arguments: '--remote-debugging-port=9222',
+      version: '13.76.44.21',
     },
-    {
-      name: 'notifications',
-      manifestUrl: 'https://cdn.openfin.co/services/openfin/notifications/0.11.1/app.json',
+    shortcut: {
+      company: 'Adaptive Consulting',
+      icon: `https://web-${env}.adaptivecluster.com/static/media/icon.ico`,
+      name,
     },
-  ],
-})
+    appAssets: [
+      {
+        src: `https://web-${env}.adaptivecluster.com/plugin/add-in.zip`,
+        alias: 'excel-api-addin',
+        version: '2.0.0',
+      },
+      {
+        src: `https://web-${env}.adaptivecluster.com/plugin/LimitChecker.zip`,
+        alias: 'LimitChecker',
+        version: '1.6.0',
+        target: 'LimitChecker.application',
+      },
+    ],
+    services: [
+      {
+        name: 'layouts',
+        config: {
+          features: {
+            snap: true,
+            dock: true,
+            tab: false,
+          },
+        },
+        manifestUrl: 'https://cdn.openfin.co/services/openfin/layouts/1.1.0/app.json',
+      },
+      {
+        name: 'fdc3',
+        manifestUrl: 'https://cdn.openfin.co/services/openfin/fdc3/0.2.0/app.json',
+      },
+      {
+        name: 'notifications',
+        manifestUrl: 'https://cdn.openfin.co/services/openfin/notifications/0.11.1/app.json',
+      },
+    ],
+  }
+}
