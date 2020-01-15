179c86def4bb1cbb25e767c88282325ce0561f17
Moz Morris <moz@earthview.co.uk>
2020-01-15 13:56:49 +0000

    chore(server): remove eslint ignores [ARTP-877]

--- src/node-server/openfin-config/src/config/get.ts
+++ src/node-server/openfin-config/src/config/get.ts
@@ -1,14 +1,11 @@
-/* eslint-disable @typescript-eslint/ban-ts-ignore */
 import { OpenFinApplicationConfiguration } from '../types'
 import appConfig from './app'
 import launcherConfig from './launcher'
 
-/* eslint-disable @typescript-eslint/camelcase */
 export default (type = 'app', env = 'demo'): OpenFinApplicationConfiguration => {
   switch (type) {
     case 'launcher':
       return launcherConfig(env)
-      break
 
     case 'app':
     default:
