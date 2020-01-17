37c2fb6c01671c0aa77ea1b35fd63512b1486dae
Moz Morris <moz@earthview.co.uk>
2020-01-17 11:11:40 +0000

    feat(server): openfin config url path updates [ARTP-877]
    * Remove url params from config url
    * Make type and env part of url path
    * Change base from openfinconfig to openfin

--- src/node-server/openfin-config/src/index.ts
+++ src/node-server/openfin-config/src/index.ts
@@ -3,8 +3,20 @@ import { getConfig } from './config'
 
 const app = new Koa()
 
-app.use(async ctx => {
-  const { type, env } = ctx.query
+const pathCapture = /\/(?<type>\w+)\/(?<env>\w+)(\.json)?$/
+
+app.use(async (ctx, next) => {
+  if (ctx.accepts('application/json') !== 'application/json') {
+    return next()
+  }
+
+  const capture = ctx.path.match(pathCapture)
+
+  if (capture === null || typeof capture.groups === 'undefined') {
+    return next()
+  }
+
+  const { type, env } = capture.groups
   ctx.body = getConfig(type, env)
 })
 
