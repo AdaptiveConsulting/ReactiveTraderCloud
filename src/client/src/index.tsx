c1872147d14bcd01245afe422500928a11abe6f8
Francelice Sardá <francelice.sarda@weareadaptive.com>
2020-01-20 17:20:30 +0100

    fix(client): taskbar/webpagetitle display name (ARTP-939)

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -11,7 +11,14 @@ const MainRoute = lazy(() => import('./apps/MainRoute'))
 const StyleguideRoute = lazy(() => import('./apps/StyleguideRoute'))
 const SimpleLauncher = lazy(() => import('./apps/SimpleLauncher'))
 
+const { pathname } = new URL(window.location.href)
 const urlParams = new URLSearchParams(window.location.search)
+
+const APP_PATHS = {
+  LAUNCHER: '/launcher',
+  TRADER: '/',
+  STYLEGUIDE: '/styleguide',
+}
 
 const envTitles = {
   localhost: '(LOCAL)',
@@ -21,12 +28,19 @@ const envTitles = {
   unknown: '(UNKNOWN)',
 }
 
+const appTitles = {
+  [APP_PATHS.LAUNCHER]: 'Reactive Ecosystem Launcher',
+  [APP_PATHS.TRADER]: 'Reactive Trader Cloud',
+  [APP_PATHS.STYLEGUIDE]: 'Style Guide for Reactive Trader',
+  unknown: document.title,
+}
+
 async function init() {
   console.info('BUILD_VERSION: ', process.env.REACT_APP_BUILD_VERSION)
   const intentsProvider = getProvider()
 
   const env = getEnvironment()
-  document.title = `${document.title} ${envTitles[env || 'unknown']}`;
+  document.title = `${appTitles[pathname] || appTitles['unknown']} ${envTitles[env || 'unknown']}`
 
   if (urlParams.has('startAsSymphonyController')) {
     const { initiateSymphony } = await getSymphonyPlatform()
@@ -39,8 +53,8 @@ async function init() {
           <BrowserRouter>
             <Suspense fallback={<div />}>
               <Switch>
-                <Route path="/launcher" render={() => <SimpleLauncher />} />
-                <Route path="/styleguide" render={() => <StyleguideRoute />} />
+                <Route path={APP_PATHS.LAUNCHER} render={() => <SimpleLauncher />} />
+                <Route path={APP_PATHS.STYLEGUIDE} render={() => <StyleguideRoute />} />
                 <Route render={() => <MainRoute />} />
               </Switch>
             </Suspense>
