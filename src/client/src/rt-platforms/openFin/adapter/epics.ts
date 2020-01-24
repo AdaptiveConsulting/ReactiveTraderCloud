22c22d781f86b1735bc41c5c4bb834bc64638f99
Moz Morris <moz@earthview.co.uk>
2020-01-24 13:57:00 +0000

    fix(client): ensure setupWorkspaces is called on boot [ARTP-938]

--- src/client/src/rt-platforms/openFin/adapter/epics.ts
+++ src/client/src/rt-platforms/openFin/adapter/epics.ts
@@ -1,16 +1,15 @@
 import { Action } from 'redux'
 import { map, switchMapTo } from 'rxjs/operators'
 import { ActionsObservable, ofType, StateObservable } from 'redux-observable'
-import { SetupActions } from 'rt-actions'
 import { LayoutActions } from 'rt-actions/layoutActions'
 import { setupWorkspaces } from './openFin'
 import { ApplicationEpic } from 'StoreTypes'
-const setupLayout = ofType<Action>(SetupActions.setup)
+import { SetupAction, CONNECTION_ACTION_TYPES } from 'rt-actions/setupActions'
 
 export const platformEpics: Array<ApplicationEpic> = [
   (action$: ActionsObservable<Action>, _: StateObservable<any>) => {
     return action$.pipe(
-      setupLayout,
+      ofType<Action, SetupAction>(CONNECTION_ACTION_TYPES.SETUP),
       switchMapTo(setupWorkspaces().pipe(map(LayoutActions.updateContainerVisibilityAction))),
     )
   },
