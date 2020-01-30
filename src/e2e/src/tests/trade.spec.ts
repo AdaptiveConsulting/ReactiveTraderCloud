268e513b388d02aaa6ff8cac89f2c90ffb613147
Moz Morris <moz@earthview.co.uk>
2020-01-30 10:35:07 +0000

    refactor(e2e): group releated blocks

--- src/e2e/src/tests/trade.spec.ts
+++ src/e2e/src/tests/trade.spec.ts
@@ -61,56 +61,75 @@ describe('UI Tests for Reactive Trader Cloud Web Application', async () => {
     await browser.close()
   })
 
-  it('Should validate a notional value larger than the maximum allowed', async () => {
-    await mainPage.tile.setNotional('EURToUSD', '99999999')
-    const notional = await mainPage.tile.tradeType.EURToUSD.notional
-    expect(notional.getAttribute('value')).toEqual('99,999,999')
-    const pricesButton = browser.element(by.qaTag('workspace-view-normal'))
-    await pricesButton.click()
-    const labelRFQ = await mainPage.tile.tradeType.initiateRFQ.buttonInitiateRFQ
-    expect(labelRFQ.getText()).toEqual('Initiate RFQ')
-    await mainPage.tile.resetNotional('EURToUSD')
-    expect(notional.getAttribute('value')).toEqual('1,000,000')
-  })
-
-  currencyList.forEach(([selectedCurrency, expectedCurrencies]) => {
-    it(`should validate filtering currencies by ${selectedCurrency}`, async () => {
-      await mainPage.workspace.selectCurrency(selectedCurrency)
-      await assertUtils.assertDisplayedCurrencies(expectedCurrencies)
+  describe('Page', () => {
+    it('Verify page title', async () => {
+      const title = await browser.getTitle()
+      const isValid = envTitles.includes(title)
+      expect(isValid).toBeTruthy()
     })
   })
 
-  tradeList.forEach(([selectedCurrency, currencyPair, direction, expectedResult, timeout]) => {
-    it(`should validate ${currencyPair} ${direction}`, async () => {
-      const tradingCurrency = currencyPair.replace('/', 'To')
-      await mainPage.workspace.selectCurrency(selectedCurrency)
-      const notional = await mainPage.tile.tradeType[tradingCurrency].notional
-      await mainPage.tile.selectSpotTile(tradingCurrency, direction)
-      await assertUtils.confirmationMessageAsserts(
-        currencyPair,
-        direction,
-        expectedResult,
-        notional.getAttribute('value'),
-        timeout,
-      )
+  describe('Notional', () => {
+    notionalList.forEach(([enteredNotional, expectedNotional]) => {
+      it(`Should validate notional entry ${enteredNotional}`, async () => {
+        await mainPage.tile.setNotional('EURToUSD', enteredNotional)
+        const notional = await mainPage.tile.tradeType.EURToUSD.notional
+        const returnedNotional = notional.getAttribute('value')
+        expect(returnedNotional).toEqual(expectedNotional)
+      })
+    })
+
+    it('Should validate a notional value larger than the maximum allowed', async () => {
+      await mainPage.tile.setNotional('EURToUSD', '99999999')
+      const notional = await mainPage.tile.tradeType.EURToUSD.notional
+      expect(notional.getAttribute('value')).toEqual('99,999,999')
+      const pricesButton = browser.element(by.qaTag('workspace-view-normal'))
+      await pricesButton.click()
+      const labelRFQ = await mainPage.tile.tradeType.initiateRFQ.buttonInitiateRFQ
+      expect(labelRFQ.getText()).toEqual('Initiate RFQ')
+      await mainPage.tile.resetNotional('EURToUSD')
+      expect(notional.getAttribute('value')).toEqual('1,000,000')
+    })
+
+    it('Should validate zero notional', async () => {
+      await mainPage.workspace.selectCurrency('usd')
+      await mainPage.tile.setNotional('USDToJPY', '0')
+      const tradeButton = await mainPage.tile.tradeType.USDToJPY.sell
+      const disabledAttribute = await tradeButton.getAttribute('disabled')
+      expect(disabledAttribute).toBe('true')
     })
   })
 
-  it('should validate RFQ mode', async () => {
-    await mainPage.workspace.selectCurrency('usd')
-    await mainPage.tile.setNotional('USDToJPY', '10m')
-    await mainPage.tile.initiateRFQ()
+  describe('Currencies', () => {
+    currencyList.forEach(([selectedCurrency, expectedCurrencies]) => {
+      it(`Should validate filtering currencies by ${selectedCurrency}`, async () => {
+        await mainPage.workspace.selectCurrency(selectedCurrency)
+        await assertUtils.assertDisplayedCurrencies(expectedCurrencies)
+      })
+    })
+
+    tradeList.forEach(([selectedCurrency, currencyPair, direction, expectedResult, timeout]) => {
+      it(`Should validate ${currencyPair} ${direction}`, async () => {
+        const tradingCurrency = currencyPair.replace('/', 'To')
+        await mainPage.workspace.selectCurrency(selectedCurrency)
+        const notional = await mainPage.tile.tradeType[tradingCurrency].notional
+        await mainPage.tile.selectSpotTile(tradingCurrency, direction)
+        await assertUtils.confirmationMessageAsserts(
+          currencyPair,
+          direction,
+          expectedResult,
+          notional.getAttribute('value'),
+          timeout,
+        )
+      })
+    })
   })
 
-  it('should validate zero notional', async () => {
-    await mainPage.workspace.selectCurrency('usd')
-    await mainPage.tile.setNotional('USDToJPY', '0')
-    const tradeButton = await mainPage.tile.tradeType.USDToJPY.sell
-    const disabledAttribute = await tradeButton.getAttribute('disabled')
-    expect(disabledAttribute).toBe('true')
-  })
-
-  afterAll(async () => {
-    await browser.close()
+  describe('RFQ', () => {
+    it('should validate RFQ mode', async () => {
+      await mainPage.workspace.selectCurrency('usd')
+      await mainPage.tile.setNotional('USDToJPY', '10m')
+      await mainPage.tile.initiateRFQ()
+    })
   })
 })
