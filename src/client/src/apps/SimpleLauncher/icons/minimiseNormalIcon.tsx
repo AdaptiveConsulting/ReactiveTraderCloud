6fbb1f81ce243912f97cc32e83b821ad545a6ca8
ianchan035 <ian035@hotmail.com>
2020-02-12 15:20:33 +0000

    fix(client): fix svg property

--- src/client/src/apps/SimpleLauncher/icons/minimiseNormalIcon.tsx
+++ src/client/src/apps/SimpleLauncher/icons/minimiseNormalIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-    <g fill="none" fill-rule="evenodd">
+    <g fill="none" fillRule="evenodd">
       <path d="M0 0h24v24H0z" />
       <path
         fill="#7F7F7F"
