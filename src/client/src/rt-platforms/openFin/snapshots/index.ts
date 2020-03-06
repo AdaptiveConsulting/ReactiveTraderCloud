fad027935ce4da98335af7c48acbcb4c053d58a4
peter.margaritoff <peter.margaritoff@weareadaptive.com>
2020-03-06 09:19:42 -0500

    Allow snapshots from cold start

--- src/client/src/rt-platforms/openFin/snapshots/index.ts
+++ src/client/src/rt-platforms/openFin/snapshots/index.ts
@@ -13,7 +13,7 @@ export const resetCurrentSnapshotName = () => {
 }
 
 export const getCurrentSnapshotName = () => {
-  return window.localStorage.getItem(OPENFIN_SNAPSHOT_CURRENT) || ''
+  return window.localStorage.getItem(OPENFIN_SNAPSHOT_CURRENT) || OPENFIN_SNAPSHOT_DEFAULT_NAME
 }
 export const getSnapshotNames = () => {
   const snapshotNamesStr = window.localStorage.getItem(OPENFIN_SNAPSHOT_NAMES)
@@ -67,7 +67,7 @@ export const applySnapshotFromStorageOnLoad = async () => {
   const snapshots = getSnapshots()
 
   let platformSnapshotName = platformCtx.platformSnapshotName
-  let currentSnapshot = snapshots.snapshots[currentSnapshotName]
+  let currentSnapshot = snapshots.snapshots && snapshots.snapshots[currentSnapshotName]
 
   // @ts-ignore
   if (!currentSnapshotName || snapshots.version !== canned.version) {
