315c841f87682bcd67d341ce015d35a95eee7ece
peter.margaritoff <peter.margaritoff@weareadaptive.com>
2020-03-11 13:29:30 -0400

    fix type errors

--- src/client/src/rt-interop/util/getProvider.ts
+++ src/client/src/rt-interop/util/getProvider.ts
@@ -5,7 +5,9 @@ const isOpenFin = 'fin' in window && !isFinsemble
 
 export const getProvider = () => {
   if (isOpenFin) {
+    // @ts-ignore
     const { OpenFinFDC3, FDC3 } = lazy(() => import('../fdc3'))
+
     const openFinFDC3 = new OpenFinFDC3()
 
     return new FDC3(openFinFDC3)
