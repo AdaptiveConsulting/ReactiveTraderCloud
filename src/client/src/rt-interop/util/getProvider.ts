43da5b623dde31675dcc3542aaab3a919247b404
peter.margaritoff <peter.margaritoff@weareadaptive.com>
2020-03-11 10:48:27 -0400

    Lazy load fdc3 lib only if openfin

--- src/client/src/rt-interop/util/getProvider.ts
+++ src/client/src/rt-interop/util/getProvider.ts
@@ -1,13 +1,13 @@
-// @ts-ignore
-import { OpenFinFDC3, FDC3 } from '../fdc3'
+import { lazy } from 'react'
 
 const isFinsemble = 'FSBL' in window
 const isOpenFin = 'fin' in window && !isFinsemble
 
 export const getProvider = () => {
   if (isOpenFin) {
-    // @ts-ignore
+    const { OpenFinFDC3, FDC3 } = lazy(() => import('../fdc3'))
     const openFinFDC3 = new OpenFinFDC3()
+
     return new FDC3(openFinFDC3)
   }
 
