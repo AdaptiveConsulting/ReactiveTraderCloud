828720b13237caea29f45c900ad6d0fecb4adcbf
Riko Eksteen <reksteen@gmail.com>
2020-03-27 12:23:13 +0000

    fix(security): remove vulnerabilities (ARTP-1077)
    - Recreate package-lock files with fresh installs to create new vulnerabilities
    - Had to add a new utility function to use correct types with useState function
    - Still 2 security vulnerabilities at root level that can't be fixed - needs a new version of lerna/handlebars

--- src/client/src/rt-util/utilityTypes.ts
+++ src/client/src/rt-util/utilityTypes.ts
@@ -6,9 +6,17 @@ export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<
 
 export type Extends<T, U extends T> = U
 
-// Workaround to get intellisense on type unions of 'literals' | string. 
+// Workaround to get intellisense on type unions of 'literals' | string.
 // See https://github.com/Microsoft/TypeScript/issues/29729
-export type LiteralUnion<T extends U, U = string> = T | (U & {zz_IGNORE_ME?: never })
+export type LiteralUnion<T extends U, U = string> = T | (U & { zz_IGNORE_ME?: never })
 
 // Like Partial<T> but recursively for deep objects. Extracted from redux types
-export type DeepPartial<T> = { [K in keyof T]?: DeepPartial<T[K]> };
+export type DeepPartial<T> = { [K in keyof T]?: DeepPartial<T[K]> }
+
+// extracts the return type of a promise, or a function that returns a promise
+// see https://stackoverflow.com/questions/48011353/how-to-unwrap-type-of-a-promise?rq=1
+export type AsyncReturnType<T> = T extends Promise<infer U>
+  ? U
+  : T extends (...args: any[]) => Promise<infer V>
+  ? V
+  : T
