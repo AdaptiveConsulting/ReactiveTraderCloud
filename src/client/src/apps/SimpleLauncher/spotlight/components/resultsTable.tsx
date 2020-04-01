465e57d2d4476e2e721d6ca0af3dc76d86aefabd
peter.margaritoff <peter.margaritoff@weareadaptive.com>
2020-04-01 10:52:03 -0400

    feat(launcher): update inline quote pips (ARTP-1064)

--- src/client/src/apps/SimpleLauncher/spotlight/components/resultsTable.tsx
+++ src/client/src/apps/SimpleLauncher/spotlight/components/resultsTable.tsx
@@ -5,6 +5,7 @@ export interface Col {
   title: String
   id: String
   align?: 'center' | 'right'
+  formatter?: (value: any) => void
 }
 interface ResultsTableProps {
   cols: Col[]
@@ -29,9 +30,11 @@ export const ResultsTable: FC<ResultsTableProps> = ({ cols, rows }) => {
             return (
               <TableRow key={rowIndex} status={row.status}>
                 {cols.map((col: any, cellIndex: number) => {
+                  const value = row[col.id]
+                  const formattedValue = col.formatter ? col.formatter(value) : value
                   return (
                     <TableCell align={col.align} key={cellIndex}>
-                      {row[col.id]}
+                      {formattedValue}
                     </TableCell>
                   )
                 })}
