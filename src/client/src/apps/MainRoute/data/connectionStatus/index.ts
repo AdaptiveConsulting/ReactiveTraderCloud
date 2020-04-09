6a9513084756732c745066e2eeb281b962cda359
Audrey <audrey.fouquier@gmail.com>
2020-04-09 20:46:22 +0200

    chore: cleaning

--- src/client/src/apps/MainRoute/data/connectionStatus/index.ts
+++ src/client/src/apps/MainRoute/data/connectionStatus/index.ts
@@ -1,5 +1,3 @@
-import * as connectionStatusService from './connectionStatusService'
 export { connectionStatusEpic } from './epics'
 export { connectionStatusReducer } from './reducer'
-export type ConnectionInfo = connectionStatusService.ConnectionInfo
 export * from './selectors'
