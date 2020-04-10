eeb7fb7597d7fbace6aa02366cf8a35d0154cbcb
peter.margaritoff <peter.margaritoff@weareadaptive.com>
2020-04-10 08:28:32 -0400

    feat(pwa): cleanup (ARTP-1121)

--- src/client/src/serviceWorker.js
+++ src/client/src/serviceWorker.js
@@ -31,7 +31,6 @@ export function register(config) {
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/rtc-sw.js`
-      console.log(process.env, isLocalhost, swUrl, config)
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config)
