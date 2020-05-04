80037744726d5db9a62b1963cc74a30ea6a5de4f
Audrey <audrey.fouquier@gmail.com>
2020-05-04 19:03:58 +0200

    feat(finsemble): add finsemble library feat(finsemble): add finsemble limit checker feat(finsemble): implement FSBL fix(build): npm token

--- src/client/src/type-declarations/globals.d.ts
+++ src/client/src/type-declarations/globals.d.ts
@@ -5,14 +5,7 @@ declare module 'react-sizeme'
 declare module 'audio-recorder-polyfill'
 interface Window {
   // types not currently available for FSBL - 1/24/2019
-  FSBL: {
-    addEventListener: any
-    UserNotification: any
-    Clients: {
-      RouterClient: any
-      LauncherClient: any
-    }
-  }
+  FSBL
 
   SYMPHONY: SymphonyClient
 
