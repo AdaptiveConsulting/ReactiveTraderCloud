33cedfa74514dc89ce7d5576625d02c081e67ee5
Riko Eksteen <reksteen@gmail.com>
2020-05-21 15:59:17 +0100

    feat(client): use token replacement for pwa manifest (ARTP-1173)
    - add tokens to manifest.json
    - switch to new RT icons for PWA
    - use token replacement with webpack (local) and openresty template (docker) to change manifest content
    - remove production-only constraint from service worker
    - use kubernetes configmap to set environment variables for client
    - add README

--- src/client/src/serviceWorker.js
+++ src/client/src/serviceWorker.js
@@ -19,7 +19,7 @@ const isLocalhost = Boolean(
 )
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href)
     if (publicUrl.origin !== window.location.origin) {
