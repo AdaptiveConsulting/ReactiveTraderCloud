841e2e05f476b08eded52caa1b27fa82569a6feb
Mattia Tollari <mattia.tollari@weareadaptive.com>
2020-07-21 15:23:49 +0200

    feat: added job that seeds data and fixed the script

--- src/services/kubernetes/deploy_locally.sh
+++ src/services/kubernetes/deploy_locally.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
+
 export DOCKER_USER='localhost:32000'
 export BUILD_VERSION='0.0.0'
 cd ../..
 docker-compose build
 docker-compose push
 
-microk8s kubectl delete --all deployments -n reactive-trader-local
-microk8s kubectl delete --all services -n reactive-trader-local
+# This is how we deploy from GitHub
+#microk8s kubectl scale deploy --replicas=0 --all -n reactive-trader-local
 
-for f in ./services/kubernetes/*.yaml; do cat $f | /usr/bin/envsubst | microk8s kubectl apply -n reactive-trader-local -f -; done
+# In this way I can start from scratch and seed the data each time
+microk8s kubectl delete --all deployments -n reactive-trader-local
+microk8s kubectl delete --all jobs -n reactive-trader-local
+
+# Removing the services will change the IP adresses 
+#microk8s kubectl delete --all services -n reactive-trader-local
+
+for f in $(find ./services/kubernetes -type f -name "*.yaml"); do cat $f | /usr/bin/envsubst | microk8s kubectl apply -n reactive-trader-local -f -; done
