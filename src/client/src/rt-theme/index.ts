d400f0d8b8d2f8294e704c4b3894f36cdffa7166
Fran <f.x.rodriguez@hotmail.com>
2020-10-03 17:32:48 +0100

    chore(styled components): cleanup

--- src/client/src/rt-theme/index.ts
+++ src/client/src/rt-theme/index.ts
@@ -1,5 +1,4 @@
 import type { Theme } from './themes'
-import baseStyled from 'styled-components/macro'
 export type {
   LightShade,
   DarkShade,
@@ -12,16 +11,11 @@ export { themes, getThemeColor } from './themes'
 export type { Theme, TouchableIntentName, ColorProps, ThemeSelector } from './themes'
 export { colors } from './colors'
 export { default as GlobalStyle } from './globals'
-
-// TEMP - remove once all imports migrated to 'styled-components/macro'
-export const styled = baseStyled
+export { ThemeName, ThemeProvider, ThemeConsumer, useTheme } from './ThemeContext'
+export { default as ThemeStorageSwitch } from './ThemeStorageSwitch'
 
 // Make all styled-component functions (e.g. `styled`, `css`) typed with Theme
 // See https://github.com/styled-components/styled-components-website/issues/447
 declare module 'styled-components' {
-  // tslint:disable-next-line:no-empty-interface
   export interface DefaultTheme extends Theme {}
 }
-
-export { ThemeName, ThemeProvider, ThemeConsumer, useTheme } from './ThemeContext'
-export { default as ThemeStorageSwitch } from './ThemeStorageSwitch'
