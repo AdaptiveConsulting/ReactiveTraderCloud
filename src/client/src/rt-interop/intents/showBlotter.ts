21b926fdb38b4eacb0bc38f24e0ecf6d7b92efa5
stewart-adaptive <stewart.moroney@weareadaptive.com>
2020-10-28 13:58:00 +0000

    fix: paths for new subwindows opened from launcher (ARTP-1323)

--- src/client/src/rt-interop/intents/showBlotter.ts
+++ src/client/src/rt-interop/intents/showBlotter.ts
@@ -1,6 +1,6 @@
 import { InteropTopics, Platform, platformHasFeature, PlatformWindow } from 'rt-platforms'
 import { stringify } from 'query-string'
-import { defaultConfig, windowOrigin } from './defaultWindowConfig'
+import { defaultConfig } from './defaultWindowConfig'
 import { BlotterFilters, validateFilters } from 'apps/MainRoute/widgets/blotter/blotterTradesFilter'
 
 let openedWindow: PlatformWindow | undefined
@@ -32,7 +32,7 @@ async function openNewWindow(
   filters: BlotterFilters,
   platform: Platform
 ): Promise<PlatformWindow | undefined> {
-  const baseUrl = `${windowOrigin}/blotter`
+  const baseUrl = `/blotter`
   const queryString = stringify(validateFilters(filters))
   const url = queryString ? `${baseUrl}/?${queryString}` : baseUrl
 
