d07c3fb7facec3576bd6c53c7ed4cf5eaa70b72b
Mark-Adaptive <mark.bekker@weareadaptive.com>
2020-11-05 18:10:59 -0500

    Artp 1288 make version url clickable

--- src/client/src/apps/MainRoute/widgets/footer-version/FooterVersion.tsx
+++ src/client/src/apps/MainRoute/widgets/footer-version/FooterVersion.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import styled from 'styled-components/macro'
 
 export const Wrapper = styled.div`
@@ -10,17 +10,42 @@ export const Link = styled.a`
   color: inherit;
   text-decoration: inherit;
 `
-const FooterVersion: FC = () => (
-  <Wrapper>
-    <Link
-      href={
-        'https://github.com/AdaptiveConsulting/ReactiveTraderCloud/releases/tag/' +
-        process.env.REACT_APP_VERSION
-      }
-    >
-      {process.env.REACT_APP_VERSION}
-    </Link>
-  </Wrapper>
-)
+const FooterVersion: FC = () => {
+  const [versionExists, setVersionExists] = useState<boolean | void>(false)
+  const gitTagExists = async (gitTag: string | undefined) => {
+    const exists = await fetch(
+      'https://api.github.com/repos/AdaptiveConsulting/ReactiveTraderCloud/releases'
+    )
+      .then(response => response.json())
+      .then(data => {
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].tag_name === gitTag) {
+            return true
+          }
+        }
+        return false
+      })
+      .catch(error => console.error(error))
+    setVersionExists(exists)
+  }
 
+  let currentVersion: string | undefined = process.env.REACT_APP_VERSION
+  gitTagExists(currentVersion)
+
+  const URL =
+    'https://github.com/AdaptiveConsulting/ReactiveTraderCloud/releases/tag/' +
+    process.env.REACT_APP_VERSION
+
+  return (
+    <Wrapper>
+      {versionExists ? (
+        <Link target="_blank" href={URL}>
+          {process.env.REACT_APP_VERSION}
+        </Link>
+      ) : (
+        <p>{process.env.REACT_APP_VERSION}</p>
+      )}
+    </Wrapper>
+  )
+}
 export default FooterVersion
