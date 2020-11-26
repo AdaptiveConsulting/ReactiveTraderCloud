6c6d6a98b702bda3c9b261db947f9fb2a61a11ca
Ziyi Wang <ziyi.wang@weareadaptive.com>
2020-11-26 15:52:10 -0500

    chore: rename

--- src/client/src/rt-platforms/openfin/components/OpenFinChrome.tsx
+++ src/client/src/rt-platforms/openfin/components/OpenFinChrome.tsx
@@ -6,7 +6,12 @@ import Header from 'apps/MainRoute/components/app-header'
 import OpenFinContactButton from './OpenFinContactButton'
 import { StatusButtonContainer } from './OpenFinStatusConnection/StatusContainers'
 import StatusBar from 'apps/MainRoute/widgets/status-bar'
-import { ExitIcon, maximiseScreenIcon, minimiseNormalIcon, popInIcon } from '../../icons'
+import {
+  ExitIcon,
+  maximiseScreenIcon,
+  minimiseNormalIcon,
+  popInIcon,
+} from 'apps/SimpleLauncher/icons'
 import ReactGA from 'react-ga'
 
 export interface ControlProps {
