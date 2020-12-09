28958718fe34c1b853658f96ebeb8a43b6b65b2b
Ignacio Prado <ignacio.prado@weareadaptive.com>
2020-12-08 20:18:39 -0500

    feat(new-client): re-arch blotter bootstrap (656)

---
+++ src/new-client/src/App/Trades/BlotterHeader/BlotterHeader.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components/macro"
+import { Subscribe } from "@react-rxjs/core"
+import { ExcelButton } from "./ExcelButton"
+import { AppliedFilters } from "./AppliedFilters"
+import { QuickFilter } from "./QuickFilter"
+import { gridApi$ } from "../BlotterGrid"
+
+const BlotterHeaderStyle = styled("div")`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 0.5rem;
+  height: 2.5rem;
+  background-color: ${({ theme }) => theme.core.darkBackground};
+`
+const BlotterRight = styled("div")`
+  display: flex;
+  align-items: center;
+`
+
+const BlotterLeft = styled("div")`
+  font-size: 0.9375rem;
+`
+
+const Fill = styled.div`
+  width: 1rem;
+  height: 1rem;
+`
+
+const BlotterToolbarStyle = styled("div")`
+  display: flex;
+  align-items: center;
+  justifycontent: flex-end;
+`
+
+export const BlotterHeader: React.FC = () => (
+  <BlotterHeaderStyle>
+    <BlotterLeft>Trades</BlotterLeft>
+    <Subscribe source$={gridApi$}>
+      <BlotterRight>
+        <ExcelButton />
+        <BlotterToolbarStyle>
+          <AppliedFilters />
+          <QuickFilter />
+        </BlotterToolbarStyle>
+        <Fill />
+      </BlotterRight>
+    </Subscribe>
+  </BlotterHeaderStyle>
+)
