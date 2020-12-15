562f1ce2dc47c32ce159de108912060b756fe2fe
Josep M Sobrepere <josepot@gmail.com>
2020-12-15 16:47:02 +0100

    services(new-client): dynamic price history

--- src/new-client/src/services/tiles.ts
+++ src/new-client/src/services/tiles.ts
@@ -10,6 +10,10 @@ import {
   switchMap,
   take,
   startWith,
+  mergeAll,
+  skip,
+  debounce,
+  debounceTime,
 } from "rxjs/operators"
 import { getRemoteProcedureCall$, getStream$ } from "./client"
 import { currencyPairs$, currencyPairUpdates$ } from "./currencyPairs"
@@ -64,14 +68,31 @@ export const [usePrice, getPrice$] = bind((symbol: string) =>
   ),
 )
 
-export const [
-  useHistoricalPrices,
-  getHistoricalPrices$,
-] = bind((symbol: string) =>
-  getRemoteProcedureCall$<HistoryPrice[], string>(
-    "priceHistory",
-    "getPriceHistory",
-    symbol,
+const HISTORY_TIMESTAMP_THRESHOLD = 1000
+const HISTORY_SIZE = 100
+export const [useHistoricalPrices, getHistoricalPrices$] = bind<
+  [string],
+  HistoryPrice[]
+>((symbol: string) =>
+  concat(
+    getRemoteProcedureCall$<HistoryPrice[], string>(
+      "priceHistory",
+      "getPriceHistory",
+      symbol,
+    ).pipe(mergeAll()),
+    getPrice$(symbol),
+  ).pipe(
+    scan((acc, price) => {
+      if (acc.length === 0) return [price]
+      const last = acc[acc.length - 1]
+      const diff = price.creationTimestamp - last.creationTimestamp
+      if (diff < HISTORY_TIMESTAMP_THRESHOLD) return acc
+      if (acc.length < HISTORY_SIZE) acc.concat(price)
+      const result = acc.slice(1)
+      result.push(price)
+      return result
+    }, [] as HistoryPrice[]),
+    debounceTime(0),
   ),
 )
 
