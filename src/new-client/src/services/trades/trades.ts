de19becf1917d3f204a5966428f11fb31bf8f6ed
Josep M Sobrepere <josepot@gmail.com>
2021-02-04 09:53:30 +0100

    test(new-client): trades service
    Co-authored-by: Ignacio Prado <ignacio.prado@weareadaptive.com>

--- src/new-client/src/services/trades/trades.ts
+++ src/new-client/src/services/trades/trades.ts
@@ -1,44 +1,7 @@
 import { bind } from "@react-rxjs/core"
 import { map, scan } from "rxjs/operators"
-import { getStream$ } from "./client"
-import { CamelCase, CollectionUpdates } from "./utils"
-
-export enum Direction {
-  Buy = "Buy",
-  Sell = "Sell",
-}
-
-export enum TradeStatus {
-  Pending = "Pending",
-  Done = "Done",
-  Rejected = "Rejected",
-}
-
-interface TradeRaw {
-  TradeId: number
-  CurrencyPair: string
-  TraderName: string
-  Notional: number
-  DealtCurrency: string
-  Direction: Direction
-  Status: TradeStatus
-  SpotRate: number
-  TradeDate: string
-  ValueDate: string
-}
-
-interface RawTradeUpdate extends CollectionUpdates {
-  Trades: TradeRaw[]
-}
-
-export interface Trade
-  extends CamelCase<
-    Omit<TradeRaw, "ValueDate" | "TradeDate" | "CurrencyPair">
-  > {
-  symbol: string
-  valueDate: Date
-  tradeDate: Date
-}
+import { getStream$ } from "../client"
+import {Trade, RawTradeUpdate} from './types'
 
 export const [useTrades, trades$] = bind<Trade[]>(
   getStream$<RawTradeUpdate>("blotter", "getTradesStream", {}).pipe(
