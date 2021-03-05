76df65835957b0259adcf6829b394197e871df90
Gary Chambers <gary.chambers@weareadaptive.com>
2021-03-05 12:31:13 +0000

    refactor: split build and deploy scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,16 +3,9 @@
 set -euo pipefail
 
 export DOCKER_USER=reactivetradercloud
-export DEPLOY_ENV=${1:-}
 
-if [ -z $DEPLOY_ENV ]; then
-  echo "No environment name specified."
-  echo "Usage: build-and-deploy.sh [environment]"
-  exit 1
-fi
-
-if [ ! -v DOCKER_PASS ]; then
-  echo "Docker registry password must be specified in DOCKER_PASS variable."
+if [ ! -v DEPLOY_ENV ]; then
+  echo "Deployment environment must be specified in DEPLOY_ENV variable."
   exit 1
 fi
 
@@ -30,47 +23,9 @@ echo "Preparing deployment to \"$DEPLOY_ENV\""
 
 kuberoot="./src/services/kubernetes/per-deployment"
 
-# TODO: parse docker-compose manifest for these values
-services=$(cat <<-END
-broker       ./src/services/broker
-servers      ./src/server
-pricehistory ./src/server/node
-nlp          ./src/server/node
-bot          ./src/server/node/bot
-client       ./src/client
-new-client   ./src/new-client
-END
-)
-
 build_version () {
   local version=$(git ls-tree HEAD -- $1 | awk '{print $3}')
   echo $version
-}
-
-tag_exists () {
-  curl --silent -f -lL https://hub.docker.com/v2/repositories/$1/tags/$2 > /dev/null
-}
-
-build_service () {
-  local version=$(build_version $2)
-
-  if ! tag_exists $DOCKER_USER/$1 $version; then
-    echo "Building \"$1\" (version: $version)"
-
-    ENVIRONMENT_NAME=$DEPLOY_ENV \
-    BUILD_VERSION=$version \
-    DOCKER_BUILDKIT=1 \
-    docker-compose -f ./src/docker-compose.yml build $1
-
-    echo "Pushing \"$1\" (version: $version)"
-
-    BUILD_VERSION=$version \
-    docker-compose -f ./src/docker-compose.yml push $1
-  fi
-}
-
-before_build () {
-  echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
 }
 
 deploy_service () {
@@ -112,14 +67,6 @@ after_deploy () {
   # Run initialisation batch job
   deploy_dotnet_service seed
 }
-
-before_build
-
-while IFS= read -r service; do
-  name=$(echo $service | awk '{print $1}')
-  context=$(echo $service | awk '{print $2}')
-  build_service $name $context
-done <<< "$services"
 
 before_deploy
 
