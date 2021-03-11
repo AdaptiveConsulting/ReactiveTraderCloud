3792c23248b647ca55d79e764a58f30df9ccda31
Josep M Sobrepere <josepot@gmail.com>
2021-03-11 11:42:33 +0100

    chore(new-client): preload key requests

--- src/new-client/vite.config.ts
+++ src/new-client/vite.config.ts
@@ -2,9 +2,25 @@ import { resolve } from "path"
 import { defineConfig } from "vite"
 import reactRefresh from "@vitejs/plugin-react-refresh"
 import eslint from "@rollup/plugin-eslint"
+import modulepreload from "rollup-plugin-modulepreload"
+
+// TODO: This is a workaround until the following issue gets
+// confirmed/resolved: https://github.com/vitejs/vite/issues/2460
+const customPreloadPlugin = () => {
+  const result: any = {
+    ...((modulepreload as any)({
+      index: resolve(__dirname, "dist", "index.html"),
+      prefix: "",
+    }) as any),
+    enforce: "post",
+  }
+  result.writeBundle = result.generateBundle
+  delete result.generateBundle
+  return result
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
@@ -16,8 +32,14 @@ export default defineConfig({
       "@": resolve(__dirname, "src"),
     },
   },
-  plugins: [
-    { ...eslint({ include: "src/**/*.+(js|jsx|ts|tsx)" }), enforce: "pre" },
-    reactRefresh(),
-  ],
-})
+  plugins:
+    mode === "development"
+      ? [
+          {
+            ...eslint({ include: "src/**/*.+(js|jsx|ts|tsx)" }),
+            enforce: "pre",
+          },
+          reactRefresh(),
+        ]
+      : [customPreloadPlugin()],
+}))
