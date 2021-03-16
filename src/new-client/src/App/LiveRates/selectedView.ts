d286c0f63a94929adaf4e3e9adad97a648c97218
julie <julie.zhu@weareadaptive.com>
2021-03-15 23:45:02 -0400

    feat(new-client): persist selected tile view

--- src/new-client/src/App/LiveRates/selectedView.ts
+++ src/new-client/src/App/LiveRates/selectedView.ts
@@ -1,6 +1,6 @@
 import { bind } from "@react-rxjs/core"
 import { createListener } from "@react-rxjs/utils"
-import { scan } from "rxjs/operators"
+import { scan, tap } from "rxjs/operators"
 
 export enum TileView {
   Normal = "normal",
@@ -10,12 +10,19 @@ export enum TileView {
 const [toggleSelectedView$, onToggleSelectedView] = createListener()
 export { onToggleSelectedView }
 
+const SELECTED_VIEW_KEY = 'selectedView'
+
+const initView = (window.localStorage.getItem(SELECTED_VIEW_KEY) as TileView) || TileView.Normal
+
 export const [useSelectedTileView] = bind(
   toggleSelectedView$.pipe(
     scan(
       (acc) => (acc === TileView.Normal ? TileView.Analytics : TileView.Normal),
-      TileView.Normal,
+      initView,
     ),
+    tap((newView) => {
+      window.localStorage.setItem(SELECTED_VIEW_KEY, newView)
+    })
   ),
-  TileView.Normal,
+  initView,
 )
