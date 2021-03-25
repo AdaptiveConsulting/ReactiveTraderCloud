42c1ebc0509e4d3c3d10618854b77d80a068d61c
Ignacio Prado <ignacio.prado@weareadaptive.com>
2021-03-25 14:35:28 -0400

    chore(new-client): point at correct broker environment, assume TLS only

--- src/new-client/src/services/client/endpoints.ts
+++ src/new-client/src/services/client/endpoints.ts
@@ -10,12 +10,7 @@ export const endpoints$ = new Observable<{
   const PORT = import.meta.env.VITE_BROKER_PORT as string
   const url = HOST || globalThis.location.hostname
   const port = +(PORT || globalThis.location.port)
-  const useSecure = globalThis.location.protocol === "https:"
-  const securePort = 443
-  const defaultPort = port ? port : 80
-  const brokerURL = useSecure
-    ? `wss://${url}:${securePort}/ws`
-    : `ws://${url}:${defaultPort}/ws`
+  const brokerURL = `wss://${url}:${port}/ws`
   const reconnectDelay = 500
   const connectionTimeout = 0
 
