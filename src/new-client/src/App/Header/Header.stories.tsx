3dc53ab27325d16a40f30ffa03eb5ddbc3fa45cc
julie <julie.zhu@weareadaptive.com>
2021-04-26 22:23:35 -0400

    feat(new-client): storybook integration setup + header story

---
+++ src/new-client/src/App/Header/Header.stories.tsx
@@ -0,0 +1,12 @@
+import { storiesOf } from '@storybook/react'
+import React from 'react'
+import Header from './Header'
+import Story from "@/stories/Story";
+
+const stories = storiesOf('Header', module)
+
+stories.add('Header', () => (
+  <Story>
+    <Header />
+  </Story>
+))
