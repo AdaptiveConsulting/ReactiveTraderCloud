f7149574f4c7aff7152d708302e79a0db1ebdb03
julie <julie.zhu@weareadaptive.com>
2021-04-26 22:23:35 -0400

    feat(new-client): storybook integration setup + header story

---
+++ src/new-client/.storybook/main.js
@@ -0,0 +1,16 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.plugins.push(new TsconfigPathsPlugin({}))
+    return config;
+  },
+}
