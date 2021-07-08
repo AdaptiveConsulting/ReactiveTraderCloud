fc6e1337f24f7e5f668987da6b6f793784e24370
Gary Chambers <gary.chambers@weareadaptive.com>
2021-07-08 15:45:46 +0100

    feat: add connection status to OpenFin footer

--- src/new-client/src/OpenFin/Window/WindowFooter.tsx
+++ src/new-client/src/OpenFin/Window/WindowFooter.tsx
@@ -1,8 +1,10 @@
 import StatusBar from "@/App/Footer/StatusBar"
+import { StatusButton } from "@/App/Footer/StatusButton"
 import { Controls } from "./WindowFooter.styles"
 
 export const WindowFooter: React.FC = () => (
   <StatusBar>
     <Controls />
+    <StatusButton />
   </StatusBar>
 )
