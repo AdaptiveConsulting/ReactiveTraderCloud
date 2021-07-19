36498671d16c23251245233ff9c5cd7500135606
Francelice Sarda <francelice@gmail.com>
2021-07-19 20:19:19 +0200

    Openfin footer button

--- src/new-client/src/OpenFin/Window/WindowFooter.tsx
+++ src/new-client/src/OpenFin/Window/WindowFooter.tsx
@@ -1,10 +1,13 @@
 import StatusBar from "@/App/Footer/StatusBar"
 import { StatusButton } from "@/App/Footer/StatusButton"
 import { Controls } from "./WindowFooter.styles"
+import { OpenFinLogo } from "../Footer/OpenFinLogo"
 
 export const WindowFooter: React.FC = () => (
   <StatusBar>
-    <Controls />
+    <Controls>
+      <OpenFinLogo />
+    </Controls>
     <StatusButton />
   </StatusBar>
 )
