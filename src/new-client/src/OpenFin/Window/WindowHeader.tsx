e8143e1d74547a9db07b4b1cf8dca04c26c61ea9
Ivan Falanga <ivanfalanga@ivans-mbp.home>
2021-07-30 16:10:25 +0100

    feat: show user and avatar for openfin header

--- src/new-client/src/OpenFin/Window/WindowHeader.tsx
+++ src/new-client/src/OpenFin/Window/WindowHeader.tsx
@@ -1,16 +1,22 @@
 import Header from "@/App/Header"
 import ThemeSwitcher from "@/App/Header/theme-switcher"
 import { LayoutLock } from "./LayoutLock"
-import { Props as ControlsProps, WindowControls } from "./WindowControls"
+import { Props as WindowControlProps, WindowControls } from "./WindowControls"
 import { TitleBar } from "./WindowHeader.styles"
+import LoginControls from "@/App/Header/LoginControls"
 
-interface Props extends ControlsProps {
+interface Props extends WindowControlProps {
   title: string
 }
 
 export const WindowHeader: React.FC<Props> = ({ title, ...controlsProps }) => (
   <Header
-    controls={<WindowControls {...controlsProps} />}
+    controls={
+      <>
+        <LoginControls />
+        <WindowControls {...controlsProps} />
+      </>
+    }
     filler={<TitleBar>{title}</TitleBar>}
     switches={
       <>
