d416b3a702e5c60f619c196ff7e95f3b45e43ea8
Taylor Mitchell-St. Joseph <taylor@weareadaptive.com>
2021-09-29 13:18:49 +0100

    fix(client): Position ExecutionMessage & Pending absolutely to fix UI on safari

--- src/new-client/src/services/trades/index.ts
+++ src/new-client/src/services/trades/index.ts
@@ -1,4 +1,5 @@
+import * as tradesTestData from "./__mocks__/mockTrades"
 export * from "./trades"
-export * as tradesTestData from "./__mocks__/mockTrades"
 export { Direction, TradeStatus } from "./types"
 export type { Trade } from "./types"
+export { tradesTestData }
