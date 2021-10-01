c9efd0604b5713d33c5a4e1c073f5db9aa227f21
Taylor Mitchell-St. Joseph <taylor@weareadaptive.com>
2021-10-01 16:00:27 +0100

    feat(client): Added reloadAfter1Hour

--- src/new-client/src/index.tsx
+++ src/new-client/src/index.tsx
@@ -1,11 +1,20 @@
 import main from "./main"
 
 declare global {
-  const __TARGET__: "web" | "openfin" | "finsemble"
-
   interface Window {
     ga: any
   }
 }
 
 main()
+
+// Google Cloud Run kills all ws connections after 1 hour
+// Reload the application to establish a new connection and avoid staring at the DisconnectionOverlay indefinitely
+// This is a temporary measure while we have the GCR limitation
+const reloadAfter1Hour = () => {
+  setTimeout(() => {
+    window.location.reload()
+  }, 60 * 1000)
+}
+
+reloadAfter1Hour()
