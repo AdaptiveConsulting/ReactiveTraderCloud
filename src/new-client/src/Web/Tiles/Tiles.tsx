6cf9fa7c24cfde0576a147407b9e71fc1a52170d
Taylor Mitchell-St. Joseph <taylor@weareadaptive.com>
2021-10-18 09:48:54 +0100

    feat(new-client): Added window utils

--- src/new-client/src/Web/Tiles/Tiles.tsx
+++ src/new-client/src/Web/Tiles/Tiles.tsx
@@ -1,33 +1,22 @@
 import { LiveRates } from "@/App/LiveRates"
 import { tearOut, tearOutEntry$ } from "@/App/LiveRates/Tile/TearOut/state"
 import { useObservableSubscription } from "@/utils/useObservableSubscription"
+import { openWindow } from "../utils/window"
 
 export const Tiles = () => {
   useObservableSubscription(
     tearOutEntry$.subscribe(
       async ([symbol, tornOut]) => {
         if (tornOut) {
-          const windowRef = window.open(
-            `/spot/${symbol}`,
-            symbol,
-            "width=380,height=170",
+          openWindow(
+            {
+              url: `/spot/${symbol}`,
+              name: symbol,
+              width: 380,
+              height: 170,
+            },
+            () => tearOut(symbol, false),
           )
-
-          const unloadListener = () => {
-            setTimeout(() => {
-              if (windowRef?.closed) {
-                tearOut(symbol, false)
-              } else {
-                // needs to be re-set after window reload
-                setUnloadListener()
-              }
-            }, 100)
-          }
-
-          const setUnloadListener = () =>
-            windowRef?.addEventListener("unload", unloadListener)
-
-          setUnloadListener()
         }
       },
       (e) => {
