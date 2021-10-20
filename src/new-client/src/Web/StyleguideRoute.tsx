a909d8de05f991bcc3d45f8c8d7e13485f81cd3b
hafidk <hafidstor@gmail.com>
2021-10-20 09:58:39 +0200

    [feat] stylguide iconography section

--- src/new-client/src/Web/StyleguideRoute.tsx
+++ src/new-client/src/Web/StyleguideRoute.tsx
@@ -11,6 +11,7 @@ import { Block } from "@/styleguide/styled/Block"
 // import Atoms fr∫om './sections/Atoms'
 import CoreBranding from "@/styleguide/sections/CoreBranding"
 import FontFamilies from "@/styleguide/sections/FontFamilies"
+import Iconography from "@/styleguide/sections/Iconography"
 // import Iconography from './sections/Iconography'
 // import Molecules from './sections/Molecules'
 // import { getAppName } from '@/utils'
@@ -22,7 +23,7 @@ const sections: Array<{
 }> = [
   { path: "core-branding", Section: CoreBranding, title: "Colour" },
   { path: "font-families", Section: FontFamilies, title: "Typography" },
-  // { path: 'icons-family', Section: Iconography, title: 'Iconography' },
+  { path: "icons-family", Section: Iconography, title: "Iconography" },
   // { path: 'atoms-components', Section: Atoms, title: 'Atoms' },
   // { path: 'molecules-components', Section: Molecules, title: 'Molecules' },
 ]
