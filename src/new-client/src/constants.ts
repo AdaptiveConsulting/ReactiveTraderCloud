54afb85ea361c9a275e2fb072319e121bad370ff
Taylor Mitchell-St. Joseph <taylor@weareadaptive.com>
2021-10-27 11:40:47 +0100

    feat(new-client): Refactoring environment and base url vars

--- src/new-client/src/constants.ts
+++ src/new-client/src/constants.ts
@@ -4,3 +4,8 @@ export const BASE_URL = (import.meta.env.URL_PATH || "") as string
 export const ENVIRONMENT =
   ["env", "dev", "uat", "prod"].find((env) => DOMAIN.includes(`.${env}.`)) ||
   "local"
+const thisEnv = import.meta.env
+console.log("thisEnv", thisEnv)
+console.log("DOMAIN", DOMAIN)
+console.log("BASE_URL", BASE_URL)
+console.log("ENVIRONMENT", ENVIRONMENT)
