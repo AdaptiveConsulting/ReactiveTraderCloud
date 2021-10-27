769a6fd9090a9cc91f90a59c531b7c1b0275f180
Taylor Mitchell-St. Joseph <taylor@weareadaptive.com>
2021-10-27 11:35:31 +0100

    feat(new-client): Refactoring environment and base url vars

--- src/new-client/src/constants.ts
+++ src/new-client/src/constants.ts
@@ -1,6 +1,6 @@
 export const GA_TRACKING_ID = "UA-46320965-5"
 export const DOMAIN = (import.meta.env.DOMAIN || "") as string
-export const BASE_URL = (import.meta.env.PATH || "") as string
+export const BASE_URL = (import.meta.env.URL_PATH || "") as string
 export const ENVIRONMENT =
   ["env", "dev", "uat", "prod"].find((env) => DOMAIN.includes(`.${env}.`)) ||
   "local"
