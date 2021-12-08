2185b3537822f6a1232088af96e40ae8dd1ad9ec
Taylor Mitchell-St. Joseph <taylor@weareadaptive.com>
2021-12-08 15:42:31 +0000

    fix: storybook public path

--- src/new-client/src/constants.ts
+++ src/new-client/src/constants.ts
@@ -8,7 +8,7 @@ export const BASE_PATH =
   BASE_URL && BASE_URL.startsWith("http") ? new URL(BASE_URL).pathname : ""
 export const ENVIRONMENT: Environment =
   (["env", "dev", "uat", "prod"].find((env) =>
-    BASE_URL.includes(`.${env}.`),
+    (BASE_URL || '').includes(`.${env}.`),
   ) as Environment) || "local"
 
 export const ROUTES_CONFIG = {
