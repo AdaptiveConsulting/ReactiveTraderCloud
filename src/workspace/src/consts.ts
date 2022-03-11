2440eccbb357fdb5bd3f4cfc9baff285cab51aac
Taylor Mitchell-St. Joseph <taylor@weareadaptive.com>
2022-03-11 10:39:42 +0000

    feat(workspace): configure RT url based on window.location

--- src/workspace/src/consts.ts
+++ src/workspace/src/consts.ts
@@ -1,3 +1,6 @@
 export const BASE_URL =
   import.meta.env.BASE_URL === '/' ? window.location.origin : import.meta.env.BASE_URL
-export const { VITE_RT_URL, VITE_RA_URL } = import.meta.env as Record<string, string>
+export const { VITE_RA_URL } = import.meta.env as Record<string, string>
+export const VITE_RT_URL =
+  (import.meta.env.VITE_RT_URL as string) ||
+  window.location.href.replace('/workspace/index.html', '')
