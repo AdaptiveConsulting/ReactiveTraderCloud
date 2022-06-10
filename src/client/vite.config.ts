4e6f776c488fd16276444d4bfc2d4795df9b205c
Ivan Falanga <ivanfalanga@ivans-mbp.home>
2022-06-10 12:05:32 +0100

    chore(client): update vite plugin html to remove template injection in ejs vulnerability

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,13 +1,13 @@
-import path, { resolve } from "path"
-import { readdirSync, statSync } from "fs"
-import { defineConfig, loadEnv } from "vite"
-import reactRefresh from "@vitejs/plugin-react-refresh"
-import { injectHtml } from "vite-plugin-html"
-import copy from "rollup-plugin-copy"
 import eslint from "@rollup/plugin-eslint"
-import typescript from "rollup-plugin-typescript2"
+import reactRefresh from "@vitejs/plugin-react-refresh"
+import { readdirSync, statSync } from "fs"
+import path, { resolve } from "path"
+import copy from "rollup-plugin-copy"
 import modulepreload from "rollup-plugin-modulepreload"
+import typescript from "rollup-plugin-typescript2"
 import { injectManifest } from "rollup-plugin-workbox"
+import { defineConfig, loadEnv } from "vite"
+import { createHtmlPlugin } from "vite-plugin-html"
 
 const PORT = Number(process.env.PORT) || 1917
 
@@ -234,7 +234,8 @@ const htmlPlugin = (dev: boolean) => {
 }
 
 const injectScriptIntoHtml = () =>
-  injectHtml({
+createHtmlPlugin({
+  inject: {
     data: {
       injectScript: `
       <script>
@@ -246,7 +247,8 @@ const injectScriptIntoHtml = () =>
       </script>
     `,
     },
-  })
+  }    
+})
 
 const injectWebServiceWorkerPlugin = (mode: string) =>
   injectManifest(
