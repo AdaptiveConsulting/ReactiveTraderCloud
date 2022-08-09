98801fe8a8421199acc95f7a5255f53b87e6679e
Audrey Fouquier <audrey@weareadaptive.com>
2022-08-09 19:36:55 +0200

    chore: add api compatibility check

---
+++ src/client/src/services/tradingGatewayCompatibility.ts
@@ -0,0 +1,36 @@
+import { checkCompatibility } from "@/generated/TradingGateway"
+import { lastValueFrom } from "rxjs"
+import { map, tap } from "rxjs/operators"
+
+export interface Compatibility {
+  isCompatible: boolean
+  incompatibilityReasons: string[]
+}
+
+export async function checkTradingGatewayCompatibility(): Promise<Compatibility> {
+  return lastValueFrom(
+    checkCompatibility().pipe(
+      tap((response) => {
+        const log =
+          response.type === "incompatible" ? console.error : console.log
+        log(`Trading Gateway Api is ${response.type}`)
+      }),
+      map((response) => {
+        if (response.type === "incompatible") {
+          const incompatibilityReasons = response.payload.methods.map(
+            ({ method: { methodName, serviceName, methodRouteKey } }) =>
+              `${serviceName} ${methodName} ${methodRouteKey}`,
+          )
+          return {
+            isCompatible: false,
+            incompatibilityReasons,
+          }
+        }
+        return {
+          isCompatible: true,
+          incompatibilityReasons: [] as string[],
+        }
+      }),
+    ),
+  )
+}
