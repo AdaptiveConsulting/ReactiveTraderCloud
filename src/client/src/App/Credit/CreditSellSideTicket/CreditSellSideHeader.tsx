be44751b5ac0904adb4b8a1d6bd576b2027e54c8
Ivan Falanga <ivanfalanga@ivans-mbp.home>
2022-08-25 16:37:07 +0100

    feat(client): add adaptive banner for sell side ticket

--- src/client/src/App/Credit/CreditSellSideTicket/CreditSellSideHeader.tsx
+++ src/client/src/App/Credit/CreditSellSideTicket/CreditSellSideHeader.tsx
@@ -1,6 +1,8 @@
+import Logo from "@/components/Logo"
 import { Direction, QuoteState, RfqState } from "@/generated/TradingGateway"
 import { useCreditInstrumentById } from "@/services/credit"
 import { memo } from "react"
+import styled from "styled-components"
 import { CusipWithBenchmark } from "../common/CusipWithBenchmark"
 import {
   DirectionContainer,
@@ -8,6 +10,22 @@ import {
   InstrumentLabelContainer,
   InstrumentName,
 } from "../common/RfqTicketHeader"
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Banner = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  gap: 8px;
+  color: ${({ theme }) => theme.textColor};
+  font-size: 11px;
+  line-height: 16px;
+  font-weight: 500;
+`
 
 interface CreditSellSideHeaderProps {
   direction: Direction
@@ -28,25 +46,35 @@ export const CreditSellSideHeader = memo(
     const terminated = rfqState !== RfqState.Open && !accepted
 
     return (
-      <DirectionContainer direction={direction} terminated={terminated}>
-        {direction === Direction.Buy && (
-          <DirectionLabel direction={direction} terminated={terminated}>
-            <div>YOU {accepted ? "BOUGHT" : "BUY"}</div>
-          </DirectionLabel>
-        )}
-        <InstrumentLabelContainer direction={direction} terminated={terminated}>
-          <InstrumentName>{instrument?.name ?? "No name found"}</InstrumentName>
-          <CusipWithBenchmark
-            cusip={instrument?.cusip}
-            benchmark={instrument?.benchmark}
-          />
-        </InstrumentLabelContainer>
-        {direction === Direction.Sell && (
-          <DirectionLabel direction={direction} terminated={terminated}>
-            <div>YOU {accepted ? "SOLD" : "SELL"}</div>
-          </DirectionLabel>
-        )}
-      </DirectionContainer>
+      <Wrapper>
+        <Banner>
+          <Logo withText={false} size={1} /> RFQ from Adaptive Asset Management
+        </Banner>
+        <DirectionContainer direction={direction} terminated={terminated}>
+          {direction === Direction.Buy && (
+            <DirectionLabel direction={direction} terminated={terminated}>
+              <div>YOU {accepted ? "BOUGHT" : "BUY"}</div>
+            </DirectionLabel>
+          )}
+          <InstrumentLabelContainer
+            direction={direction}
+            terminated={terminated}
+          >
+            <InstrumentName>
+              {instrument?.name ?? "No name found"}
+            </InstrumentName>
+            <CusipWithBenchmark
+              cusip={instrument?.cusip}
+              benchmark={instrument?.benchmark}
+            />
+          </InstrumentLabelContainer>
+          {direction === Direction.Sell && (
+            <DirectionLabel direction={direction} terminated={terminated}>
+              <div>YOU {accepted ? "SOLD" : "SELL"}</div>
+            </DirectionLabel>
+          )}
+        </DirectionContainer>
+      </Wrapper>
     )
   },
 )
