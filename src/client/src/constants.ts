8f7b4d8992f2a5aad5258605887083501f105eb8
Alan Greasley <alan.greasley@weareadaptive.com>
2022-11-01 23:44:04 +0000

    chore(client): update openfin scripts

--- src/client/src/constants.ts
+++ src/client/src/constants.ts
@@ -2,7 +2,8 @@ type Environment = "local" | "env" | "dev" | "uat" | "prod"
 
 export const GA_TRACKING_ID = "UA-46320965-5"
 // Comes from vite.config - base
-// Will be / for localhost or platform.env.reactivetrader.com
+// Will be / for vite development mode (local),
+// otherwise the full domain / path of the deployment e.g. https://web.env.reactivetrader.com/pull/2064
 const BASE_URL = import.meta.env.BASE_URL
 export const BASE_PATH =
   BASE_URL && BASE_URL.startsWith("http") ? new URL(BASE_URL).pathname : ""
