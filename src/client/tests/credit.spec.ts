ed45d661c27323c8d0beebeeb980f640f2334dca
snony14 <herr_moise@hotmail.com>
2023-01-26 17:31:18 +0000

    define openfin pages for credit

---
+++ src/client/tests/credit.spec.ts
@@ -0,0 +1,9 @@
+import { expect, Locator } from "@playwright/test"
+import * as dotenv from "dotenv"
+import { test } from "../fixtures"
+
+dotenv.config()
+
+test.describe("Credit", () => {
+  test.describe("Add New RFQ", () => {})
+})
