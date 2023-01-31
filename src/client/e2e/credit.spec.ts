eed6aec7c9f27676d3b250570c03fa634331bd4a
snony14 <herr_moise@hotmail.com>
2023-01-31 12:34:25 +0000

    add tests for openfin credit

--- src/client/e2e/credit.spec.ts
+++ src/client/e2e/credit.spec.ts
@@ -1,9 +1,96 @@
-import { expect, Locator } from "@playwright/test"
+import { expect, Page } from "@playwright/test"
 import * as dotenv from "dotenv"
 import { test } from "../fixtures"
 
 dotenv.config()
 
 test.describe("Credit", () => {
-  test.describe("Add New RFQ", () => {})
+  test.describe("New RFQ", () => {
+    test("When I select Googl instrument and click Send RFQ button then I should see a GOOGL RFQ created on the RFQ sections and I can accept any value", async ({
+      context,
+      creditOpenfinPagesRec,
+    }, testInfo) => {
+      test.setTimeout(120000)
+      let newRfqPage: Page
+      let rfqsPage: Page
+      let rfqBlotterPage: Page
+      if (testInfo.project.name === "openfin") {
+        const mainWindow = creditOpenfinPagesRec["mainWindow"]
+        await mainWindow.evaluate(async () => {
+          const currentWindow = window.fin.desktop.Window.getCurrent()
+          currentWindow.maximize()
+          return window.fin
+        })
+        newRfqPage = creditOpenfinPagesRec["credit-new-rfq"]
+        rfqsPage = creditOpenfinPagesRec["credit-rfqs"]
+        rfqBlotterPage = creditOpenfinPagesRec["credit-blotter"]
+      } else {
+        const pages = await context.pages()
+        newRfqPage = pages.length > 0 ? pages[0] : await context.newPage()
+        await newRfqPage.goto(
+          `${process.env.URL_PATH ?? "http://localhost:1917/credit"}`,
+        )
+        rfqsPage = newRfqPage
+        rfqBlotterPage = newRfqPage
+      }
+      await newRfqPage.getByPlaceholder(/Enter a CUSIP/).click()
+      await newRfqPage
+        .locator("[data-testid='search-result-item']")
+        .nth(5)
+        .click()
+
+      const quantity = newRfqPage.locator("[data-testid='quantity']")
+      await quantity.type("2")
+      await quantity.blur()
+
+      await newRfqPage.locator("span").getByText(/All/).click()
+
+      await newRfqPage
+        .locator("span")
+        .getByText(/Adaptive Bank/)
+        .click()
+
+      await newRfqPage
+        .locator("button")
+        .getByText(/Send RFQ/)
+        .click()
+      // Navigate to Live
+      await rfqsPage.getByText(/Live/).click()
+      await rfqsPage.waitForTimeout(15000)
+
+      await rfqsPage
+        .locator("[data-testid='quotes']")
+        .locator("div")
+        .first()
+        .hover()
+
+      await rfqsPage
+        .getByTestId("quotes")
+        .getByText(/Accept/)
+        .first()
+        .click()
+
+      await rfqsPage.locator("li").getByText(/All/).nth(0).click()
+      const btnTxt = await rfqsPage
+        .locator("button")
+        .getByText(/View Trade/)
+        .first()
+        .innerText()
+
+      await rfqsPage
+        .locator("button")
+        .getByText(/View Trade/)
+        .first()
+        .click()
+
+      const tradeId = btnTxt.split(" ")[2]
+      const blotterId = await rfqBlotterPage
+        .locator("td")
+        .getByText(tradeId)
+        .first()
+        .innerText()
+
+      expect(tradeId).toEqual(blotterId)
+    })
+  })
 })
--- src/client/e2e/spot-tile.spec.ts
+++ src/client/e2e/spot-tile.spec.ts
@@ -1,4 +1,4 @@
-import { expect, Locator } from "@playwright/test"
+import { expect, Locator, Page } from "@playwright/test"
 import * as dotenv from "dotenv"
 import { test } from "./fixtures"
 
@@ -9,10 +9,8 @@ test.describe("Spot Tile", () => {
     context,
     fxOpenfinPagesRec,
   }, testInfo) => {
-    let button: Locator
-    let tradeIdLocator: Locator
-    let tradeId: string
-    let blotterTradeID: string
+    let tilePage: Page
+    let blotterPage: Page
 
     if (testInfo.project.name === "openfin") {
       const mainWindow = fxOpenfinPagesRec["mainWindow"]
@@ -22,35 +20,27 @@ test.describe("Spot Tile", () => {
         currentWindow.maximize()
         return window.fin
       })
-
-      const openfinTile = fxOpenfinPagesRec["fx-tiles"]
-
-      button = openfinTile.locator("[data-testid='Sell-EURUSD']")
-      await button.click()
-
-      tradeIdLocator = openfinTile.locator("[data-testid='trade-id']")
-      tradeId = await tradeIdLocator.innerText()
-
-      const openfinBlotter = fxOpenfinPagesRec["fx-blotter"]
-      blotterTradeID = await openfinBlotter
-        .getByRole("cell", { name: tradeId })
-        .innerText()
+      tilePage = fxOpenfinPagesRec["fx-tiles"]
+      blotterPage = fxOpenfinPagesRec["fx-blotter"]
     } else {
       const pages = await context.pages()
       const mainWindow = pages.length > 0 ? pages[0] : await context.newPage()
       await mainWindow.goto(
         `${process.env.URL_PATH ?? "http://localhost:1917/"}`,
       )
-      button = mainWindow.locator("[data-testid='Sell-EURUSD']")
-      await button.click()
-
-      tradeIdLocator = mainWindow.locator("[data-testid='trade-id']")
-      tradeId = await tradeIdLocator.innerText()
-
-      blotterTradeID = await mainWindow
-        .getByRole("cell", { name: tradeId })
-        .innerText()
+      tilePage = mainWindow
+      blotterPage = mainWindow
     }
+
+    await tilePage.locator("[data-testid='Sell-EURUSD']").click()
+
+    const tradeId = await tilePage
+      .locator("[data-testid='trade-id']")
+      .innerText()
+
+    const blotterTradeID = await blotterPage
+      .getByRole("cell", { name: tradeId })
+      .innerText()
 
     expect(tradeId).toBe(blotterTradeID)
   })
