a32e9c3b1ba18f5831d3d8b08d71f1c6180c1ae2
snony14 <herr_moise@hotmail.com>
2023-01-31 13:56:58 +0000

    increase workers in playwright

--- src/client/playwright.config.ts
+++ src/client/playwright.config.ts
@@ -5,7 +5,7 @@ const config: PlaywrightTestConfig = {
   testDir: "./e2e",
   /* Maximum time one test can run for. */
   timeout: 50 * 1000,
-  workers: 1,
+  workers: 2,
   projects: [
     {
       name: "chrome",
