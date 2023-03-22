773aa739b3918fd48f915b16a5ffa41971a2ba6a
Ignacio Prado <ignacio.prado@weareadaptive.com>
2023-03-22 15:18:49 -0400

    fix: revert server changes

--- src/server/node/bot/src/connection/StompConfig.ts
+++ src/server/node/bot/src/connection/StompConfig.ts
@@ -2,13 +2,14 @@
  * WsProxy: makes the broker connection api more explicit, aids testing
  */
 export default class StompConfig {
-  public brokerURL: string
-  public connectionTimeout = 500
-  public reconnectDelay = 500
+  public brokerURL: string;
+  public connectionTimeout = 500;
+  public reconnectDelay = 500;
 
   constructor(url: string, port?: number) {
-    const defaultPort = port ? port : 80
-    const useSecure = port === 443
-    this.brokerURL = `${useSecure ? 'wss' : 'ws'}://${url}:${defaultPort}/ws`
+    /* eslint-disable-next-line */
+    const defaultPort = port ? port : 80;
+    const useSecure = port === 443;
+    this.brokerURL = `${useSecure ? "wss" : "ws"}://${url}:${defaultPort}/ws`;
   }
 }
