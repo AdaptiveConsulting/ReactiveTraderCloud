6267498f7a103805979cf5bac56a36d8fd0e22b7
Sam Holleworth <sam.holleworth@weareadaptive.com>
2023-06-21 17:40:43 +0100

    chore: updated service worker modal render method

--- src/client/src/Web/cacheUpdateModal.tsx
+++ src/client/src/Web/cacheUpdateModal.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 import styled from "styled-components"
 
 import { Modal } from "@/components/Modal"
@@ -28,7 +28,9 @@ export const showCacheUpdateModal = () => {
   const updateRoot = document.createElement("div")
   document.body.appendChild(updateRoot)
 
-  ReactDOM.render(
+  const root = createRoot(updateRoot)
+
+  root.render(
     <ThemeProvider>
       <Modal title="New updates are available" shouldShow>
         <p>Reload the page to see them.</p>
@@ -47,6 +49,5 @@ export const showCacheUpdateModal = () => {
         </Buttons>
       </Modal>
     </ThemeProvider>,
-    updateRoot,
   )
 }
