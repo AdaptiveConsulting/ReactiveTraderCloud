bfd02067c4ab36bc168508e1b9c4b817b2eb3147
Moise Lubwimi <herr_moise@hotmail.com>
2023-09-08 15:52:05 +0100

    add logs

--- packages/client/src/client/main.ts
+++ packages/client/src/client/main.ts
@@ -9,29 +9,34 @@ import { register } from "./Web/serviceWorkerRegistration"
 export const gaDimension = "browser"
 
 export const getMainApp: () => React.FC = () => {
-  if (import.meta.env.PROD) {
-    register({
-      onUpdate: (registration) => {
-        // If the SW got updated, then we have to be careful. We can't immediately
-        // skip the waiting phase, because if there are requests on the fly that
-        // could be a disaster
-        // Wait for our async chunks to be loaded, then skip waiting phase and show
-        // the user a modal informing them that there are new updates available
+  // if (import.meta.env.PROD) {
+  register({
+    onUpdate: (registration) => {
+      // If the SW got updated, then we have to be careful. We can't immediately
+      // skip the waiting phase, because if there are requests on the fly that
+      // could be a disaster
+      // Wait for our async chunks to be loaded, then skip waiting phase and show
+      // the user a modal informing them that there are new updates available
 
-        console.log("Service worker on update")
+      console.log("Service worker on update")
 
-        Promise.all([
-          AnalyticsCoreDeferred,
-          LiveRatesCoreDeferred,
-          TradesCoreDeferred,
-        ]).then(() => {
-          console.log("Deferred components resolved, sending skip message")
-          registration.waiting?.postMessage({ type: "SKIP_WAITING" })
-          showCacheUpdateModal()
-        })
-      },
-    })
-  }
+      Promise.all([
+        AnalyticsCoreDeferred,
+        LiveRatesCoreDeferred,
+        TradesCoreDeferred,
+      ]).then(() => {
+        console.log("Deferred components resolved, sending skip message")
+        registration.waiting?.postMessage({ type: "SKIP_WAITING" })
+        console.log("Function call showCacheUpdateModal")
+        showCacheUpdateModal()
+      })
+
+      console.log(
+        "After Deferred components promise resolved and cache update modal",
+      )
+    },
+  })
+  // }
 
   return WebApp
 }
