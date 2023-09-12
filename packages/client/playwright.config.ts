3cbda5c06e4a28ba35093cfb19f8c1fe747b4d5e
Stephan Thibodeau <103059406+stephan-thibodeau@users.noreply.github.com>
2023-09-12 08:10:16 -0400

    remove build steps from ci

--- packages/client/playwright.config.ts
+++ packages/client/playwright.config.ts
@@ -3,7 +3,7 @@ import { devices } from "@playwright/test"
 
 const config: PlaywrightTestConfig = {
   testDir: "./e2e",
-  testIgnore: "./e2e/openfinchecks",
+  testIgnore: "./e2e/openfinchecks/**",
   /* Maximum time one test can run for. */
   timeout: 30_000,
   workers: 1,
