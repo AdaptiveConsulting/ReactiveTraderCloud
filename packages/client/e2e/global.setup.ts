a2b514c277c5964f237119bdff0df6b2e61d7281
Stephan Thibodeau <103059406+stephan-thibodeau@users.noreply.github.com>
2023-09-12 08:19:09 -0400

    openfin tests using playwright

--- packages/client/e2e/global.setup.ts
+++ packages/client/e2e/global.setup.ts
@@ -1,12 +1,11 @@
 // we need to start the runtime and know the devtools port
 import { test as setup } from '@playwright/test'
-//import { launch } from 'openfin-adapter'
+import { launch } from 'openfin-adapter'
 
-const E2E_RTC_OPENFIN_ROOT_URL = process.env.E2E_RTC_OPENFIN_ROOT_URL ?? "http://localhost:1917"
+const E2E_RTC_WEB_ROOT_URL = process.env.E2E_RTC_WEB_ROOT_URL ?? "http://localhost:1917"
 
 setup('Launching application...', async () => {
-  console.log("nothing to do here")
-  // await launch({ manifestUrl: `${E2E_RTC_OPENFIN_ROOT_URL}/config/rt-fx.json`})
-  // await launch({ manifestUrl: `${E2E_RTC_OPENFIN_ROOT_URL}/config/rt-credit.json`})
-  // await launch({ manifestUrl: `${E2E_RTC_OPENFIN_ROOT_URL}/config/limit-checker.json`})
+  await launch({ manifestUrl: `${E2E_RTC_WEB_ROOT_URL}/config/rt-fx.json`})
+  await launch({ manifestUrl: `${E2E_RTC_WEB_ROOT_URL}/config/rt-credit.json`})
+  await launch({ manifestUrl: `${E2E_RTC_WEB_ROOT_URL}/config/limit-checker.json`})
 })
