021102bc73936a67d28dd736b2d59135e1186b3c
Stephan Thibodeau <103059406+stephan-thibodeau@users.noreply.github.com>
2023-09-20 17:48:38 -0400

    update playwright dependency and remove unused playwright config

--- packages/client/playwright.config.ts
+++ packages/client/playwright.config.ts
@@ -27,13 +27,6 @@ const config: PlaywrightTestConfig = {
       },
     },
   ],
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run openfin:dev',
-    port:1917,
-    reuseExistingServer: !process.env.CI,
-    timeout: 120_000,
-  },
   reporter: [
     ["list"],
     [
