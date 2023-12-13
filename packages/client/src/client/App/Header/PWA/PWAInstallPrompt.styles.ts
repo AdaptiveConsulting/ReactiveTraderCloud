1686d95afad3051d51998d6de712031bcdffa4f7
Alan Greasley <alan.greasley@weareadaptive.com>
2023-12-13 15:36:35 +0000

    style: PWA prompt blueify

--- packages/client/src/client/App/Header/PWA/PWAInstallPrompt.styles.ts
+++ packages/client/src/client/App/Header/PWA/PWAInstallPrompt.styles.ts
@@ -1,12 +1,14 @@
 import styled from "styled-components"
 
+import { BAM_THEME_BLUE } from "@/client/theme/colors"
+
 export const MainBanner = styled.div<{ isHidden: boolean }>`
   display: ${({ isHidden }) => (isHidden ? "none" : "flex")};
   align-items: center;
   padding: 0 10px;
   width: 100%;
-  height: 32px;
-  background-color: ${({ theme }) => theme.core.textColor};
+  height: 36px;
+  background-color: ${BAM_THEME_BLUE};
   color: ${({ theme }) => theme.core.darkBackground};
   z-index: 100;
 `
@@ -28,11 +30,12 @@ export const BannerText = styled.p`
 `
 
 export const InstallButton = styled.button`
-  background-color: ${({ theme }) => theme.accents.primary.base};
+  background-color: ${BAM_THEME_BLUE};
   color: #ffffff;
   padding: 5px 9px;
   margin: 0 10px;
   border-radius: 4px;
+  border: 2px solid rgba(255, 255, 255, 0.25);
   font-size: 0.6875rem;
   &:hover {
     background-color: ${({ theme }) => theme.accents.primary.darker};
