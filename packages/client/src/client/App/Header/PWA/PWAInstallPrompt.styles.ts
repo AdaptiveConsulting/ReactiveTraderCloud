ead97a342be4507b6a2b63515b9483548259d7a9
Sam Holleworth <sam.holleworth@weareadaptive.com>
2024-12-04 12:03:20 +0000

    feat: PWA banner

--- packages/client/src/client/App/Header/PWA/PWAInstallPrompt.styles.ts
+++ packages/client/src/client/App/Header/PWA/PWAInstallPrompt.styles.ts
@@ -3,38 +3,22 @@ import styled from "styled-components"
 export const MainBanner = styled.div<{ isHidden: boolean }>`
   display: ${({ isHidden }) => (isHidden ? "none" : "flex")};
   align-items: center;
-  padding: 0 10px;
+  gap: ${({ theme }) => theme.newTheme.spacing.md};
+  padding: 0 ${({ theme }) => theme.newTheme.spacing.lg};
   width: 100%;
   height: 32px;
-  background-color: ${({ theme }) => theme.core.textColor};
-  color: ${({ theme }) => theme.core.darkBackground};
   z-index: 100;
+  background-color: ${({ theme }) =>
+    theme.newTheme.color["Colors/Background/bg-primary-solid"]};
 `
 
 export const CrossButton = styled.div`
-  width: 24px;
-  height: 24px;
-  svg path:last-child {
-    fill: ${({ theme }) => theme.core.darkBackground};
+  svg {
+    height: 12px;
+    stroke: ${({ theme }) =>
+      theme.newTheme.color["Colors/Foreground/fg-primary (900)"]};
   }
   &:hover {
     cursor: pointer;
-  }
-`
-
-export const BannerText = styled.p`
-  font-size: 0.8125rem;
-  font-weight: 600;
-`
-
-export const InstallButton = styled.button`
-  background-color: ${({ theme }) => theme.accents.primary.base};
-  color: #ffffff;
-  padding: 5px 9px;
-  margin: 0 10px;
-  border-radius: 4px;
-  font-size: 0.6875rem;
-  &:hover {
-    background-color: ${({ theme }) => theme.accents.primary.darker};
   }
 `
