d679655956650e0a6125d6a5c16f19f717f3586b
Sam Holleworth <sam.holleworth@weareadaptive.com>
2025-01-10 10:50:28 +0000

    chore: update isMobile conditions

--- packages/client/src/client/utils/isMobile.ts
+++ packages/client/src/client/utils/isMobile.ts
@@ -1,7 +1,26 @@
-export const isMobileDevice =
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
-    navigator.userAgent,
-  )
+// source: https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent
+const getHasTouchScreen = () => {
+  let hasTouchScreen = false
+  if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0
+  } else {
+    const mQ = matchMedia?.("(pointer:coarse)")
+    if (mQ?.media === "(pointer:coarse)") {
+      hasTouchScreen = !!mQ.matches
+    } else if ("orientation" in window) {
+      hasTouchScreen = true // deprecated, but good fallback
+    } else {
+      // Only as a last resort, fall back to user agent sniffing
+      const UA = (navigator as Navigator).userAgent
+      hasTouchScreen =
+        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+    }
+  }
+  return hasTouchScreen
+}
+
+export const isMobileDevice = getHasTouchScreen()
 
 export const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent)
 
