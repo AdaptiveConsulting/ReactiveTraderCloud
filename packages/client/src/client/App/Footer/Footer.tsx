2f8a56e06da9bb7352301786d548135cb2cf55ac
Sam Holleworth <sam.holleworth@weareadaptive.com>
2025-01-14 13:06:39 +0000

    chore: added Aeron logo

--- packages/client/src/client/App/Footer/Footer.tsx
+++ packages/client/src/client/App/Footer/Footer.tsx
@@ -1,4 +1,7 @@
 import { Suspense } from "react"
+import styled from "styled-components"
+
+import { AeronLogo } from "@/client/components/icons/AeronLogo"
 
 import ContactUsButton from "./ContactUsButton"
 import { Stats } from "./Stats"
@@ -6,8 +9,15 @@ import StatusBar from "./StatusBar"
 import { StatusButton } from "./StatusButton"
 import { Version } from "./Version"
 
+const AeronLogoWrapper = styled.div`
+  margin-right: auto;
+`
+
 export const Footer = () => (
   <StatusBar>
+    <AeronLogoWrapper>
+      <AeronLogo height={14} fill="white" />
+    </AeronLogoWrapper>
     <Suspense fallback={null}>
       <Stats />
     </Suspense>
--- packages/client/src/client/App/Footer/StatusBar/styled.tsx
+++ packages/client/src/client/App/Footer/StatusBar/styled.tsx
@@ -4,56 +4,19 @@ import Icon from "./Icon"
 
 const headerHeight = "2rem"
 
-export const Header = styled.div`
+export const Root = styled.div`
   display: flex;
+  justify-content: flex-end;
   align-items: center;
+  width: 100%;
+  min-height: ${headerHeight};
+  max-height: ${headerHeight};
+  z-index: 20;
   height: 2rem;
   padding: 0 1rem;
   gap: ${({ theme }) => theme.newTheme.spacing.sm};
 `
 
-export const Root = styled.div`
-  width: 100%;
-  min-height: ${headerHeight};
-  max-height: ${headerHeight};
-  z-index: 20;
-`
-
 export const ChevronIcon = (props: { expand: boolean }) => (
   <Icon IconComponent={ChevronIcon} {...props} />
 )
-
-export const ExpandToggle = styled(ChevronIcon)`
-  transform: rotate(
-    ${(props: { expand: boolean }) => (props.expand ? 180 : 0)}deg
-  );
-  transition: transform ${({ theme }) => theme.motion.duration}ms ease;
-`
-
-export const Fill = styled.div<{ size?: number }>`
-  flex: ${({ size = 1 }) => size};
-`
-
-export const ServiceList = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
-  grid-auto-flow: dense;
-  box-shadow: 0 0 1rem 0rem rgba(0, 0, 0, 0.1) inset;
-`
-
-export const ServiceName = styled.div`
-  min-width: 5rem;
-  text-transform: capitalize;
-  font-size: 1rem;
-  line-height: 1rem;
-`
-
-export const NodeCount = styled.div`
-  display: block;
-  margin-bottom: -0.5rem;
-  min-height: 1rem;
-  max-height: 1rem;
-  line-height: 1rem;
-  font-size: 0.5rem;
-  opacity: 0.6;
-`
