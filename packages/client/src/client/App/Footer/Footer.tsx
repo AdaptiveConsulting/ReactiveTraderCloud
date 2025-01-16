82bfda78197ee24a18fa2c9d01b9765ea2118bc6
Sam Holleworth <sam.holleworth@weareadaptive.com>
2025-01-16 14:35:22 +0000

    chore: update aeron footer, add link to aeron.io

--- packages/client/src/client/App/Footer/Footer.tsx
+++ packages/client/src/client/App/Footer/Footer.tsx
@@ -1,7 +1,10 @@
 import { Suspense } from "react"
 import styled from "styled-components"
 
+import { FlexBox } from "@/client/components/FlexBox"
 import { AeronLogo } from "@/client/components/icons/AeronLogo"
+import { Typography } from "@/client/components/Typography"
+import { AERON } from "@/client/constants"
 
 import ContactUsButton from "./ContactUsButton"
 import { Stats } from "./Stats"
@@ -9,8 +12,11 @@ import StatusBar from "./StatusBar"
 import { StatusButton } from "./StatusButton"
 import { Version } from "./Version"
 
-const AeronLogoWrapper = styled.div`
+const AeronLogoWrapper = styled(FlexBox)`
   margin-right: auto;
+  align-items: flex-start;
+  gap: ${({ theme }) => theme.newTheme.spacing.xs};
+  cursor: pointer;
   svg {
     fill: ${({ theme }) =>
       theme.newTheme.color["Colors/Text/text-primary (900)"]};
@@ -19,8 +25,14 @@ const AeronLogoWrapper = styled.div`
 
 export const Footer = () => (
   <StatusBar>
-    <AeronLogoWrapper>
-      <AeronLogo height={14} />
+    <AeronLogoWrapper onClick={() => window.open(AERON)}>
+      <Typography
+        variant="Text xs/Regular"
+        color="Colors/Text/text-quaternary (500)"
+      >
+        Powered by
+      </Typography>
+      <AeronLogo height={10} />
     </AeronLogoWrapper>
     <Suspense fallback={null}>
       <Stats />
