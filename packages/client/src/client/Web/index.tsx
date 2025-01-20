bf969896334fe1ffd06d539d018f174532f11f0b
Sam Holleworth <sam.holleworth@weareadaptive.com>
2025-01-20 17:27:41 +0000

    chore: add jira id

--- packages/client/src/client/Web/index.tsx
+++ packages/client/src/client/Web/index.tsx
@@ -89,7 +89,7 @@ export const WebApp = () => {
               <StyleguidePage />
             </Suspense>
           }
-        /> TODO: either remove if UISK takes this role, or refactor with new branding */}
+        /> TODO: either remove if UISK takes this role, or refactor with new branding - https://weareadaptive.atlassian.net/jira/software/c/projects/SU/boards/284?selectedIssue=SU-47 */}
 
         {canUseAdmin && (
           <Route path={ROUTES_CONFIG.admin} element={<Admin />} />
