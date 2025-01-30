d7bd14c95061ce62f4b9fb3985e2e50feffb15de
Sam Holleworth <sam.holleworth@weareadaptive.com>
2025-01-30 17:28:54 +0000

    feat: give typography spacing props, add stack component

---
+++ packages/client/src/client/components/SpacingContainer.tsx
@@ -0,0 +1,97 @@
+import styled, { css } from "styled-components"
+
+import { Spacing as ThemeSpacing } from "../theme/types"
+
+// add custom css properties here
+const customMarginPropNames = ["marginX", "marginY"] as const
+const customPaddingPropNames = ["paddingX", "paddingY"] as const
+
+// add existing css properties here
+const marginPropNames = [
+  "margin",
+  "marginTop",
+  "marginBottom",
+  "marginLeft",
+  "marginRight",
+  ...customMarginPropNames,
+] as const
+
+const paddingPropNames = [
+  "padding",
+  "paddingTop",
+  "paddingBottom",
+  "paddingLeft",
+  "paddingRight",
+  ...customPaddingPropNames,
+] as const
+
+type MarginKeys = (typeof marginPropNames)[number]
+type PaddingKeys = (typeof paddingPropNames)[number]
+
+type MarginProps = { [k in MarginKeys]?: ThemeSpacing | number }
+
+type PaddingProps = { [k in PaddingKeys]?: ThemeSpacing | number }
+export type SpacingProps = MarginProps & PaddingProps
+
+export const SpacingContainer = styled.div<SpacingProps>`
+  ${({ theme, ...props }) => {
+    const mapSpacingCss = (prop?: ThemeSpacing | number) =>
+      prop && (typeof prop === "number" ? prop : theme.newTheme.spacing[prop])
+
+    const marginProps = Object.entries(props).filter(
+      ([name]) =>
+        marginPropNames.includes(name as (typeof marginPropNames)[number]) &&
+        !customMarginPropNames.includes(
+          name as (typeof customMarginPropNames)[number],
+        ),
+    )
+
+    const paddingProps = Object.entries(props).filter(
+      ([name]) =>
+        paddingPropNames.includes(name as (typeof paddingPropNames)[number]) &&
+        !customPaddingPropNames.includes(
+          name as (typeof customPaddingPropNames)[number],
+        ),
+    )
+
+    const { marginX, marginY, paddingX, paddingY } = props
+
+    const customSpacingStyles = {
+      ...(paddingX &&
+        Object.fromEntries([
+          ["paddingLeft", mapSpacingCss(paddingX)],
+          ["paddingRight", mapSpacingCss(paddingX)],
+        ])),
+      ...(paddingY &&
+        Object.fromEntries([
+          ["paddingTop", mapSpacingCss(paddingY)],
+          ["paddingBottom", mapSpacingCss(paddingY)],
+        ])),
+      ...(marginX &&
+        Object.fromEntries([
+          ["marginLeft", mapSpacingCss(marginX)],
+          ["marginRight", mapSpacingCss(marginX)],
+        ])),
+      ...(marginY &&
+        Object.fromEntries([
+          ["marginTop", mapSpacingCss(marginY)],
+          ["marginBottom", mapSpacingCss(marginY)],
+        ])),
+    }
+
+    const marginStyles = Object.fromEntries(
+      marginProps.map(([name, value]) => [name, mapSpacingCss(value)]),
+    )
+
+    const paddingStyles = Object.fromEntries(
+      paddingProps.map(([name, value]) => [name, mapSpacingCss(value)]),
+    )
+
+    return css({
+      ...customSpacingStyles,
+      // the more specific styles should overwrite the more general custom styles
+      ...marginStyles,
+      ...paddingStyles,
+    })
+  }}
+`
