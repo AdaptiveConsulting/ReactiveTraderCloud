e47a4226d0adecdcf4e1558adbbc34208a4ef41a
Umar Kayani <umar.kayani@weareadaptive.com>
2025-03-20 14:13:20 +0000

    Addressed PR comments
    * renamed PO file
    * moved getters to top of PO file
    * cleaned up POs - removed async/awaits

--- packages/client/e2e/pages/index.ts
+++ packages/client/e2e/pages/index.ts
@@ -2,6 +2,5 @@ export * from "./CreditBlotterPageObject"
 export * from "./CreditNewRfqPageObject"
 export * from "./CreditRfqTilesPageObject"
 export * from "./CreditSellSidePageObject"
-// FX pages
 export * from "./FxBlotterPageObject"
-export * from "./FxNewRfqPageObject"
+export * from "./FxTilesPageObject"
